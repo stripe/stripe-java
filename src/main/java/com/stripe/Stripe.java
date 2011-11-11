63015696c511607814f94112afdf390dfd797724
Anurag Goel <_@anur.ag>
2011-11-11 14:18:30 -0800

    include VERSION in jar

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -1,8 +1,8 @@
 package com.stripe;
 
-public abstract class Stripe 
+public abstract class Stripe
 {
 	public static final String API_BASE = "https://api.stripe.com/v1";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static String apiKey;
 }
