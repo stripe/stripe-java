c8ca58960ca0be2d9ebc6304d47ca2d6a2683184
Anurag Goel <_@anur.ag>
2012-03-16 17:06:39 -0700

    Support nested previous_attributes; version bump.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Add this dependency to your project's POM:
     <dependency>
       <groupId>com.stripe</groupId>
       <artifactId>stripe-java</artifactId>
-      <version>1.0.13</version>
+      <version>1.0.14</version>
     </dependency>
 
 ### Others
--- src/main/java/com/stripe/model/EventDataDeserializer.java
+++ src/main/java/com/stripe/model/EventDataDeserializer.java
@@ -2,14 +2,16 @@ package com.stripe.model;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonPrimitive;
 import com.stripe.net.APIResource;
 
 public class EventDataDeserializer implements JsonDeserializer<EventData> {
@@ -26,6 +28,54 @@ public class EventDataDeserializer implements JsonDeserializer<EventData> {
         objectMap.put("subscription", Subscription.class);
         objectMap.put("token", Token.class);
     }
+    
+    private Object deserializeJsonPrimitive(JsonPrimitive element) {
+    	if (element.isBoolean()) {
+    		return element.getAsBoolean();
+    	} else if (element.isNumber()) {
+    		return element.getAsNumber();
+    	} else {
+    		return element.getAsString();
+    	}
+    }
+    
+    private Object[] deserializeJsonArray(JsonArray arr) {
+    	Object[] elems = new Object[arr.size()];
+    	Iterator<JsonElement> elemIter = arr.iterator();
+    	int i = 0;
+    	while (elemIter.hasNext()) {
+    		JsonElement elem = elemIter.next();
+    		elems[++i] = deserializeJsonElement(elem);
+    	}
+    	return elems;
+    }
+    
+    private Object deserializeJsonElement(JsonElement element) {
+    	if (element.isJsonNull()) {
+    		return null;
+    	} else if (element.isJsonObject()) {
+			Map<String, Object> valueMap = new HashMap<String, Object>();
+			populateMapFromJSONObject(valueMap, element.getAsJsonObject());
+			return valueMap;
+		} else if (element.isJsonPrimitive()) {
+			return deserializeJsonPrimitive(element.getAsJsonPrimitive());
+		} else if (element.isJsonArray()) {
+			return deserializeJsonArray(element.getAsJsonArray());
+		} else {
+			System.err.println("Unknown JSON element type for element " + element + ". " +
+					"If you're seeing this messaage, it's probably a bug in the Stripe Java " +
+					"library. Please contact us by email at support@stripe.com.");
+			return null;
+		}
+	}
+    
+    private void populateMapFromJSONObject(Map<String, Object> objMap, JsonObject jsonObject) {
+		for(Map.Entry<String, JsonElement> entry: jsonObject.entrySet()) {
+			String key = entry.getKey();
+			JsonElement element = entry.getValue();
+			objMap.put(key, deserializeJsonElement(element));
+		}
+    }
 
 	@SuppressWarnings("unchecked")
 	public EventData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
@@ -36,9 +86,9 @@ public class EventDataDeserializer implements JsonDeserializer<EventData> {
 			String key = entry.getKey();
 			JsonElement element = entry.getValue();
 			if("previous_attributes".equals(key)) {
-				Type typeOfPrevAttrs = new TypeToken<Map<String, Object>>(){}.getType();
-				eventData.setPreviousAttributes(
-					(Map<String, Object>) APIResource.gson.fromJson(element, typeOfPrevAttrs));
+				Map<String, Object> previousAttributes = new HashMap<String, Object>();
+				populateMapFromJSONObject(previousAttributes, element.getAsJsonObject());
+				eventData.setPreviousAttributes(previousAttributes);
 			} else if ("object".equals(key)) {
 				String type = element.getAsJsonObject().get("object").getAsString();
 				Class<StripeObject> cl = objectMap.get(type);
