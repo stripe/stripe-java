1b5e0f99e72047180d0088dfefa5d59d5456917a
Sheena Pakanati <spakanati@gmail.com>
2012-11-15 22:34:19 -0800

    Bump version to 1.1.10

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -3,6 +3,6 @@ package com.stripe;
 public abstract class Stripe
 {
 	public static final String API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.1.9";
+	public static final String VERSION = "1.1.10";
 	public static String apiKey;
 }
