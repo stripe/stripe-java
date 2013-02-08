58b0ab8f0b3c35a5394989560ff87325575394b6
Bryan Bess <info@bsbess.com>
2013-02-08 09:11:29 -0600

    Make whitespace consistent

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -5,5 +5,5 @@ public abstract class Stripe
 	public static final String API_BASE = "https://api.stripe.com";
 	public static final String VERSION = "1.1.15";
 	public static String apiKey;
-  public static String apiVersion;
+	public static String apiVersion;
 }
