8f9f544a5abe9ce000c109a119bf4d88c0ef88c0
Anurag Goel <_@anur.ag>
2013-04-20 14:11:40 -0700

    Bump version to 1.2.1

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -3,7 +3,7 @@ package com.stripe;
 public abstract class Stripe
 {
 	public static final String API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	public static String apiKey;
 	public static String apiVersion;
 }
