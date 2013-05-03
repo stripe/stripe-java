dbbbb764a9bdb8326ec1d6027fad8c5bff1c11b1
Steve Nester <steve@sahfor.com>
2013-05-03 19:48:19 +0100

    -Added overloaded constructor for StripeResponse to pass HTTP response headers -Updated contant name so it is uppercase: prettyPrintGson -> PRETTY_PRINT_GSON / gson -> GSON -Added more readable timeout values 30000 -> 30 * 1000 / 80000 -> 80 * 1000 -Removed trailing comments (per Steve McConnel in "Code Complete") -Replaced StringBuffer with StringBuilder -instance variable should be generic Map type -Per Josh Block on Design Copy Constructor should be used over clone (StripeTest) -Refactored createQuery so it doesn't have to call queryStringBuffer.deleteCharAt(0);

--- src/main/java/com/stripe/net/StripeResponse.java
+++ src/main/java/com/stripe/net/StripeResponse.java
@@ -1,13 +1,23 @@
 package com.stripe.net;
 
+import java.util.List;
+import java.util.Map;
+
 public class StripeResponse {
 	
 	int responseCode;
 	String responseBody;
+        Map<String, List<String>> responseHeaders;
 	
 	public StripeResponse(int responseCode, String responseBody) {
 		this.responseCode = responseCode;
 		this.responseBody = responseBody;
+                this.responseHeaders = null;
+	}
+        public StripeResponse(int responseCode, String responseBody, Map<String, List<String>> responseHeaders) {
+		this.responseCode = responseCode;
+		this.responseBody = responseBody;
+                this.responseHeaders = responseHeaders;
 	}
 
 	public int getResponseCode() {
@@ -24,5 +34,8 @@ public class StripeResponse {
 	
 	public void setResponseBody(String responseBody) {
 		this.responseBody = responseBody;
+	}
+        public Map<String, List<String>> getResponseHeaders() {
+		return responseHeaders;
 	}
 }
