6b19571ed20b4769223cc366e91ae874c3ba640d
Jim Danz <jim@stripe.com>
2013-07-31 18:46:31 -0700

    Bump version to 1.2.7

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Add this dependency to your project's POM:
     <dependency>
       <groupId>com.stripe</groupId>
       <artifactId>stripe-java</artifactId>
-      <version>1.2.6</version>
+      <version>1.2.7</version>
     </dependency>
 
 ### Others
--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -3,7 +3,7 @@ package com.stripe;
 public abstract class Stripe
 {
 	public static final String API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.2.6";
+	public static final String VERSION = "1.2.7";
 	public static String apiKey;
 	public static String apiVersion;
 }
