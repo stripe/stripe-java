9879223da80edda81ac605e1c0ca8afc58890ec6
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-16 17:18:49 -0800

    version 1.6.4

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -3,7 +3,7 @@ package com.stripe;
 public abstract class Stripe
 {
 	public static final String LIVE_API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.6.3";
+	public static final String VERSION = "1.6.4";
 	public static String apiKey;
 	public static String apiVersion;
 
