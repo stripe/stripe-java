7591a6563da0eb481101d16f67bd559ffd58a4c9
Jim Danz <jim@stripe.com>
2014-04-07 00:37:24 -0700

    upgrade customer, charge, and coupon collections to new pagination style
    Addresses #69

--- src/main/java/com/stripe/model/ChargeCollection.java
+++ src/main/java/com/stripe/model/ChargeCollection.java
@@ -1,22 +1,4 @@
 package com.stripe.model;
 
-import java.util.List;
-
-
-public class ChargeCollection extends StripeObject {
-	List<Charge> data;
-	Integer count;
-	
-	public List<Charge> getData() {
-		return data;
-	}
-	public void setData(List<Charge> data) {
-		this.data = data;
-	}
-	public Integer getCount() {
-		return count;
-	}
-	public void setCount(Integer count) {
-		this.count = count;
-	}
+public class ChargeCollection extends StripeCollection<Charge> {
 }
