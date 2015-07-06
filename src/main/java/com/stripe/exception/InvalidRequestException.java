6997a7c117525a8c6301b1461ef207606195afec
Nik Youdale <nik.youdale@gmail.com>
2015-07-06 12:21:08 +1000

    Adds http status code to InvalidRequestException

--- src/main/java/com/stripe/exception/InvalidRequestException.java
+++ src/main/java/com/stripe/exception/InvalidRequestException.java
@@ -5,14 +5,19 @@ public class InvalidRequestException extends StripeException {
 	private static final long serialVersionUID = 1L;
 
 	private final String param;
+	private final Integer code;
 
-	public InvalidRequestException(String message, String param, Throwable e) {
+	public InvalidRequestException(String message, String param, Integer code, Throwable e) {
 		super(message, e);
 		this.param = param;
+		this.code = code;
 	}
 
 	public String getParam() {
 		return param;
 	}
 
+	public Integer getCode() {
+		return code;
+	}
 }
