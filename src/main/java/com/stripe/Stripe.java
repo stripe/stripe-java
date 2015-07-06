8474e32febdc659c2f428a5e6da252b55de87f87
Caitlin Johnson <caitlin@stripe.com>
2015-07-06 15:13:34 -0700

    Bump version to 1.32.0

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -3,7 +3,7 @@ package com.stripe;
 public abstract class Stripe {
 	public static final String UPLOAD_API_BASE = "https://uploads.stripe.com";
 	public static final String LIVE_API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.31.0";
+	public static final String VERSION = "1.32.0";
 	public static volatile String apiKey;
 	public static volatile String apiVersion;
 
