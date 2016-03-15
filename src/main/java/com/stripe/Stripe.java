075c4614f8342d73c3f03c18e5db87d6dad820c7
Brandur <brandur@mutelight.org>
2016-03-15 14:09:15 -0700

    Bump version to 1.46.0

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -6,7 +6,7 @@ import java.net.Proxy;
 public abstract class Stripe {
 	public static final String UPLOAD_API_BASE = "https://uploads.stripe.com";
 	public static final String LIVE_API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "1.45.0";
+	public static final String VERSION = "1.46.0";
 	public static volatile String apiKey;
 	public static volatile String apiVersion;
 
