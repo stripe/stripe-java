6083eb7a118741753146b73ff29315ed521b78f9
Miloš Milivojević <mmilivojevic@deployinc.com>
2016-06-11 17:54:49 +0200

    Account URLs deserialization test

--- src/test/java/com/stripe/model/AccountTest.java
+++ src/test/java/com/stripe/model/AccountTest.java
@@ -1,21 +1,18 @@
 package com.stripe.model;
 
-import com.stripe.BaseStripeTest;
-import com.stripe.exception.StripeException;
-import com.stripe.model.Account;
-import com.stripe.net.APIResource;
-import com.stripe.net.LiveStripeResponseGetter;
-import com.stripe.net.RequestOptions.RequestOptionsBuilder;
-import com.stripe.net.RequestOptions;
-import org.junit.After;
-import org.junit.Before;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
+import com.stripe.BaseStripeTest;
+import com.stripe.exception.StripeException;
+import com.stripe.net.APIResource;
+import com.stripe.net.LiveStripeResponseGetter;
+import com.stripe.net.RequestOptions;
+import com.stripe.net.RequestOptions.RequestOptionsBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -42,6 +39,8 @@ public class AccountTest extends BaseStripeTest {
 		assertEquals(false, acc.getChargesEnabled());
 		assertEquals(false, acc.getDetailsSubmitted());
 		assertEquals(false, acc.getTransfersEnabled());
+		assertEquals("http://www.business.com", acc.getBusinessURL());
+		assertEquals("http://support.business.com", acc.getSupportURL());
 
 		LinkedList<String> cs = new LinkedList<String>();
 		cs.add("usd");
