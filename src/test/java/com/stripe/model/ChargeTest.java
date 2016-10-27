fcd010bc609ab2830daa92afa726d740e4bbdf5b
David Petrovics <dpetrovics@stripe.com>
2016-10-27 15:32:20 -0700

    Refactor Charge expansion tests

--- src/test/java/com/stripe/model/ChargeTest.java
+++ src/test/java/com/stripe/model/ChargeTest.java
@@ -3,6 +3,7 @@ package com.stripe.model;
 import com.stripe.BaseStripeTest;
 import com.stripe.exception.StripeException;
 import com.stripe.net.APIResource;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,59 +12,91 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ChargeTest extends BaseStripeTest {
-    @Test
-    public void testDeserialize() throws StripeException, IOException {
+    Charge basicCharge;
+    Charge expandedCharge;
+
+    @Before
+    public void deserializeCharges() throws IOException {
         String json = resource("charge.json");
-        Charge charge = APIResource.GSON.fromJson(json, Charge.class);
-        assertEquals("ch_198c152iMoKf4jyWWYvyqjTX", charge.getId());
-        assertEquals(1477429167, (long) charge.getCreated());
+        basicCharge = APIResource.GSON.fromJson(json, Charge.class);
+        String expandedJson = resource("charge_expansions.json");
+        expandedCharge = APIResource.GSON.fromJson(expandedJson, Charge.class);
     }
 
     @Test
-    public void testDeserializeExpanded() throws StripeException, IOException {
-        String json = resource("charge_expansions.json");
-        Charge charge = APIResource.GSON.fromJson(json, Charge.class);
-        assertEquals("ch_198c152iMoKf4jyWWYvyqjTX", charge.getId());
-        assertEquals(1477429167, (long) charge.getCreated());
-        //AppFee
-        ApplicationFee appFee = charge.getApplicationFeeObject();
-        assertEquals(charge.getApplicationFee(), "fee_9RV1Zcr87rvr68");
+    public void testDeserialize() throws IOException {
+        assertEquals("ch_198c152iMoKf4jyWWYvyqjTX", basicCharge.getId());
+        assertEquals(1477429167, (long) basicCharge.getCreated());
+    }
+
+
+    @Test
+    public void testDeserializeWithExpansions() throws IOException {
+        assertEquals("ch_198c152iMoKf4jyWWYvyqjTX", expandedCharge.getId());
+        assertEquals(1477429167, (long) expandedCharge.getCreated());
+    }
+
+    @Test
+    public void testDeserializeExpandApplicationFee() throws IOException {
+        ApplicationFee appFee = expandedCharge.getApplicationFeeObject();
+        assertEquals(expandedCharge.getApplicationFee(), "fee_9RV1Zcr87rvr68");
         assertEquals(appFee.getId(), "fee_9RV1Zcr87rvr68");
         assertEquals((long) appFee.getAmount(), 111);
-        //BalanceTransaction
-        BalanceTransaction bt = charge.getBalanceTransactionObject();
-        assertEquals(charge.getBalanceTransaction(), "txn_18tjj22eZvKYlo2CeFxM3FxI");
+    }
+
+    @Test
+    public void testDeserializeExpandBalanceTransaction() throws IOException {
+        BalanceTransaction bt = expandedCharge.getBalanceTransactionObject();
+        assertEquals(expandedCharge.getBalanceTransaction(), "txn_18tjj22eZvKYlo2CeFxM3FxI");
         assertEquals(bt.getId(), "txn_18tjj22eZvKYlo2CeFxM3FxI");
         assertEquals((long) bt.getAmount(), 316);
-        //Customer
-        Customer customer = charge.getCustomerObject();
-        assertEquals(charge.getCustomer(), "cus_7K0ztY0WclKUdF");
+    }
+
+    @Test
+    public void testDeserializeExpandCustomer() throws IOException {
+        Customer customer = expandedCharge.getCustomerObject();
+        assertEquals(expandedCharge.getCustomer(), "cus_7K0ztY0WclKUdF");
         assertEquals(customer.getId(), "cus_7K0ztY0WclKUdF");
         assertEquals((long) customer.getCreated(), 1447103608);
-        //Destination
-        Account destination = charge.getDestinationObject();
-        assertEquals(charge.getDestination(), "acct_102wRO2iMoKf4jyz");
+    }
+
+    @Test
+    public void testDeserializeExpandDestination() throws IOException {
+        Account destination = expandedCharge.getDestinationObject();
+        assertEquals(expandedCharge.getDestination(), "acct_102wRO2iMoKf4jyz");
         assertEquals(destination.getId(), "acct_102wRO2iMoKf4jyz");
         assertEquals(destination.getBusinessName(), "RiskyBusiness");
         assertTrue(destination.getManaged());
-        //Invoice
-        Invoice invoice = charge.getInvoiceObject();
-        assertEquals(charge.getInvoice(), "in_199Llh2eZvKYlo2CMA8qc7Ng");
+    }
+
+    @Test
+    public void testDeserializeExpandInvoice() throws IOException {
+        Invoice invoice = expandedCharge.getInvoiceObject();
+        assertEquals(expandedCharge.getInvoice(), "in_199Llh2eZvKYlo2CMA8qc7Ng");
         assertEquals(invoice.getId(), "in_199Llh2eZvKYlo2CMA8qc7Ng");
         assertEquals((long) invoice.getAmountDue(), 999);
-        //Order
-        Order order = charge.getOrderObject();
-        assertEquals(charge.getOrder(), "or_199IRC2eZvKYlo2CPfFd4CB6");
+    }
+
+    @Test
+    public void testDeserializeExpandBalanceOrder() throws IOException {
+        Order order = expandedCharge.getOrderObject();
+        assertEquals(expandedCharge.getOrder(), "or_199IRC2eZvKYlo2CPfFd4CB6");
         assertEquals(order.getId(), "or_199IRC2eZvKYlo2CPfFd4CB6");
         assertEquals((long) order.getAmount(), 1500);
-        //SourceTransfer
-        Transfer sourceTransfer = charge.getSourceTransferObject();
-        assertEquals(charge.getSourceTransfer(), "tr_199IPs2eZvKYlo2C9hMneg1R");
+    }
+
+    @Test
+    public void testDeserializeExpandSourceTransfer() throws IOException {
+        Transfer sourceTransfer = expandedCharge.getSourceTransferObject();
+        assertEquals(expandedCharge.getSourceTransfer(), "tr_199IPs2eZvKYlo2C9hMneg1R");
         assertEquals(sourceTransfer.getId(), "tr_199IPs2eZvKYlo2C9hMneg1R");
         assertEquals((long) sourceTransfer.getAmount(), 400);
-        //Transfer
-        Transfer transfer = charge.getTransferObject();
-        assertEquals(charge.getTransfer(), "tr_199IPs2eZvKYlo2C9hMneg1R");
+    }
+
+    @Test
+    public void testDeserializeExpandTransfer() throws IOException {
+        Transfer transfer = expandedCharge.getTransferObject();
+        assertEquals(expandedCharge.getTransfer(), "tr_199IPs2eZvKYlo2C9hMneg1R");
         assertEquals(transfer.getId(), "tr_199IPs2eZvKYlo2C9hMneg1R");
         assertEquals((long) transfer.getAmount(), 400);
     }
