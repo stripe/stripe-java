23ea069ddf07009ace749329165c2e9538ccdea5
David Petrovics <dpetrovics@stripe.com>
2016-11-02 09:59:12 -0700

    Change indents to tabs

--- src/main/java/com/stripe/model/Application.java
+++ src/main/java/com/stripe/model/Application.java
@@ -1,22 +1,22 @@
 package com.stripe.model;
 
 public class Application extends StripeObject implements HasId {
-    String id;
-    String name;
+	String id;
+	String name;
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 }
