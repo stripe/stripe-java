9695db09eedc6450fb9ca207ac4a99c4071cc026
Brandur <brandur@mutelight.org>
2017-02-02 09:35:15 -0800

    Bump version to 3.9.0

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -6,7 +6,7 @@ import java.net.Proxy;
 public abstract class Stripe {
 	public static final String UPLOAD_API_BASE = "https://uploads.stripe.com";
 	public static final String LIVE_API_BASE = "https://api.stripe.com";
-	public static final String VERSION = "3.8.0";
+	public static final String VERSION = "3.9.0";
 	public static volatile String apiKey;
 	public static volatile String apiVersion;
 
