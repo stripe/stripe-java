56369f7b38bba8a83576fdff72a6a7d85ba52d3f
Brandur <brandur@mutelight.org>
2017-07-06 08:03:48 -0700

    Add documentation to new methods

--- src/test/java/com/stripe/BaseStripeStubTest.java
+++ src/test/java/com/stripe/BaseStripeStubTest.java
@@ -22,6 +22,11 @@ public class BaseStripeStubTest {
 	private String oldApiBase;
 	private static String stubPort;
 
+	/**
+	 * Checks that STRIPE_STUB_PORT is specified in the environment and that
+	 * the stub is online by making a sample request. If either isn't true, an
+	 * exception is thrown and the test suite stops.
+	 */
 	@BeforeClass
 	public static void checkStripeStubPort() throws Exception {
 		stubPort = System.getenv().get("STRIPE_STUB_PORT");
@@ -40,6 +45,10 @@ public class BaseStripeStubTest {
 		}
 	}
 
+	/**
+	 * Activates stripestub by overriding the API host and putting a test key
+	 * into the environment.
+	 */
 	@Before
 	public void setUpStripeStub() {
 		Stripe.apiKey = "sk_test_myValidKey";
@@ -47,11 +56,27 @@ public class BaseStripeStubTest {
 		Stripe.overrideApiBase("http://localhost:"+stubPort);
 	}
 
+	/**
+	 * Deactivates stripestub by returning the API host to whatever it was
+	 * before stripestub was activated.
+	 */
 	@After
 	public void tearDownStripeStub() {
 		Stripe.overrideApiBase(this.oldApiBase);
 	}
 
+	/**
+	 * Convenience method that extracts a subset of JSON data and returns it.
+	 *
+	 * <p>For example, if I know that my charge object has a customer under it,
+	 * I can pass my charge JSON data and specify {@code field} as {@code
+	 * customer}. This returns everything that had been under the {@code
+	 * customer} key (encoded as JSON).
+	 *
+	 * @param data JSON encoded data.
+	 * @param field Field under data which to extract.
+	 * @return Extracted JSON encoded data.
+	 */
 	protected static String getDataAt(String data, String field) {
 		Gson gson = new Gson();
 		Type type = new TypeToken<Map<String, Object>>(){}.getType();
@@ -60,10 +85,34 @@ public class BaseStripeStubTest {
 		return gson.toJson(value);
 	}
 
+	/**
+	 * Gets fixture data from stripestub for a resource expected to be at the
+	 * given API path. stripestub ignores whether IDs are actually valid, so
+	 * it's only important to make sure that the route exists, rather than the
+	 * actual resource. It's common to use a symbolic ID stand-in like {@code
+	 * ch_123}.
+	 *
+	 * <pre>
+	 * getFixture("/v1/charges/ch_123")
+	 * </pre>
+	 *
+	 * @param path API path to use to get a fixture for stripestub.
+	 * @return Fixture data encoded as JSON.
+	 */
 	protected static String getFixture(String path) throws Exception, IOException, MalformedURLException, ProtocolException {
 		return getFixture(path, null);
 	}
 
+	/**
+	 * Gets fixture data with expansions specified. Expansions are specified
+	 * the same way as they are in the normal API like {@code customer} or
+	 * {@code data.customer}. Use the special {@code *} character to specify
+	 * that all fields should be expanded.
+	 *
+	 * @param path API path to use to get a fixture for stripestub.
+	 * @param expansions Set of expansions that should be applied.
+	 * @return Fixture data encoded as JSON.
+	 */
 	protected static String getFixture(String path, String[] expansions) throws Exception, IOException, MalformedURLException, ProtocolException {
 		StringBuffer urlStringBuffer = new StringBuffer();
 		urlStringBuffer.append("http://localhost:"+stubPort+path);
