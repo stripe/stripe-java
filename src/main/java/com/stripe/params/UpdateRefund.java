d3fa229a5bda88b5ed8358751867f6c641ed60cb
Kyle Conroy <kjc@stripe.com>
2017-07-07 12:12:19 -0700

    Add value objects and typed methods for refunds

---
+++ src/main/java/com/stripe/params/UpdateRefund.java
@@ -0,0 +1,31 @@
+package com.stripe.params;
+
+import com.google.auto.value.AutoValue;
+
+import java.util.Map;
+import java.util.HashMap;
+import javax.annotation.Nullable;
+
+@AutoValue
+public abstract class UpdateRefund {
+	@Nullable abstract Map<String, String> metadata();
+
+	public static Builder builder() {
+		return new AutoValue_UpdateRefund.Builder();
+	}
+
+	public Map<String, Object> toMap() {
+		Map<String, Object> m = new HashMap<String, Object>();
+		if (metadata() != null) {
+			m.put("metadata", metadata());
+		}
+		return m;
+	}
+
+	@AutoValue.Builder
+	public abstract static class Builder {
+		public abstract Builder metadata(Map<String, String> value);
+
+		public abstract UpdateRefund build();
+	}
+}
