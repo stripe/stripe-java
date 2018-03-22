8b16d8709303f59dbcdaddaf66ee4f245dbdbb49
Olivier Bellone <ob@stripe.com>
2018-03-22 14:21:15 +0100

    Switch indentation to tabs

--- src/test/java/com/stripe/model/ExpandableFieldSerializerTest.java
+++ src/test/java/com/stripe/model/ExpandableFieldSerializerTest.java
@@ -10,46 +10,46 @@ import static org.junit.Assert.assertEquals;
 
 public class ExpandableFieldSerializerTest extends BaseStripeTest {
 
-    private class TestNestedObject implements HasId {
-        String id;
-        int bar;
+	private class TestNestedObject implements HasId {
+		String id;
+		int bar;
 
-        public String getId() {
-            return id;
-        }
-    }
+		public String getId() {
+			return id;
+		}
+	}
 
-    private class TestTopLevelObject extends StripeObject {
-        ExpandableField<TestNestedObject> nested;
-    }
+	private class TestTopLevelObject extends StripeObject {
+		ExpandableField<TestNestedObject> nested;
+	}
 
-    @Test
-    public void serializeNotExpanded() throws IOException {
-        TestTopLevelObject object = new TestTopLevelObject();
-        object.nested = new ExpandableField<TestNestedObject>("id_not_expanded", null);
+	@Test
+	public void serializeNotExpanded() throws IOException {
+		TestTopLevelObject object = new TestTopLevelObject();
+		object.nested = new ExpandableField<TestNestedObject>("id_not_expanded", null);
 
-        String expected = "{\n  \"nested\": \"id_not_expanded\"\n}";
-        assertEquals(expected, object.toJson());
-    }
+		String expected = "{\n  \"nested\": \"id_not_expanded\"\n}";
+		assertEquals(expected, object.toJson());
+	}
 
-    @Test
-    public void serializeExpanded() throws IOException {
-        TestNestedObject nested = new TestNestedObject();
-        nested.id = "id_expanded";
-        nested.bar = 42;
-        TestTopLevelObject object = new TestTopLevelObject();
-        object.nested = new ExpandableField<TestNestedObject>(nested.id, nested);
+	@Test
+	public void serializeExpanded() throws IOException {
+		TestNestedObject nested = new TestNestedObject();
+		nested.id = "id_expanded";
+		nested.bar = 42;
+		TestTopLevelObject object = new TestTopLevelObject();
+		object.nested = new ExpandableField<TestNestedObject>(nested.id, nested);
 
-        String expected = "{\n  \"nested\": {\n    \"id\": \"id_expanded\",\n    \"bar\": 42\n  }\n}";
-        assertEquals(expected, object.toJson());
-    }
+		String expected = "{\n  \"nested\": {\n    \"id\": \"id_expanded\",\n    \"bar\": 42\n  }\n}";
+		assertEquals(expected, object.toJson());
+	}
 
-    @Test
-    public void serializeNull() throws IOException {
-        TestTopLevelObject object = new TestTopLevelObject();
-        object.nested = new ExpandableField<TestNestedObject>(null, null);
+	@Test
+	public void serializeNull() throws IOException {
+		TestTopLevelObject object = new TestTopLevelObject();
+		object.nested = new ExpandableField<TestNestedObject>(null, null);
 
-        String expected = "{\n  \"nested\": null\n}";
-        assertEquals(expected, object.toJson());
-    }
+		String expected = "{\n  \"nested\": null\n}";
+		assertEquals(expected, object.toJson());
+	}
 }
