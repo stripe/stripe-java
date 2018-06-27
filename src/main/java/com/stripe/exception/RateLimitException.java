101decc8ec6be400ab066fa35baa8327d66b0128
Olivier Bellone <ob@stripe.com>
2018-06-27 14:49:47 +0200

    Remove deprecated exception constructors

--- src/main/java/com/stripe/exception/RateLimitException.java
+++ src/main/java/com/stripe/exception/RateLimitException.java
@@ -3,18 +3,6 @@ package com.stripe.exception;
 public class RateLimitException extends InvalidRequestException {
   private static final long serialVersionUID = 2L;
 
-  /**
-   * Constructs a new rate limit exception with the specified details.
-   *
-   * @deprecated Use new constructor with `code` argument instead.
-   */
-  @Deprecated
-  // TODO: remove this constructor in next major version bump
-  public RateLimitException(String message, String param, String requestId, Integer statusCode,
-      Throwable e) {
-    this(message, param, requestId, null, statusCode, e);
-  }
-
   public RateLimitException(String message, String param, String requestId, String code,
       Integer statusCode, Throwable e) {
     super(message, param, requestId, code, statusCode, e);
