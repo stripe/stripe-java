f568cdb8d2d8f29c10f1bfdd2b67070630e076e7
Olivier Bellone <ob@stripe.com>
2018-06-27 14:49:47 +0200

    Remove deprecated exception constructors

--- src/main/java/com/stripe/exception/ApiException.java
+++ src/main/java/com/stripe/exception/ApiException.java
@@ -3,17 +3,6 @@ package com.stripe.exception;
 public class ApiException extends StripeException {
   private static final long serialVersionUID = 2L;
 
-  /**
-   * Constructs a new API exception with the specified details.
-   *
-   * @deprecated Use new constructor with `code` argument instead.
-   */
-  @Deprecated
-  // TODO: remove this constructor in next major version bump
-  public ApiException(String message, String requestId, Integer statusCode, Throwable e) {
-    this(message, requestId, null, statusCode, e);
-  }
-
   public ApiException(String message, String requestId, String code, Integer statusCode,
       Throwable e) {
     super(message, requestId, code, statusCode, e);
