d4cf7de6c962521053443119743f6318a3297ba0
Steeve Pastorelli <spastorelli@stripe.com>
2018-06-28 14:33:42 +0100

    Fix account.application.* event data deserialization issue

---
+++ src/test/java/com/stripe/model/EventDataDeserializerTest.java
@@ -0,0 +1,26 @@
+package com.stripe.model;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.stripe.BaseStripeTest;
+import com.stripe.net.APIResource;
+
+import org.junit.Test;
+
+public class EventDataDeserializerTest extends BaseStripeTest {
+
+  @Test
+  public void testEventAccountApplicationDeauthorized() throws Exception {
+    final String data = getResourceAsString("/api_fixtures/account_application_deauthorized.json");
+    final Event event = APIResource.GSON.fromJson(data, Event.class);
+    assertNotNull(event);
+    assertNotNull(event.getId());
+    assertNotNull(event.getData());
+    assertNotNull(event.getData().getObject());
+
+    final Application application = (Application) event.getData().getObject();
+    assertNotNull(application);
+    assertNotNull(application.getId());
+  }
+
+}
