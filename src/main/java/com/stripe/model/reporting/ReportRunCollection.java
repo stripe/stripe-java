9a9b0d7901cb3bd32dd26bd9624befbf3829fc9c
Remi Jannel <remi@stripe.com>
2018-09-05 17:37:04 -0400

    Add support for the Reporting resources

---
+++ src/main/java/com/stripe/model/reporting/ReportRunCollection.java
@@ -0,0 +1,6 @@
+package com.stripe.model.reporting;
+
+import com.stripe.model.StripeCollection;
+
+public class ReportRunCollection extends StripeCollection<ReportRun> {
+}
---
+++ src/main/java/com/stripe/model/reporting/ReportTypeCollection.java
@@ -0,0 +1,6 @@
+package com.stripe.model.reporting;
+
+import com.stripe.model.StripeCollection;
+
+public class ReportTypeCollection extends StripeCollection<ReportType> {
+}
