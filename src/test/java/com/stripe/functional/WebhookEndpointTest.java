71aaa3072af9aedb14628c801bc77b9ab8eada2f
Remi Jannel <remi@stripe.com>
2018-10-17 15:08:29 -0700

    Add support for the Webhook Endpoint resource

---
+++ src/test/java/com/stripe/functional/WebhookEndpointTest.java
@@ -0,0 +1,105 @@
+package com.stripe.functional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.stripe.BaseStripeTest;
+import com.stripe.exception.StripeException;
+import com.stripe.model.WebhookEndpoint;
+import com.stripe.model.WebhookEndpointCollection;
+import com.stripe.net.ApiResource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class WebhookEndpointTest extends BaseStripeTest {
+  public static final String WEBHOOK_ENDPOINT_ID = "we_123";
+
+  private WebhookEndpoint getWebhookEndpointFixture() throws StripeException {
+    final WebhookEndpoint endpoint = WebhookEndpoint.retrieve(WEBHOOK_ENDPOINT_ID);
+    resetNetworkSpy();
+    return endpoint;
+  }
+
+  @Test
+  public void testCreate() throws StripeException {
+    final List<String> enabledEvents = new ArrayList<String>();
+    enabledEvents.add("charge.succeeded");
+
+    final Map<String, Object> params = new HashMap<String, Object>();
+    params.put("enabled_events", enabledEvents);
+    params.put("url", "https://stripe.com");
+
+    final WebhookEndpoint endpoint = WebhookEndpoint.create(params);
+
+    assertNotNull(endpoint);
+    verifyRequest(
+        ApiResource.RequestMethod.POST,
+        String.format("/v1/webhook_endpoints"),
+        params
+    );
+  }
+
+  @Test
+  public void testRetrieve() throws StripeException {
+    final WebhookEndpoint endpoint = WebhookEndpoint.retrieve(WEBHOOK_ENDPOINT_ID);
+
+    assertNotNull(endpoint);
+    verifyRequest(
+        ApiResource.RequestMethod.GET,
+        String.format("/v1/webhook_endpoints/%s", WEBHOOK_ENDPOINT_ID)
+    );
+  }
+
+  @Test
+  public void testUpdate() throws StripeException {
+    final WebhookEndpoint endpoint = getWebhookEndpointFixture();
+
+    final List<String> enabledEvents = new ArrayList<String>();
+    enabledEvents.add("charge.succeeded");
+
+    final Map<String, Object> params = new HashMap<String, Object>();
+    params.put("enabled_events", enabledEvents);
+
+    final WebhookEndpoint updatedWebhookEndpoint = endpoint.update(params);
+
+    assertNotNull(updatedWebhookEndpoint);
+    verifyRequest(
+        ApiResource.RequestMethod.POST,
+        String.format("/v1/webhook_endpoints/%s", endpoint.getId()),
+        params
+    );
+  }
+
+  @Test
+  public void testList() throws StripeException {
+    final Map<String, Object> params = new HashMap<String, Object>();
+    params.put("limit", 1);
+
+    final WebhookEndpointCollection endpoints = WebhookEndpoint.list(params);
+
+    assertNotNull(endpoints);
+    verifyRequest(
+        ApiResource.RequestMethod.GET,
+        String.format("/v1/webhook_endpoints")
+    );
+  }
+
+  @Test
+  public void testDelete() throws StripeException {
+    final WebhookEndpoint endpoint = getWebhookEndpointFixture();
+
+    final WebhookEndpoint deletedWebhookEndpoint = endpoint.delete();
+
+    assertNotNull(deletedWebhookEndpoint);
+    assertTrue(deletedWebhookEndpoint.getDeleted());
+    verifyRequest(
+        ApiResource.RequestMethod.DELETE,
+        String.format("/v1/webhook_endpoints/%s", endpoint.getId())
+    );
+  }
+}
