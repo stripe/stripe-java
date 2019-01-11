5778bc32c73285f221a3a429b70c7513c1ff1c99
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-01-11 14:15:59 -0800

    src/test: [version-pin] pin API_VERSION and update RequestOptions and EphemeralKey

--- src/main/java/com/stripe/net/RequestOptions.java
+++ src/main/java/com/stripe/net/RequestOptions.java
@@ -1,28 +1,39 @@
 package com.stripe.net;
 
 import com.stripe.Stripe;
+import java.util.Map;
 
 public class RequestOptions {
   public static RequestOptions getDefault() {
-    return new RequestOptions(Stripe.apiKey, Stripe.clientId, Stripe.apiVersion, null, null,
+    return new RequestOptions(Stripe.apiKey, Stripe.clientId, null, null, null,
         Stripe.getConnectTimeout(), Stripe.getReadTimeout());
   }
 
   private final String apiKey;
   private final String clientId;
-  private final String stripeVersion;
   private final String idempotencyKey;
   private final String stripeAccount;
+  /**
+   * Stripe version always set at {@link Stripe#API_VERSION}.
+   */
+  private final String stripeVersion = Stripe.API_VERSION;
+  /**
+   * Stripe version override when made on behalf of others. This can be used when the returned
+   * response will not be deserialized into the current classes pinned to {@link Stripe#VERSION}.
+   */
+  private final String stripeVersionOverride;
+
   private final int connectTimeout;
   private final int readTimeout;
 
-  private RequestOptions(String apiKey, String clientId, String stripeVersion,
-      String idempotencyKey, String stripeAccount, int connectTimeout, int readTimeout) {
+  private RequestOptions(String apiKey, String clientId, String idempotencyKey,
+                         String stripeAccount, String stripeVersionOverride,
+                         int connectTimeout, int readTimeout) {
     this.apiKey = apiKey;
     this.clientId = clientId;
-    this.stripeVersion = stripeVersion;
     this.idempotencyKey = idempotencyKey;
     this.stripeAccount = stripeAccount;
+    this.stripeVersionOverride = stripeVersionOverride;
     this.connectTimeout = connectTimeout;
     this.readTimeout = readTimeout;
   }
@@ -35,16 +46,20 @@ public class RequestOptions {
     return clientId;
   }
 
-  public String getStripeVersion() {
-    return stripeVersion;
-  }
-
   public String getIdempotencyKey() {
     return idempotencyKey;
   }
 
   public String getStripeAccount() {
     return stripeAccount;
+  }
+
+  public String getStripeVersion() {
+    return stripeVersion;
+  }
+
+  public String getStripeVersionOverride() {
+    return stripeVersionOverride;
   }
 
   public int getReadTimeout() {
@@ -72,12 +87,21 @@ public class RequestOptions {
     if (clientId != null ? !clientId.equals(that.clientId) : that.clientId != null) {
       return false;
     }
-    if (idempotencyKey != null ? !idempotencyKey.equals(that.idempotencyKey)
-        : that.idempotencyKey != null) {
+    if (idempotencyKey != null ? !idempotencyKey.equals(that.idempotencyKey) :
+        that.idempotencyKey != null) {
       return false;
     }
-    if (stripeVersion != null ? !stripeVersion.equals(that.stripeVersion)
-        : that.stripeVersion != null) {
+    if (stripeAccount != null ? !stripeAccount.equals(that.stripeAccount) :
+        that.stripeAccount != null) {
+      return false;
+    }
+    if (stripeVersion != null ? !stripeVersion.equals(that.stripeVersion) :
+        that.stripeVersion != null) {
+      return false;
+    }
+    if (stripeVersionOverride != null
+        ? !stripeVersionOverride.equals(that.stripeVersionOverride) :
+        that.stripeVersionOverride != null) {
       return false;
     }
 
@@ -92,10 +116,13 @@ public class RequestOptions {
   public int hashCode() {
     int result = apiKey != null ? apiKey.hashCode() : 0;
     result = 31 * result + (clientId != null ? clientId.hashCode() : 0);
-    result = 31 * result + (stripeVersion != null ? stripeVersion.hashCode() : 0);
     result = 31 * result + (idempotencyKey != null ? idempotencyKey.hashCode() : 0);
-    result = 31 * result + readTimeout;
+    result = 31 * result + (stripeAccount != null ? stripeAccount.hashCode() : 0);
+    result = 31 * result + (stripeVersion != null ? stripeVersion.hashCode() : 0);
+    result = 31 * result + (stripeVersionOverride != null ? stripeVersionOverride.hashCode()
+        : 0);
     result = 31 * result + connectTimeout;
+    result = 31 * result + readTimeout;
     return result;
   }
 
@@ -103,17 +130,21 @@ public class RequestOptions {
     return new RequestOptionsBuilder();
   }
 
+  /**
+   * Convert request options to builder, retaining invariant values for the integration.
+   * @return option builder.
+   */
   public RequestOptionsBuilder toBuilder() {
-    return new RequestOptionsBuilder().setApiKey(this.apiKey).setStripeVersion(this.stripeVersion)
-        .setStripeAccount(this.stripeAccount);
+    return new RequestOptionsBuilder()
+        .setApiKey(this.apiKey).setStripeAccount(this.stripeAccount);
   }
 
   public static final class RequestOptionsBuilder {
     private String apiKey;
     private String clientId;
-    private String stripeVersion;
     private String idempotencyKey;
     private String stripeAccount;
+    private String stripeVersionOverride;
     private int connectTimeout;
     private int readTimeout;
 
@@ -124,7 +155,6 @@ public class RequestOptions {
     public RequestOptionsBuilder() {
       this.apiKey = Stripe.apiKey;
       this.clientId = Stripe.clientId;
-      this.stripeVersion = Stripe.apiVersion;
     }
 
     public String getApiKey() {
@@ -152,16 +182,6 @@ public class RequestOptions {
 
     public RequestOptionsBuilder clearClientId() {
       this.clientId = null;
-      return this;
-    }
-
-    public RequestOptionsBuilder setStripeVersion(String stripeVersion) {
-      this.stripeVersion = normalizeStripeVersion(stripeVersion);
-      return this;
-    }
-
-    public RequestOptionsBuilder clearStripeVersion() {
-      this.stripeVersion = null;
       return this;
     }
 
@@ -226,6 +246,31 @@ public class RequestOptions {
       return setStripeAccount(null);
     }
 
+    public String getStripeVersionOverride() {
+      return this.stripeVersionOverride;
+    }
+
+    /**
+     * Do not use this except for in API where JSON response is not fully deserialized into
+     * to explicit Stripe classes, but only passed to other clients as raw data -- essentially
+     * making request on behalf of others with their API version. One example is in
+     * {@link com.stripe.model.EphemeralKey#create(Map, RequestOptions)}.
+     * Setting this value in a typical scenario will result in deserialization error
+     * as the model classes have schema according to the pinned {@link Stripe#API_VERSION} and not
+     * the {@code stripeVersionOverride}
+     * @param stripeVersionOverride stripe version override which belongs to the client to make
+     *                              request on behalf of.
+     * @return option builder
+     */
+    public RequestOptionsBuilder setStripeVersionOverride(String stripeVersionOverride) {
+      this.stripeVersionOverride = normalizeStripeVersion(stripeVersionOverride);
+      return this;
+    }
+
+    public RequestOptionsBuilder clearStripeVersionOverride() {
+      return setStripeVersionOverride(null);
+    }
+
     /**
      * Constructs a {@link RequestOptions} with the specified values.
      */
@@ -233,9 +278,9 @@ public class RequestOptions {
       return new RequestOptions(
           normalizeApiKey(this.apiKey),
           normalizeClientId(this.clientId),
-          normalizeStripeVersion(this.stripeVersion),
           normalizeIdempotencyKey(this.idempotencyKey),
           normalizeStripeAccount(this.stripeAccount),
+          normalizeStripeVersion(this.stripeVersionOverride),
           connectTimeout,
           readTimeout);
     }
