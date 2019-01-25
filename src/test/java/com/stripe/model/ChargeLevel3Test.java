d6ce1c19fecbc18989f18fc3f958e2727215b86c
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-01-24 16:25:52 -0800

    test: rename static inner Charge.Level3 to top-level Level3

--- src/test/java/com/stripe/model/ChargeLevel3Test.java
+++ src/test/java/com/stripe/model/ChargeLevel3Test.java
@@ -11,7 +11,7 @@ public class ChargeLevel3Test extends BaseStripeTest {
   @Test
   public void testDeserialize() throws Exception {
     final String data = getResourceAsString("/api_fixtures/charge_level3.json");
-    final Charge.Level3 object = ApiResource.GSON.fromJson(data, Charge.Level3.class);
+    final Level3 object = ApiResource.GSON.fromJson(data, Level3.class);
     assertEquals("1234", object.getMerchantReference());
     assertEquals(2, object.getLineItems().size());
   }
