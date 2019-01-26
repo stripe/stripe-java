d443a3345ff1e132e624502d9488ff332d1e96a5
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-01-26 14:30:16 -0800

    test: add deserialization test to OrderItem expanding parent

--- src/test/java/com/stripe/model/OrderItemTest.java
+++ src/test/java/com/stripe/model/OrderItemTest.java
@@ -2,10 +2,14 @@ package com.stripe.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import com.google.gson.reflect.TypeToken;
 import com.stripe.BaseStripeTest;
 import com.stripe.net.ApiResource;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import org.junit.Test;
 
 public class OrderItemTest extends BaseStripeTest {
@@ -17,5 +21,27 @@ public class OrderItemTest extends BaseStripeTest {
     final OrderItem orderItem = ApiResource.GSON.fromJson(itemData, OrderItem.class);
     assertNotNull(orderItem);
     assertEquals("order_item", orderItem.getObject());
+  }
+
+  @Test
+  public void testDeserializeWithExpansions() throws Exception {
+    final String orderItemsData = getResourceAsString(
+        "/api_fixtures/order_item_parent_expansions.json");
+    Type type = new TypeToken<List<OrderItem>>(){}.getType();
+    final List<OrderItem> orderItemList = ApiResource.GSON.fromJson(orderItemsData, type);
+    for (int i = 0; i < orderItemList.size(); i++) {
+      final OrderItem orderItem = orderItemList.get(i);
+      assertNotNull(orderItem);
+      assertEquals("order_item", orderItem.getObject());
+
+      // only SKU can be expanded
+      if (orderItem.getParent() != null && orderItem.getParent().startsWith("sku_")) {
+        assertNotNull(orderItem.getParentObject());
+        Sku sku = orderItem.getParentObject();
+        assertEquals(orderItem.getParent(), sku.getId());
+      } else {
+        assertNull(orderItem.getParentObject());
+      }
+    }
   }
 }
