32b4b2af6e8ecd1776d93ee099741ac8c6782afc
mickjermsurawong-stripe <42550941+mickjermsurawong-stripe@users.noreply.github.com>
2019-02-01 09:48:31 -0800

    Interface for handling event deserialization failure due to api version mismatch (#664)
    * make event data backward compat
    
    * implement versioned data
    
    * rename work
    
    * rename exception
    
    * improve deserializeUnsafeWith
    
    * add comments using static pinned version

--- src/main/java/com/stripe/model/EventData.java
+++ src/main/java/com/stripe/model/EventData.java
@@ -1,7 +1,8 @@
 package com.stripe.model;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import java.util.Map;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,21 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class EventData extends StripeObject {
-  StripeObject object;
+  /**
+   * Raw JSON object intended to be deserialized as {@code StripeObject}. The deserialization
+   * should be deferred to the user. See the now deprecated method {@link EventData#getObject()}.
+   */
+  JsonObject object;
   Map<String, Object> previousAttributes;
+
+  /**
+   * Deprecated in favor of getting {@code StripeObject} from {@link EventDataObjectDeserializer}.
+   * Throws {@link JsonParseException} deserialization failure due to general invalid JSON,
+   * or more specifically when JSON data and model class have incompatible schemas.
+   * @return deserialized stripe object for event data.
+   */
+  @Deprecated
+  public StripeObject getObject() {
+    return EventDataDeserializer.deserializeStripeObject(object);
+  }
 }
