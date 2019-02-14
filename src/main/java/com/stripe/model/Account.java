5deaadc5c4969ee00367c5cd267d8671a896087f
mickjermsurawong-stripe <42550941+mickjermsurawong-stripe@users.noreply.github.com>
2019-02-13 20:17:07 -0800

    [generated] source: spec3.sdk.yaml@non-master-spec-d2b863d in mickjermsurawong/subscription-schedules (#672)

--- src/main/java/com/stripe/model/Account.java
+++ src/main/java/com/stripe/model/Account.java
@@ -38,6 +38,14 @@ public class Account extends ApiResource implements PaymentSource, MetadataStore
   @Setter(lombok.AccessLevel.NONE)
   ExpandableField<File> businessLogo;
 
+  /**
+   * (ID of a [file upload](https://stripe.com/docs/guides/file-upload)) A larger logo for the
+   * account that will be used in Checkout instead of the smaller logo and without the account's
+   * name next to it if provided. Must be at least 128px x 128px.
+   */
+  @SerializedName("business_logo_large")
+  String businessLogoLarge;
+
   /** The publicly visible name of the business. */
   @SerializedName("business_name")
   String businessName;
@@ -221,34 +229,34 @@ public class Account extends ApiResource implements PaymentSource, MetadataStore
     this.businessLogo = new ExpandableField<File>(expandableObject.getId(), expandableObject);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve() throws StripeException {
     return retrieve((Map<String, Object>) null, (RequestOptions) null);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve(RequestOptions options) throws StripeException {
     return retrieve((Map<String, Object>) null, options);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve(Map<String, Object> params, RequestOptions options)
       throws StripeException {
     String url = String.format("%s%s", Stripe.getApiBase(), "/v1/account");
     return request(ApiResource.RequestMethod.GET, url, params, Account.class, options);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve(String account) throws StripeException {
     return retrieve(account, (Map<String, Object>) null, (RequestOptions) null);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve(String account, RequestOptions options) throws StripeException {
     return retrieve(account, (Map<String, Object>) null, options);
   }
 
-  /** Retrieves the details of the account. */
+  /** Retrieves the details of an account. */
   public static Account retrieve(String account, Map<String, Object> params, RequestOptions options)
       throws StripeException {
     String url =
