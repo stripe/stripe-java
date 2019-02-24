b0e7a3d8485ce9d4016a863016a01c1590fd387b
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-02-23 19:58:13 -0800

    test: typed params create charge
    test: typed params charge

--- src/test/java/com/stripe/functional/ChargeTest.java
+++ src/test/java/com/stripe/functional/ChargeTest.java
@@ -11,6 +11,7 @@ import com.stripe.model.ChargeCollection;
 import com.stripe.net.ApiResource;
 
 import com.stripe.net.RequestOptions;
+import com.stripe.param.ChargeCreateParams;
 import com.stripe.param.ChargeUpdateParams;
 import java.util.HashMap;
 import java.util.Map;
@@ -40,6 +41,24 @@ public class ChargeTest extends BaseStripeTest {
         ApiResource.RequestMethod.POST,
         "/v1/charges",
         params
+    );
+  }
+
+  @Test
+  public void testCreateWithTypedParams() throws StripeException {
+    ChargeCreateParams params = ChargeCreateParams.builder()
+        .setSource("card_123")
+        .setAmount(100L)
+        .setCurrency("usd")
+        .build();
+
+    final Charge charge = Charge.create(params, RequestOptions.getDefault());
+
+    assertNotNull(charge);
+    verifyRequest(
+        ApiResource.RequestMethod.POST,
+        "/v1/charges",
+        params.toMap()
     );
   }
 
