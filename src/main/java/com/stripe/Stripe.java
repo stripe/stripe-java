e56807ee60da7319431a4cf79e0e0bbefc93a131
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-03-19 12:50:36 -0700

    remove parts that should be done by script

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -10,7 +10,7 @@ public abstract class Stripe {
   private static final int DEFAULT_CONNECT_TIMEOUT = 30 * 1000;
   private static final int DEFAULT_READ_TIMEOUT = 80 * 1000;
 
-  public static final String API_VERSION = "2018-11-08";
+  public static final String API_VERSION = "2019-03-14";
   public static final String CONNECT_API_BASE = "https://connect.stripe.com";
   public static final String LIVE_API_BASE = "https://api.stripe.com";
   public static final String UPLOAD_API_BASE = "https://files.stripe.com";
