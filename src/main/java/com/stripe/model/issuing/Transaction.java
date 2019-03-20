a6778a5abd318d2a7fe82ab20bf6331f96376760
Alexey Kalinichenko <akalini@gmail.com>
2019-03-19 17:03:13 -0700

    Adds settlement field.

--- src/main/java/com/stripe/model/issuing/Transaction.java
+++ src/main/java/com/stripe/model/issuing/Transaction.java
@@ -98,6 +98,8 @@ public class Transaction extends ApiResource
   @SerializedName("merchant_data")
   Authorization.MerchantData merchantData;
 
+  String settlement;
+
   /**
    * Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach
    * to an object. This can be useful for storing additional information about the object in a
