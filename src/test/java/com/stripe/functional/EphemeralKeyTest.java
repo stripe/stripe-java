6129ace5ac58b18aecc253cbccce5f71ac01a6c8
mickjermsurawong-stripe <42550941+mickjermsurawong-stripe@users.noreply.github.com>
2019-03-20 10:51:51 -0700

    Pass version check flag to stripe-mock (#697)
    * add check stripemock strict version
    
    * pass pinned version to comply with stripe-mock version check
    
    * be non-strict for custom openapi spec

--- src/test/java/com/stripe/functional/EphemeralKeyTest.java
+++ src/test/java/com/stripe/functional/EphemeralKeyTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import com.stripe.BaseStripeTest;
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.EphemeralKey;
 import com.stripe.net.ApiResource;
@@ -21,8 +22,11 @@ public class EphemeralKeyTest extends BaseStripeTest {
     final Map<String, Object> params = new HashMap<>();
     params.put("customer", "cus_123");
 
+    // In actual usage, version override is likely different from the pinned API version.
+    // Passing the same API version here to comply with stripe-mock `-strict-version-check`
+    // flag--it errors on passing API version different from that in OpenAPI spec.
     final RequestOptions options = RequestOptions.builder()
-        .setStripeVersionOverride("2017-05-25").build();
+        .setStripeVersionOverride(Stripe.API_VERSION).build();
 
     final EphemeralKey key = EphemeralKey.create(params, options);
 
@@ -52,8 +56,10 @@ public class EphemeralKeyTest extends BaseStripeTest {
     final Map<String, Object> params = new HashMap<>();
     params.put("customer", "cus_123");
 
+    // Passing the same API version here to comply with stripe-mock `-strict-version-check`
+    // flag--it errors on passing API version different from that in OpenAPI spec.
     final RequestOptions options = RequestOptions.builder()
-        .setStripeVersionOverride("2017-05-25").build();
+        .setStripeVersionOverride(Stripe.API_VERSION).build();
 
     final EphemeralKey key = EphemeralKey.create(params, options);
 
