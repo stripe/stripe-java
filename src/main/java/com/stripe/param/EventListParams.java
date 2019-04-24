f8f8b6061d45c01919915a5a279693306dc51786
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-04-24 00:33:00 -0700

    event params
    update event param

--- src/main/java/com/stripe/param/EventListParams.java
+++ src/main/java/com/stripe/param/EventListParams.java
@@ -3,7 +3,9 @@ package com.stripe.param;
 import com.google.gson.annotations.SerializedName;
 import com.stripe.net.ApiRequestParams;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EventListParams extends ApiRequestParams {
   @SerializedName("created")
@@ -28,6 +30,15 @@ public class EventListParams extends ApiRequestParams {
   /** Specifies which fields in the response should be expanded. */
   @SerializedName("expand")
   List<String> expand;
+
+  /**
+   * Map of extra parameters for custom features not available in this client library. The content
+   * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+   * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+   * param object. Effectively, this map is flattened to its parent instance.
+   */
+  @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+  Map<String, Object> extraParams;
 
   /**
    * A limit on the number of objects to be returned. Limit can range between 1 and 100, and the
@@ -65,6 +76,7 @@ public class EventListParams extends ApiRequestParams {
       Boolean deliverySuccess,
       String endingBefore,
       List<String> expand,
+      Map<String, Object> extraParams,
       Long limit,
       String startingAfter,
       String type,
@@ -73,6 +85,7 @@ public class EventListParams extends ApiRequestParams {
     this.deliverySuccess = deliverySuccess;
     this.endingBefore = endingBefore;
     this.expand = expand;
+    this.extraParams = extraParams;
     this.limit = limit;
     this.startingAfter = startingAfter;
     this.type = type;
@@ -92,6 +105,8 @@ public class EventListParams extends ApiRequestParams {
 
     private List<String> expand;
 
+    private Map<String, Object> extraParams;
+
     private Long limit;
 
     private String startingAfter;
@@ -107,6 +122,7 @@ public class EventListParams extends ApiRequestParams {
           this.deliverySuccess,
           this.endingBefore,
           this.expand,
+          this.extraParams,
           this.limit,
           this.startingAfter,
           this.type,
@@ -170,6 +186,32 @@ public class EventListParams extends ApiRequestParams {
     }
 
     /**
+     * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+     * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+     * EventListParams#extraParams} for the field documentation.
+     */
+    public Builder putExtraParam(String key, Object value) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.put(key, value);
+      return this;
+    }
+
+    /**
+     * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+     * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+     * See {@link EventListParams#extraParams} for the field documentation.
+     */
+    public Builder putAllExtraParam(Map<String, Object> map) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.putAll(map);
+      return this;
+    }
+
+    /**
      * A limit on the number of objects to be returned. Limit can range between 1 and 100, and the
      * default is 10.
      */
@@ -226,6 +268,15 @@ public class EventListParams extends ApiRequestParams {
   }
 
   public static class Created {
+    /**
+     * Map of extra parameters for custom features not available in this client library. The content
+     * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+     * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+     * param object. Effectively, this map is flattened to its parent instance.
+     */
+    @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+    Map<String, Object> extraParams;
+
     /** Minimum value to filter by (exclusive). */
     @SerializedName("gt")
     Long gt;
@@ -242,7 +293,8 @@ public class EventListParams extends ApiRequestParams {
     @SerializedName("lte")
     Long lte;
 
-    private Created(Long gt, Long gte, Long lt, Long lte) {
+    private Created(Map<String, Object> extraParams, Long gt, Long gte, Long lt, Long lte) {
+      this.extraParams = extraParams;
       this.gt = gt;
       this.gte = gte;
       this.lt = lt;
@@ -254,6 +306,8 @@ public class EventListParams extends ApiRequestParams {
     }
 
     public static class Builder {
+      private Map<String, Object> extraParams;
+
       private Long gt;
 
       private Long gte;
@@ -264,7 +318,33 @@ public class EventListParams extends ApiRequestParams {
 
       /** Finalize and obtain parameter instance from this builder. */
       public Created build() {
-        return new Created(this.gt, this.gte, this.lt, this.lte);
+        return new Created(this.extraParams, this.gt, this.gte, this.lt, this.lte);
+      }
+
+      /**
+       * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+       * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+       * EventListParams.Created#extraParams} for the field documentation.
+       */
+      public Builder putExtraParam(String key, Object value) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.put(key, value);
+        return this;
+      }
+
+      /**
+       * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+       * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+       * See {@link EventListParams.Created#extraParams} for the field documentation.
+       */
+      public Builder putAllExtraParam(Map<String, Object> map) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.putAll(map);
+        return this;
       }
 
       /** Minimum value to filter by (exclusive). */
