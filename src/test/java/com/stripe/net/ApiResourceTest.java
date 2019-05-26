87284d012e71a05cb8340bc7fd52e9791434e98f
Mick Jermsurawong <mickjermsurawong@stripe.com>
2019-05-26 08:35:06 -0700

    check null in id encoding

--- src/test/java/com/stripe/net/ApiResourceTest.java
+++ src/test/java/com/stripe/net/ApiResourceTest.java
@@ -1,16 +1,27 @@
 package com.stripe.net;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.stripe.exception.InvalidRequestException;
 
 import java.io.UnsupportedEncodingException;
+
 import org.junit.jupiter.api.Test;
 
 class ApiResourceTest {
 
   @Test
-  public void testUrlEncodeId() throws UnsupportedEncodingException {
+  public void testUrlEncode() throws UnsupportedEncodingException {
     assertEquals("cus_123", ApiResource.urlEncode("cus_123"));
     // legacy Ids allow customer-defined and can have arbitrary names
     assertEquals("Plan+100%24%2Fmonth", ApiResource.urlEncode("Plan 100$/month"));
+  }
+
+  @Test
+  public void testUrlEncodeIdThrowingOnNull() {
+    assertThrows(InvalidRequestException.class, () -> {
+      ApiResource.urlEncodeId(null);
+    });
   }
 }
