3f6bed3a3de45bf4b964200ebbba101ba4bf3325
Brandur <brandur@brandur.org>
2019-05-29 15:17:42 -0700

    Make headers access case insensitive
    Makes access to header names case insensitive so that we can easily
    support HTTP/2 where the convention is to have downcased header names.
    This allows us to access something like `Request-Id` regardless of the
    backend protocol that's in use.

--- src/main/java/com/stripe/net/StripeHeaders.java
+++ src/main/java/com/stripe/net/StripeHeaders.java
@@ -1,14 +1,34 @@
 package com.stripe.net;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StripeHeaders {
 
-  Map<String, List<String>> headers;
+  Map<String, List<String>> headers = new HashMap<String, List<String>>();
 
+  /**
+   * Constructs a collection of headers from the given map.
+   */
   public StripeHeaders(Map<String, List<String>> headers) {
-    this.headers = headers;
+    // Downcase all header names so that we can easily and efficiently perform
+    // case-insensitive lookups.
+    //
+    // This is a general convenience feature, but is particularly important for
+    // HTTP/2 where header names are downcased and looking up something like
+    // `Request-Id` would not otherwise work.
+    for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+      String key = entry.getKey() != null ? entry.getKey().toLowerCase() : null;
+      if (this.headers.containsKey(key)) {
+        throw new IllegalArgumentException(String.format(
+        "Header map contained key `%s` multiple times with varying casing",
+        entry.getKey()
+      ));
+      }
+
+      this.headers.put(key, entry.getValue());
+    }
   }
 
   /**
@@ -26,7 +46,7 @@ public class StripeHeaders {
   }
 
   public List<String> values(String name) {
-    return headers == null ? null : headers.get(name);
+    return headers == null ? null : headers.get(name.toLowerCase());
   }
 
 }
