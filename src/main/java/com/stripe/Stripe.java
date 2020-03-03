ad14b380248f7af544f388ed7e2959cc60a7108a
Remi Jannel <remi@stripe.com>
2020-03-03 09:39:59 -0800

    Move to API version 2020-03-02 and related API changes

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -9,7 +9,7 @@ public abstract class Stripe {
   public static final int DEFAULT_CONNECT_TIMEOUT = 30 * 1000;
   public static final int DEFAULT_READ_TIMEOUT = 80 * 1000;
 
-  public static final String API_VERSION = "2019-12-03";
+  public static final String API_VERSION = "2020-03-02";
   public static final String CONNECT_API_BASE = "https://connect.stripe.com";
   public static final String LIVE_API_BASE = "https://api.stripe.com";
   public static final String UPLOAD_API_BASE = "https://files.stripe.com";
