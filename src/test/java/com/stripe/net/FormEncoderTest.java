df9b2fc1d1564a56058daa75484f8f23f1f0f6c6
Olivier Bellone <ob@stripe.com>
2020-10-21 16:27:42 -0700

    Fix deprecation warning in test file (#1135)

--- src/test/java/com/stripe/net/FormEncoderTest.java
+++ src/test/java/com/stripe/net/FormEncoderTest.java
@@ -1,6 +1,6 @@
 package com.stripe.net;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
