5b77d61275b4a3ec34955deaaf1ac27ff0d85fe1
Remi Jannel <remi@stripe.com>
2020-10-27 11:33:19 -0700

    Codegen for openapi 9f2a7bc

--- src/main/java/com/stripe/param/PaymentIntentConfirmParams.java
+++ src/main/java/com/stripe/param/PaymentIntentConfirmParams.java
@@ -2679,6 +2679,13 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
     Object oxxo;
 
     /**
+     * If this is a {@code p24} PaymentMethod, this sub-hash contains details about the Przelewy24
+     * payment method options.
+     */
+    @SerializedName("p24")
+    Object p24;
+
+    /**
      * If this is a {@code sofort} PaymentMethod, this sub-hash contains details about the SOFORT
      * payment method options.
      */
@@ -2691,12 +2698,14 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
         Object card,
         Map<String, Object> extraParams,
         Object oxxo,
+        Object p24,
         Object sofort) {
       this.alipay = alipay;
       this.bancontact = bancontact;
       this.card = card;
       this.extraParams = extraParams;
       this.oxxo = oxxo;
+      this.p24 = p24;
       this.sofort = sofort;
     }
 
@@ -2715,12 +2724,20 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
 
       private Object oxxo;
 
+      private Object p24;
+
       private Object sofort;
 
       /** Finalize and obtain parameter instance from this builder. */
       public PaymentMethodOptions build() {
         return new PaymentMethodOptions(
-            this.alipay, this.bancontact, this.card, this.extraParams, this.oxxo, this.sofort);
+            this.alipay,
+            this.bancontact,
+            this.card,
+            this.extraParams,
+            this.oxxo,
+            this.p24,
+            this.sofort);
       }
 
       /**
@@ -2813,6 +2830,24 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
        */
       public Builder setOxxo(EmptyParam oxxo) {
         this.oxxo = oxxo;
+        return this;
+      }
+
+      /**
+       * If this is a {@code p24} PaymentMethod, this sub-hash contains details about the Przelewy24
+       * payment method options.
+       */
+      public Builder setP24(P24 p24) {
+        this.p24 = p24;
+        return this;
+      }
+
+      /**
+       * If this is a {@code p24} PaymentMethod, this sub-hash contains details about the Przelewy24
+       * payment method options.
+       */
+      public Builder setP24(EmptyParam p24) {
+        this.p24 = p24;
         return this;
       }
 
@@ -3538,6 +3573,63 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
          * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
          * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
          * map. See {@link PaymentIntentConfirmParams.PaymentMethodOptions.Oxxo#extraParams} for the
+         * field documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+      }
+    }
+
+    @Getter
+    public static class P24 {
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      private P24(Map<String, Object> extraParams) {
+        this.extraParams = extraParams;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private Map<String, Object> extraParams;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public P24 build() {
+          return new P24(this.extraParams);
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link PaymentIntentConfirmParams.PaymentMethodOptions.P24#extraParams} for the
+         * field documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link PaymentIntentConfirmParams.PaymentMethodOptions.P24#extraParams} for the
          * field documentation.
          */
         public Builder putAllExtraParam(Map<String, Object> map) {
