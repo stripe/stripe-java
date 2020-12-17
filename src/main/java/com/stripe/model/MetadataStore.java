677f8d923519caf3c07733fd447298a8747a0cf4
Christophe Levesque <christophe.levesque@gmail.com>
2020-12-17 09:57:47 -0800

    MetadataStore extends HasId

--- src/main/java/com/stripe/model/MetadataStore.java
+++ src/main/java/com/stripe/model/MetadataStore.java
@@ -5,11 +5,10 @@ import com.stripe.net.RequestOptions;
 import java.util.Map;
 
 /** Common interface for Stripe objects that can store metadata. */
-public interface MetadataStore<T> {
+public interface MetadataStore<T extends MetadataStore<T>> extends HasId {
   Map<String, String> getMetadata();
 
-  MetadataStore<T> update(Map<String, Object> params) throws StripeException;
+  T update(Map<String, Object> params) throws StripeException;
 
-  MetadataStore<T> update(Map<String, Object> params, RequestOptions options)
-      throws StripeException;
+  T update(Map<String, Object> params, RequestOptions options) throws StripeException;
 }
