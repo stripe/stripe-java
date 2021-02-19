65f1e027871fb7cbe30f54c18c8c5fcc35eee737
Richard Marmorstein <richardm@stripe.com>
2021-02-19 18:12:17 -0500

    Tests

--- src/test/java/com/stripe/functional/billingportal/ConfigurationTest.java
+++ src/test/java/com/stripe/functional/billingportal/ConfigurationTest.java
@@ -5,14 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.stripe.BaseStripeTest;
 import com.stripe.exception.StripeException;
 import com.stripe.model.billingportal.Configuration;
+import com.stripe.model.billingportal.ConfigurationCollection;
 import com.stripe.net.ApiResource;
 import com.stripe.param.billingportal.ConfigurationCreateParams;
+import com.stripe.param.billingportal.ConfigurationUpdateParams;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class ConfigurationTest extends BaseStripeTest {
   public static final String SESSION_ID = "bps_123";
+
+  private Configuration getConfigurationFixture() throws StripeException {
+    final Configuration configuration = Configuration.retrieve(SESSION_ID);
+    resetNetworkSpy();
+    return configuration;
+  }
 
   @Test
   public void testCreate() throws StripeException {
@@ -44,5 +54,42 @@ public class ConfigurationTest extends BaseStripeTest {
         ApiResource.RequestMethod.POST,
         String.format("/v1/billing_portal/configurations"),
         params.toMap());
+  }
+
+  @Test
+  public void testRetrieve() throws StripeException {
+    final Configuration configuration = Configuration.retrieve(SESSION_ID);
+
+    assertNotNull(configuration);
+    verifyRequest(
+        ApiResource.RequestMethod.GET,
+        String.format("/v1/billing_portal/configurations/%s", configuration.getId()));
+  }
+
+  @Test
+  public void testUpdate() throws StripeException {
+    final Configuration configuration = getConfigurationFixture();
+
+    ConfigurationUpdateParams params = ConfigurationUpdateParams.builder().setActive(false).build();
+
+    final Configuration updatedConfiguration = configuration.update(params);
+
+    assertNotNull(updatedConfiguration);
+    verifyRequest(
+        ApiResource.RequestMethod.POST,
+        String.format("/v1/billing_portal/configurations/%s", configuration.getId()),
+        params.toMap());
+  }
+
+  @Test
+  public void testList() throws StripeException {
+    final Map<String, Object> params = new HashMap<>();
+    params.put("limit", 1);
+
+    final ConfigurationCollection coupons = Configuration.list(params);
+
+    assertNotNull(coupons);
+    verifyRequest(
+        ApiResource.RequestMethod.GET, String.format("/v1/billing_portal/configurations"));
   }
 }
