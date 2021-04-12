66c98a1e32ab5f47830a787466baed59c0187852
Richard Marmorstein <richardm@stripe.com>
2021-04-12 14:12:20 -0400

    Add currency

--- src/main/java/com/stripe/model/checkout/Session.java
+++ src/main/java/com/stripe/model/checkout/Session.java
@@ -473,6 +473,14 @@ public class Session extends ApiResource implements HasId {
       @SerializedName("verification_method")
       String verificationMethod;
 
+      /**
+       * Three-letter <a href="https://www.iso.org/iso-4217-currency-codes.html">ISO currency
+       * code</a>, in lowercase. Must be a <a href="https://stripe.com/docs/currencies">supported
+       * currency</a>.
+       */
+      @SerializedName("currency")
+      String currency;
+
       @Getter
       @Setter
       @EqualsAndHashCode(callSuper = false)
