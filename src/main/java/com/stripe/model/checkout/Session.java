a7817e45b6d716ab96826b070d0ed447310d5563
Richard Marmorstein <richardm@stripe.com>
2021-04-16 12:25:53 -0400

    Codegen for openapi 6546d96

--- src/main/java/com/stripe/model/checkout/Session.java
+++ src/main/java/com/stripe/model/checkout/Session.java
@@ -473,11 +473,6 @@ public class Session extends ApiResource implements HasId {
       @SerializedName("verification_method")
       String verificationMethod;
 
-      /**
-       * Three-letter <a href="https://www.iso.org/iso-4217-currency-codes.html">ISO currency
-       * code</a>, in lowercase. Must be a <a href="https://stripe.com/docs/currencies">supported
-       * currency</a>.
-       */
       @SerializedName("currency")
       String currency;
 
