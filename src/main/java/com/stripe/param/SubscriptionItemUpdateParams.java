0fca4f3cca1e8a18cbd9ef1446234a59e62d729e
Richard Marmorstein <richardm@stripe.com>
2021-07-27 12:57:41 -0400

    Codegen for openapi 1d88c6a

--- src/main/java/com/stripe/param/SubscriptionItemUpdateParams.java
+++ src/main/java/com/stripe/param/SubscriptionItemUpdateParams.java
@@ -81,7 +81,10 @@ public class SubscriptionItemUpdateParams extends ApiRequestParams {
   @SerializedName("plan")
   Object plan;
 
-  /** The ID of the price object. */
+  /**
+   * The ID of the price object. When changing a subscription item's price, {@code quantity} is set
+   * to 1 unless a {@code quantity} parameter is provided.
+   */
   @SerializedName("price")
   Object price;
 
@@ -381,13 +384,19 @@ public class SubscriptionItemUpdateParams extends ApiRequestParams {
       return this;
     }
 
-    /** The ID of the price object. */
+    /**
+     * The ID of the price object. When changing a subscription item's price, {@code quantity} is
+     * set to 1 unless a {@code quantity} parameter is provided.
+     */
     public Builder setPrice(String price) {
       this.price = price;
       return this;
     }
 
-    /** The ID of the price object. */
+    /**
+     * The ID of the price object. When changing a subscription item's price, {@code quantity} is
+     * set to 1 unless a {@code quantity} parameter is provided.
+     */
     public Builder setPrice(EmptyParam price) {
       this.price = price;
       return this;
