58caf82d6d24d3684de04161877f1abadde30c96
Dominic Charley-Roy <dcr@stripe.com>
2021-07-29 16:31:48 -0400

    Codegen for openapi 007d5f7

--- src/main/java/com/stripe/model/PaymentIntent.java
+++ src/main/java/com/stripe/model/PaymentIntent.java
@@ -1334,8 +1334,8 @@ public class PaymentIntent extends ApiResource implements HasId, MetadataStore<P
         String customMandateUrl;
 
         /**
-         * Description of the interval. Only required if 'payment_schedule' parmeter is 'interval'
-         * or 'combined'.
+         * Description of the interval. Only required if the 'payment_schedule' parameter is
+         * 'interval' or 'combined'.
          */
         @SerializedName("interval_description")
         String intervalDescription;
