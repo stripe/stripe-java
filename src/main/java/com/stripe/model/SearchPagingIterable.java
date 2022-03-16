f1d24dd0c3c663968c0d5141d591c7b25de5dad9
Pavel Krymets <pakrym@stripe.com>
2022-03-16 13:23:33 -0700

    Update search results types

--- src/main/java/com/stripe/model/SearchPagingIterable.java
+++ src/main/java/com/stripe/model/SearchPagingIterable.java
@@ -5,8 +5,6 @@ import java.util.Iterator;
 /**
  * Provides an <code>{@code Iterable<T>}</code> target that automatically iterates across all API
  * pages and which is suitable for use with a <code>{@code foreach}</code> loop.
- *
- * <p>Please note SearchPagingIterable is in beta and is subject to change or removal at any time.
  */
 public class SearchPagingIterable<T> implements Iterable<T> {
   private StripeSearchResultInterface<T> page;
