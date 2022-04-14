739fb4f45b4dd9593388ac7e655bbaa00b545c10
Remi Jannel <remi@stripe.com>
2022-04-13 18:50:15 -0700

    Add support for putExtraParam on EphemeralKeyCreateParams

--- src/main/java/com/stripe/param/EphemeralKeyCreateParams.java
+++ src/main/java/com/stripe/param/EphemeralKeyCreateParams.java
@@ -3,7 +3,9 @@ package com.stripe.param;
 import com.google.gson.annotations.SerializedName;
 import com.stripe.net.ApiRequestParams;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EphemeralKeyCreateParams extends ApiRequestParams {
   /** The ID of the Customer you'd like to modify using the resulting ephemeral key. */
@@ -14,13 +16,24 @@ public class EphemeralKeyCreateParams extends ApiRequestParams {
   @SerializedName("expand")
   List<String> expand;
 
+  /**
+   * Map of extra parameters for custom features not available in this client library. The content
+   * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+   * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+   * param object. Effectively, this map is flattened to its parent instance.
+   */
+  @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+  Map<String, Object> extraParams;
+
   /** The ID of the Issuing Card you'd like to access using the resulting ephemeral key. */
   @SerializedName("issuing_card")
   String issuingCard;
 
-  private EphemeralKeyCreateParams(String customer, List<String> expand, String issuingCard) {
+  private EphemeralKeyCreateParams(
+      String customer, List<String> expand, Map<String, Object> extraParams, String issuingCard) {
     this.customer = customer;
     this.expand = expand;
+    this.extraParams = extraParams;
     this.issuingCard = issuingCard;
   }
 
@@ -33,16 +46,45 @@ public class EphemeralKeyCreateParams extends ApiRequestParams {
 
     private List<String> expand;
 
+    private Map<String, Object> extraParams;
+
     private String issuingCard;
 
     /** Finalize and obtain parameter instance from this builder. */
     public EphemeralKeyCreateParams build() {
-      return new EphemeralKeyCreateParams(this.customer, this.expand, this.issuingCard);
+      return new EphemeralKeyCreateParams(
+          this.customer, this.expand, this.extraParams, this.issuingCard);
     }
 
     /** The ID of the Customer you'd like to modify using the resulting ephemeral key. */
     public Builder setCustomer(String customer) {
       this.customer = customer;
+      return this;
+    }
+
+    /**
+     * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+     * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+     * EphemeralKeyCreateParams#extraParams} for the field documentation.
+     */
+    public Builder putExtraParam(String key, Object value) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.put(key, value);
+      return this;
+    }
+
+    /**
+     * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+     * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+     * See {@link EphemeralKeyCreateParams#extraParams} for the field documentation.
+     */
+    public Builder putAllExtraParam(Map<String, Object> map) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.putAll(map);
       return this;
     }
 
