a270cb30063d2319503a6ede44dbd15dece12155
pakrym-stripe <99349468+pakrym-stripe@users.noreply.github.com>
2022-06-21 13:45:19 -0700

    Use the generated API version (#1377)

--- src/main/java/com/stripe/Stripe.java
+++ src/main/java/com/stripe/Stripe.java
@@ -9,7 +9,7 @@ public abstract class Stripe {
   public static final int DEFAULT_CONNECT_TIMEOUT = 30 * 1000;
   public static final int DEFAULT_READ_TIMEOUT = 80 * 1000;
 
-  public static final String API_VERSION = "2020-08-27";
+  public static final String API_VERSION = ApiVersion.CURRENT;
   public static final String CONNECT_API_BASE = "https://connect.stripe.com";
   public static final String LIVE_API_BASE = "https://api.stripe.com";
   public static final String UPLOAD_API_BASE = "https://files.stripe.com";
