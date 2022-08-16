3e430d48a05b9917dff9d7c390da92fb0c665bde
Kamil Pajdzik <kamil@stripe.com>
2022-08-16 08:58:54 -0700

    Fix latest JAR hyperlink and related tests

--- src/test/java/com/stripe/DocumentationTest.java
+++ src/test/java/com/stripe/DocumentationTest.java
@@ -90,7 +90,7 @@ public class DocumentationTest {
     try (final BufferedReader reader =
         new BufferedReader(
             new InputStreamReader(new FileInputStream(readmeFile), StandardCharsets.UTF_8))) {
-      final int expectedMentionsOfVersion = 2;
+      final int expectedMentionsOfVersion = 3;
       // Currently two places mention the Stripe version: the sample pom and gradle files.
       final List<String> mentioningLines = new ArrayList<String>();
       String line;
