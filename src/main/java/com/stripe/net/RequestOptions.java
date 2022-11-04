64b24289933a642cb3561cf63a211e1071c5e3a3
Richard Marmorstein <richardm@stripe.com>
2022-11-04 15:54:40 -0700

    Format

--- src/main/java/com/stripe/net/RequestOptions.java
+++ src/main/java/com/stripe/net/RequestOptions.java
@@ -291,7 +291,7 @@ public class RequestOptions {
     }
 
     /** For internal use only. */
-   String getStripeVersionOverride() {
+    String getStripeVersionOverride() {
       return this.stripeVersionOverride;
     }
 
