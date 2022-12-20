e0f69fabfb0c5160dd5142c56bc7f96f9333712e
Richard Marmorstein <richardm@stripe.com>
2022-12-20 14:14:18 -0600

    Add test

--- src/test/java/com/stripe/net/ApiResourceTest.java
+++ src/test/java/com/stripe/net/ApiResourceTest.java
@@ -2,8 +2,11 @@ package com.stripe.net;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.gson.JsonIOException;
 import com.stripe.exception.InvalidRequestException;
+import java.net.Proxy;
 import org.junit.jupiter.api.Test;
 
 class ApiResourceTest {
@@ -22,5 +25,18 @@ class ApiResourceTest {
         () -> {
           ApiResource.urlEncodeId(null);
         });
+  }
+
+  static class MyClass extends ApiResource {
+    public Proxy proxy;
+  }
+
+  @Test
+  public void testReflectionFilter() {
+    JsonIOException e =
+        assertThrows(JsonIOException.class, () -> ApiResource.GSON.fromJson("{}", MyClass.class));
+
+    // Assert that the error message involves a ReflectionAccessFilter.
+    assertTrue(e.getMessage().contains("ReflectionAccessFilter"));
   }
 }
