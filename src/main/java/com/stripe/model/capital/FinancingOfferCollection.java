2881d44d4b68eaf0bbae578da0710cc20a4bf61a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-03-15 15:39:25 +0000

    Update generated code for v258

--- src/main/java/com/stripe/model/capital/FinancingOfferCollection.java
+++
@@ -1,6 +0,0 @@
-// File generated from our OpenAPI spec
-package com.stripe.model.capital;
-
-import com.stripe.model.StripeCollection;
-
-public class FinancingOfferCollection extends StripeCollection<FinancingOffer> {}
