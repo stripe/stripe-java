636f81efbd138cef79b0e5121bf97808dd317e2e
Tyler J. Kuhn <tyler@wargoats.com>
2023-04-12 10:31:46 -0700

    [BO-2592] ingest dispute loss reason (#28)
    * add loss reason, which is in preview, and available to Step
    
    * formatting
    
    * use local java version 18
    
    * add to issuing dispute, not charge dispute
    
    * add link to stripe doc

--- src/main/java/com/stripe/model/issuing/Dispute.java
+++ src/main/java/com/stripe/model/issuing/Dispute.java
@@ -117,6 +117,13 @@ public class Dispute extends ApiResource
   @SerializedName("status")
   String status;
 
+  /**
+   * The <a href="https://stripe.com/docs/api/issuing/disputes/object">enum</a> that describes the
+   * dispute loss outcome. If the dispute is not lost, this field will be absent.
+   */
+  @SerializedName("loss_reason")
+  String lossReason;
+
   /** The transaction being disputed. */
   @SerializedName("transaction")
   @Getter(lombok.AccessLevel.NONE)
