14630c8a8a88689f5e07862c9a1955e4dd1711dd
Annie Li <anniel@stripe.com>
2023-04-28 16:33:22 -0700

    don't import *

--- src/test/java/com/stripe/net/StripeRequestTest.java
+++ src/test/java/com/stripe/net/StripeRequestTest.java
@@ -1,6 +1,13 @@
 package com.stripe.net;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.common.collect.ImmutableMap;
 import com.stripe.BaseStripeTest;
