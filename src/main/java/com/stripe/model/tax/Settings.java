71ec1729617a486e80674430615d80766d1280c1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-05-11 15:43:50 +0000

    Update generated code for v337

--- src/main/java/com/stripe/model/tax/Settings.java
+++ src/main/java/com/stripe/model/tax/Settings.java
@@ -28,6 +28,10 @@ public class Settings extends ApiResource {
   @SerializedName("defaults")
   Defaults defaults;
 
+  /** The place where your business is located. */
+  @SerializedName("head_office")
+  HeadOffice headOffice;
+
   /**
    * Has the value {@code true} if the object exists in live mode or the value {@code false} if the
    * object exists in test mode.
@@ -35,7 +39,7 @@ public class Settings extends ApiResource {
   @SerializedName("livemode")
   Boolean livemode;
 
-  /** The places where your business is located. */
+  /** The deprecated places where your business is located. */
   @SerializedName("locations")
   List<Settings.Location> locations;
 
@@ -131,6 +135,14 @@ public class Settings extends ApiResource {
      */
     @SerializedName("tax_code")
     String taxCode;
+  }
+
+  @Getter
+  @Setter
+  @EqualsAndHashCode(callSuper = false)
+  public static class HeadOffice extends StripeObject {
+    @SerializedName("address")
+    Address address;
   }
 
   @Getter
