99299626663a5be3705e0901b919ce115b51e59b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-06-13 17:25:56 +0000

    Update generated code for v384

--- src/main/java/com/stripe/param/FileCreateParams.java
+++ src/main/java/com/stripe/param/FileCreateParams.java
@@ -1,57 +1,74 @@
+// File generated from our OpenAPI spec
 package com.stripe.param;
 
 import com.google.gson.annotations.SerializedName;
 import com.stripe.net.ApiRequestParams;
+import com.stripe.param.common.EmptyParam;
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
 
+@Getter
 public class FileCreateParams extends ApiRequestParams {
   /** Specifies which fields in the response should be expanded. */
   @SerializedName("expand")
   List<String> expand;
 
   /**
-   * A file to upload. The file should follow the specifications of RFC 2388 (which defines file
-   * transfers for the `multipart/form-data` protocol).
-   *
-   * <p>Note this field is marked as transient to avoid JSON deserializer. Override {@link
-   * FileCreateParams#toMap()} makes sure that the returned map has this same file instance.
+   * Map of extra parameters for custom features not available in this client library. The content
+   * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+   * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+   * param object. Effectively, this map is flattened to its parent instance.
    */
+  @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+  Map<String, Object> extraParams;
+
+  /**
+   * <strong>Required.</strong> A file to upload. The file should follow the specifications of RFC
+   * 2388 (which defines file transfers for the {@code multipart/form-data} protocol).
+   */
+  @SerializedName("file")
   transient Object file;
 
   /**
-   * Optional parameters to automatically create a [file link](#file_links) for the newly created
-   * file.
+   * Optional parameters to automatically create a <a
+   * href="https://stripe.com/docs/api#file_links">file link</a> for the newly created file.
    */
   @SerializedName("file_link_data")
   FileLinkData fileLinkData;
 
   /**
-   * The purpose of the uploaded file. Possible values are `business_icon`, `business_logo`,
-   * `customer_signature`, `dispute_evidence`, `identity_document`, `pci_document`, or
-   * `tax_document_user_upload`
+   * <strong>Required.</strong> The <a
+   * href="https://stripe.com/docs/file-upload#uploading-a-file">purpose</a> of the uploaded file.
    */
   @SerializedName("purpose")
   Purpose purpose;
 
   private FileCreateParams(
-      List<String> expand, Object file, FileLinkData fileLinkData, Purpose purpose) {
+      List<String> expand,
+      Map<String, Object> extraParams,
+      Object file,
+      FileLinkData fileLinkData,
+      Purpose purpose) {
     this.expand = expand;
+    this.extraParams = extraParams;
     this.file = file;
     this.fileLinkData = fileLinkData;
     this.purpose = purpose;
   }
 
   public static Builder builder() {
-    return new com.stripe.param.FileCreateParams.Builder();
+    return new Builder();
   }
 
   public static class Builder {
     private List<String> expand;
+
+    private Map<String, Object> extraParams;
 
     private Object file;
 
@@ -61,7 +78,8 @@ public class FileCreateParams extends ApiRequestParams {
 
     /** Finalize and obtain parameter instance from this builder. */
     public FileCreateParams build() {
-      return new FileCreateParams(this.expand, this.file, this.fileLinkData, this.purpose);
+      return new FileCreateParams(
+          this.expand, this.extraParams, this.file, this.fileLinkData, this.purpose);
     }
 
     /**
@@ -91,45 +109,77 @@ public class FileCreateParams extends ApiRequestParams {
     }
 
     /**
-     * A file to upload. The file should follow the specifications of RFC 2388 (which defines file
-     * transfers for the `multipart/form-data` protocol).
+     * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+     * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+     * FileCreateParams#extraParams} for the field documentation.
      */
-    public Builder setFile(java.io.File file) {
+    public Builder putExtraParam(String key, Object value) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.put(key, value);
+      return this;
+    }
+
+    /**
+     * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+     * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+     * See {@link FileCreateParams#extraParams} for the field documentation.
+     */
+    public Builder putAllExtraParam(Map<String, Object> map) {
+      if (this.extraParams == null) {
+        this.extraParams = new HashMap<>();
+      }
+      this.extraParams.putAll(map);
+      return this;
+    }
+
+    /**
+     * <strong>Required.</strong> A file to upload. The file should follow the specifications of RFC
+     * 2388 (which defines file transfers for the {@code multipart/form-data} protocol).
+     */
+    public Builder setFile(InputStream file) {
       this.file = file;
       return this;
     }
 
     /**
-     * A file to upload. The file should follow the specifications of RFC 2388 (which defines file
-     * transfers for the `multipart/form-data` protocol).
+     * <strong>Required.</strong> A file to upload. The file should follow the specifications of RFC
+     * 2388 (which defines file transfers for the {@code multipart/form-data} protocol).
      */
-    public Builder setFile(java.io.InputStream file) {
+    public Builder setFile(File file) {
       this.file = file;
       return this;
     }
 
     /**
-     * Optional parameters to automatically create a [file link](#file_links) for the newly created
-     * file.
+     * Optional parameters to automatically create a <a
+     * href="https://stripe.com/docs/api#file_links">file link</a> for the newly created file.
      */
-    public Builder setFileLinkData(FileLinkData fileLinkData) {
+    public Builder setFileLinkData(FileCreateParams.FileLinkData fileLinkData) {
       this.fileLinkData = fileLinkData;
       return this;
     }
 
     /**
-     * The purpose of the uploaded file. Possible values are `business_icon`, `business_logo`,
-     * `customer_signature`, `dispute_evidence`, `identity_document`, `pci_document`, or
-     * `tax_document_user_upload`
+     * <strong>Required.</strong> The <a
+     * href="https://stripe.com/docs/file-upload#uploading-a-file">purpose</a> of the uploaded file.
      */
-    public Builder setPurpose(Purpose purpose) {
+    public Builder setPurpose(FileCreateParams.Purpose purpose) {
       this.purpose = purpose;
       return this;
     }
   }
 
+  @Getter
   public static class FileLinkData {
-    /** Set this to `true` to create a file link for the newly created file. */
+    /**
+     * <strong>Required.</strong> Set this to {@code true} to create a file link for the newly
+     * created file. Creating a link is only possible when the file's {@code purpose} is one of the
+     * following: {@code business_icon}, {@code business_logo}, {@code customer_signature}, {@code
+     * dispute_evidence}, {@code pci_document}, {@code tax_document_user_upload}, or {@code
+     * terminal_reader_splashscreen}.
+     */
     @SerializedName("create")
     Boolean create;
 
@@ -138,20 +188,33 @@ public class FileCreateParams extends ApiRequestParams {
     Long expiresAt;
 
     /**
-     * Set of key-value pairs that you can attach to an object. This can be useful for storing
-     * additional information about the object in a structured format.
+     * Map of extra parameters for custom features not available in this client library. The content
+     * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+     * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+     * param object. Effectively, this map is flattened to its parent instance.
+     */
+    @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+    Map<String, Object> extraParams;
+
+    /**
+     * Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can attach
+     * to an object. This can be useful for storing additional information about the object in a
+     * structured format. Individual keys can be unset by posting an empty value to them. All keys
+     * can be unset by posting an empty value to {@code metadata}.
      */
     @SerializedName("metadata")
-    Map<String, String> metadata;
+    Object metadata;
 
-    private FileLinkData(Boolean create, Long expiresAt, Map<String, String> metadata) {
+    private FileLinkData(
+        Boolean create, Long expiresAt, Map<String, Object> extraParams, Object metadata) {
       this.create = create;
       this.expiresAt = expiresAt;
+      this.extraParams = extraParams;
       this.metadata = metadata;
     }
 
     public static Builder builder() {
-      return new com.stripe.param.FileCreateParams.FileLinkData.Builder();
+      return new Builder();
     }
 
     public static class Builder {
@@ -159,14 +222,23 @@ public class FileCreateParams extends ApiRequestParams {
 
       private Long expiresAt;
 
-      private Map<String, String> metadata;
+      private Map<String, Object> extraParams;
+
+      private Object metadata;
 
       /** Finalize and obtain parameter instance from this builder. */
-      public FileLinkData build() {
-        return new FileLinkData(this.create, this.expiresAt, this.metadata);
+      public FileCreateParams.FileLinkData build() {
+        return new FileCreateParams.FileLinkData(
+            this.create, this.expiresAt, this.extraParams, this.metadata);
       }
 
-      /** Set this to `true` to create a file link for the newly created file. */
+      /**
+       * <strong>Required.</strong> Set this to {@code true} to create a file link for the newly
+       * created file. Creating a link is only possible when the file's {@code purpose} is one of
+       * the following: {@code business_icon}, {@code business_logo}, {@code customer_signature},
+       * {@code dispute_evidence}, {@code pci_document}, {@code tax_document_user_upload}, or {@code
+       * terminal_reader_splashscreen}.
+       */
       public Builder setCreate(Boolean create) {
         this.create = create;
         return this;
@@ -179,28 +251,78 @@ public class FileCreateParams extends ApiRequestParams {
       }
 
       /**
+       * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+       * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+       * FileCreateParams.FileLinkData#extraParams} for the field documentation.
+       */
+      public Builder putExtraParam(String key, Object value) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.put(key, value);
+        return this;
+      }
+
+      /**
+       * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+       * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+       * See {@link FileCreateParams.FileLinkData#extraParams} for the field documentation.
+       */
+      public Builder putAllExtraParam(Map<String, Object> map) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.putAll(map);
+        return this;
+      }
+
+      /**
        * Add a key/value pair to `metadata` map. A map is initialized for the first `put/putAll`
        * call, and subsequent calls add additional key/value pairs to the original map. See {@link
-       * FileLinkData#metadata} for the field documentation.
+       * FileCreateParams.FileLinkData#metadata} for the field documentation.
        */
+      @SuppressWarnings("unchecked")
       public Builder putMetadata(String key, String value) {
-        if (this.metadata == null) {
-          this.metadata = new HashMap<>();
+        if (this.metadata == null || this.metadata instanceof EmptyParam) {
+          this.metadata = new HashMap<String, String>();
         }
-        this.metadata.put(key, value);
+        ((Map<String, String>) this.metadata).put(key, value);
         return this;
       }
 
       /**
        * Add all map key/value pairs to `metadata` map. A map is initialized for the first
        * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
-       * See {@link FileLinkData#metadata} for the field documentation.
+       * See {@link FileCreateParams.FileLinkData#metadata} for the field documentation.
        */
+      @SuppressWarnings("unchecked")
       public Builder putAllMetadata(Map<String, String> map) {
-        if (this.metadata == null) {
-          this.metadata = new HashMap<>();
+        if (this.metadata == null || this.metadata instanceof EmptyParam) {
+          this.metadata = new HashMap<String, String>();
         }
-        this.metadata.putAll(map);
+        ((Map<String, String>) this.metadata).putAll(map);
+        return this;
+      }
+
+      /**
+       * Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+       * attach to an object. This can be useful for storing additional information about the object
+       * in a structured format. Individual keys can be unset by posting an empty value to them. All
+       * keys can be unset by posting an empty value to {@code metadata}.
+       */
+      public Builder setMetadata(EmptyParam metadata) {
+        this.metadata = metadata;
+        return this;
+      }
+
+      /**
+       * Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+       * attach to an object. This can be useful for storing additional information about the object
+       * in a structured format. Individual keys can be unset by posting an empty value to them. All
+       * keys can be unset by posting an empty value to {@code metadata}.
+       */
+      public Builder setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
         return this;
       }
     }
@@ -245,18 +367,10 @@ public class FileCreateParams extends ApiRequestParams {
     }
   }
 
-  /**
-   * Returns untyped parameters for file creation. Map value for {@code "file"} is the same instance
-   * of value set in {@link Builder#setFile(File)} or {@link Builder#setFile(java.io.InputStream)};
-   * the file is not transformed or serialized at this level.
-   *
-   * @return Untyped parameters containing file object set in the builder.
-   */
   @Override
   public Map<String, Object> toMap() {
-    Object fileObject = this.file;
-    Map<String, Object> untypedParamWithPrimitiveTypes = super.toMap();
-    untypedParamWithPrimitiveTypes.put("file", fileObject);
-    return untypedParamWithPrimitiveTypes;
+    Map<String, Object> baseMap = super.toMap();
+    baseMap.put("file", this.file);
+    return baseMap;
   }
 }
