7f522be34721779b3e745800aab04fbca52317f8
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-12 14:02:57 +0000

    Update generated code for v415

--- src/main/java/com/stripe/ApiVersion.java
+++ src/main/java/com/stripe/ApiVersion.java
@@ -3,5 +3,5 @@ package com.stripe;
 
 final class ApiVersion {
   public static final String CURRENT = "2022-11-15";
-  public static final String PREVIEW_CURRENT = "2023-06-08.preview-v2";
+  public static final String PREVIEW_CURRENT = "20230630T012332";
 }
