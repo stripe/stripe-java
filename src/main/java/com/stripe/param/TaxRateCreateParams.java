119e4da898a847635578e491a4986e15b915819e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-17 08:56:42 +0000

    Update generated code for v420

--- src/main/java/com/stripe/param/TaxRateCreateParams.java
+++ src/main/java/com/stripe/param/TaxRateCreateParams.java
@@ -344,6 +344,9 @@ public class TaxRateCreateParams extends ApiRequestParams {
     @SerializedName("sales_tax")
     SALES_TAX("sales_tax"),
 
+    @SerializedName("service_tax")
+    SERVICE_TAX("service_tax"),
+
     @SerializedName("vat")
     VAT("vat");
 
