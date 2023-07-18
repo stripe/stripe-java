b07438ab7cedd0507f349dbe667b05354436d32e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-18 14:42:29 +0000

    Update generated code for v423

--- src/main/java/com/stripe/model/LineItem.java
+++ src/main/java/com/stripe/model/LineItem.java
@@ -116,12 +116,11 @@ public class LineItem extends StripeObject implements HasId {
      * The reasoning behind this tax, for example, if the product is tax exempt. The possible values
      * for this field may be extended as new tax rules are supported.
      *
-     * <p>One of {@code customer_exempt}, {@code excluded_territory}, {@code
-     * jurisdiction_unsupported}, {@code not_collecting}, {@code not_subject_to_tax}, {@code
+     * <p>One of {@code customer_exempt}, {@code not_collecting}, {@code not_subject_to_tax}, {@code
      * not_supported}, {@code portion_product_exempt}, {@code portion_reduced_rated}, {@code
      * portion_standard_rated}, {@code product_exempt}, {@code product_exempt_holiday}, {@code
      * proportionally_rated}, {@code reduced_rated}, {@code reverse_charge}, {@code standard_rated},
-     * {@code taxable_basis_reduced}, {@code vat_exempt}, or {@code zero_rated}.
+     * {@code taxable_basis_reduced}, or {@code zero_rated}.
      */
     @SerializedName("taxability_reason")
     String taxabilityReason;
