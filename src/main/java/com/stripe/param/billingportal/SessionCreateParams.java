6eb802fdbe91932b6190dc714f2d100646d5f910
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-22 20:12:22 +0000

    Update generated code for v478

--- src/main/java/com/stripe/param/billingportal/SessionCreateParams.java
+++ src/main/java/com/stripe/param/billingportal/SessionCreateParams.java
@@ -656,12 +656,18 @@ public class SessionCreateParams extends ApiRequestParams {
       @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
       Map<String, Object> extraParams;
 
+      /** Specify a retention strategy to be used in the cancellation flow. */
+      @SerializedName("retention")
+      Retention retention;
+
       /** <strong>Required.</strong> The ID of the subscription to be canceled. */
       @SerializedName("subscription")
       String subscription;
 
-      private SubscriptionCancel(Map<String, Object> extraParams, String subscription) {
+      private SubscriptionCancel(
+          Map<String, Object> extraParams, Retention retention, String subscription) {
         this.extraParams = extraParams;
+        this.retention = retention;
         this.subscription = subscription;
       }
 
@@ -672,12 +678,14 @@ public class SessionCreateParams extends ApiRequestParams {
       public static class Builder {
         private Map<String, Object> extraParams;
 
+        private Retention retention;
+
         private String subscription;
 
         /** Finalize and obtain parameter instance from this builder. */
         public SessionCreateParams.FlowData.SubscriptionCancel build() {
           return new SessionCreateParams.FlowData.SubscriptionCancel(
-              this.extraParams, this.subscription);
+              this.extraParams, this.retention, this.subscription);
         }
 
         /**
@@ -708,10 +716,190 @@ public class SessionCreateParams extends ApiRequestParams {
           return this;
         }
 
+        /** Specify a retention strategy to be used in the cancellation flow. */
+        public Builder setRetention(
+            SessionCreateParams.FlowData.SubscriptionCancel.Retention retention) {
+          this.retention = retention;
+          return this;
+        }
+
         /** <strong>Required.</strong> The ID of the subscription to be canceled. */
         public Builder setSubscription(String subscription) {
           this.subscription = subscription;
           return this;
+        }
+      }
+
+      @Getter
+      public static class Retention {
+        /** <strong>Required.</strong> Configuration when {@code retention.type=coupon_offer}. */
+        @SerializedName("coupon_offer")
+        CouponOffer couponOffer;
+
+        /**
+         * Map of extra parameters for custom features not available in this client library. The
+         * content in this map is not serialized under this field's {@code @SerializedName} value.
+         * Instead, each key/value pair is serialized as if the key is a root-level field
+         * (serialized) name in this param object. Effectively, this map is flattened to its parent
+         * instance.
+         */
+        @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+        Map<String, Object> extraParams;
+
+        /** <strong>Required.</strong> Type of retention strategy to use with the customer. */
+        @SerializedName("type")
+        Type type;
+
+        private Retention(CouponOffer couponOffer, Map<String, Object> extraParams, Type type) {
+          this.couponOffer = couponOffer;
+          this.extraParams = extraParams;
+          this.type = type;
+        }
+
+        public static Builder builder() {
+          return new Builder();
+        }
+
+        public static class Builder {
+          private CouponOffer couponOffer;
+
+          private Map<String, Object> extraParams;
+
+          private Type type;
+
+          /** Finalize and obtain parameter instance from this builder. */
+          public SessionCreateParams.FlowData.SubscriptionCancel.Retention build() {
+            return new SessionCreateParams.FlowData.SubscriptionCancel.Retention(
+                this.couponOffer, this.extraParams, this.type);
+          }
+
+          /** <strong>Required.</strong> Configuration when {@code retention.type=coupon_offer}. */
+          public Builder setCouponOffer(
+              SessionCreateParams.FlowData.SubscriptionCancel.Retention.CouponOffer couponOffer) {
+            this.couponOffer = couponOffer;
+            return this;
+          }
+
+          /**
+           * Add a key/value pair to `extraParams` map. A map is initialized for the first
+           * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+           * map. See {@link SessionCreateParams.FlowData.SubscriptionCancel.Retention#extraParams}
+           * for the field documentation.
+           */
+          public Builder putExtraParam(String key, Object value) {
+            if (this.extraParams == null) {
+              this.extraParams = new HashMap<>();
+            }
+            this.extraParams.put(key, value);
+            return this;
+          }
+
+          /**
+           * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+           * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+           * map. See {@link SessionCreateParams.FlowData.SubscriptionCancel.Retention#extraParams}
+           * for the field documentation.
+           */
+          public Builder putAllExtraParam(Map<String, Object> map) {
+            if (this.extraParams == null) {
+              this.extraParams = new HashMap<>();
+            }
+            this.extraParams.putAll(map);
+            return this;
+          }
+
+          /** <strong>Required.</strong> Type of retention strategy to use with the customer. */
+          public Builder setType(
+              SessionCreateParams.FlowData.SubscriptionCancel.Retention.Type type) {
+            this.type = type;
+            return this;
+          }
+        }
+
+        @Getter
+        public static class CouponOffer {
+          /** <strong>Required.</strong> The ID of the coupon to be offered. */
+          @SerializedName("coupon")
+          String coupon;
+
+          /**
+           * Map of extra parameters for custom features not available in this client library. The
+           * content in this map is not serialized under this field's {@code @SerializedName} value.
+           * Instead, each key/value pair is serialized as if the key is a root-level field
+           * (serialized) name in this param object. Effectively, this map is flattened to its
+           * parent instance.
+           */
+          @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+          Map<String, Object> extraParams;
+
+          private CouponOffer(String coupon, Map<String, Object> extraParams) {
+            this.coupon = coupon;
+            this.extraParams = extraParams;
+          }
+
+          public static Builder builder() {
+            return new Builder();
+          }
+
+          public static class Builder {
+            private String coupon;
+
+            private Map<String, Object> extraParams;
+
+            /** Finalize and obtain parameter instance from this builder. */
+            public SessionCreateParams.FlowData.SubscriptionCancel.Retention.CouponOffer build() {
+              return new SessionCreateParams.FlowData.SubscriptionCancel.Retention.CouponOffer(
+                  this.coupon, this.extraParams);
+            }
+
+            /** <strong>Required.</strong> The ID of the coupon to be offered. */
+            public Builder setCoupon(String coupon) {
+              this.coupon = coupon;
+              return this;
+            }
+
+            /**
+             * Add a key/value pair to `extraParams` map. A map is initialized for the first
+             * `put/putAll` call, and subsequent calls add additional key/value pairs to the
+             * original map. See {@link
+             * SessionCreateParams.FlowData.SubscriptionCancel.Retention.CouponOffer#extraParams}
+             * for the field documentation.
+             */
+            public Builder putExtraParam(String key, Object value) {
+              if (this.extraParams == null) {
+                this.extraParams = new HashMap<>();
+              }
+              this.extraParams.put(key, value);
+              return this;
+            }
+
+            /**
+             * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+             * `put/putAll` call, and subsequent calls add additional key/value pairs to the
+             * original map. See {@link
+             * SessionCreateParams.FlowData.SubscriptionCancel.Retention.CouponOffer#extraParams}
+             * for the field documentation.
+             */
+            public Builder putAllExtraParam(Map<String, Object> map) {
+              if (this.extraParams == null) {
+                this.extraParams = new HashMap<>();
+              }
+              this.extraParams.putAll(map);
+              return this;
+            }
+          }
+        }
+
+        public enum Type implements ApiRequestParams.EnumParam {
+          @SerializedName("coupon_offer")
+          COUPON_OFFER("coupon_offer");
+
+          @Getter(onMethod_ = {@Override})
+          private final String value;
+
+          Type(String value) {
+            this.value = value;
+          }
         }
       }
     }
