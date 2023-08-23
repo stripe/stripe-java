54ef6c5ecc8725f8b810e209c4a71b9e1ea5948d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-08-23 19:43:51 +0000

    Update generated code for v482

--- src/main/java/com/stripe/model/PaymentMethod.java
+++ src/main/java/com/stripe/model/PaymentMethod.java
@@ -811,8 +811,8 @@ public class PaymentMethod extends ApiResource implements HasId, MetadataStore<P
      * payment methods that tokenize card information (Apple Pay, Google Pay), the tokenized number
      * might be provided instead of the underlying card number.
      *
-     * <p><em>Starting May 1, 2021, card fingerprint in India for Connect will change to allow two
-     * fingerprints for the same card --- one for India and one for the rest of the world.</em>
+     * <p><em>As of May 1, 2021, card fingerprint in India for Connect changed to allow two
+     * fingerprints for the same card---one for India and one for the rest of the world.</em>
      */
     @SerializedName("fingerprint")
     String fingerprint;
@@ -1082,8 +1082,8 @@ public class PaymentMethod extends ApiResource implements HasId, MetadataStore<P
      * payment methods that tokenize card information (Apple Pay, Google Pay), the tokenized number
      * might be provided instead of the underlying card number.
      *
-     * <p><em>Starting May 1, 2021, card fingerprint in India for Connect will change to allow two
-     * fingerprints for the same card --- one for India and one for the rest of the world.</em>
+     * <p><em>As of May 1, 2021, card fingerprint in India for Connect changed to allow two
+     * fingerprints for the same card---one for India and one for the rest of the world.</em>
      */
     @SerializedName("fingerprint")
     String fingerprint;
@@ -1281,8 +1281,8 @@ public class PaymentMethod extends ApiResource implements HasId, MetadataStore<P
      * payment methods that tokenize card information (Apple Pay, Google Pay), the tokenized number
      * might be provided instead of the underlying card number.
      *
-     * <p><em>Starting May 1, 2021, card fingerprint in India for Connect will change to allow two
-     * fingerprints for the same card --- one for India and one for the rest of the world.</em>
+     * <p><em>As of May 1, 2021, card fingerprint in India for Connect changed to allow two
+     * fingerprints for the same card---one for India and one for the rest of the world.</em>
      */
     @SerializedName("fingerprint")
     String fingerprint;
