4509249816f93d1c21a847ee2d41a72f2d8e93de
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-11-15 18:39:25 +0000

    Update generated code for v662

--- src/main/java/com/stripe/service/InvoiceLineItemService.java
+++ src/main/java/com/stripe/service/InvoiceLineItemService.java
@@ -13,6 +13,7 @@ import com.stripe.net.BaseAddress;
 import com.stripe.net.RequestOptions;
 import com.stripe.net.StripeResponseGetter;
 import com.stripe.param.InvoiceLineItemListParams;
+import com.stripe.param.InvoiceLineItemUpdateParams;
 
 public final class InvoiceLineItemService extends ApiService {
   public InvoiceLineItemService(StripeResponseGetter responseGetter) {
@@ -61,6 +62,63 @@ public final class InvoiceLineItemService extends ApiService {
             path,
             ApiRequestParams.paramsToMap(params),
             new TypeToken<StripeCollection<InvoiceLineItem>>() {}.getType(),
+            options,
+            ApiMode.V1);
+  }
+  /**
+   * Updates an invoice’s line item. Some fields, such as {@code tax_amounts}, only live on the
+   * invoice line item, so they can only be updated through this endpoint. Other fields, such as
+   * {@code amount}, live on both the invoice item and the invoice line item, so updates on this
+   * endpoint will propagate to the invoice item as well. Updating an invoice’s line item is only
+   * possible before the invoice is finalized.
+   */
+  public InvoiceLineItem update(
+      String invoice, String lineItemId, InvoiceLineItemUpdateParams params)
+      throws StripeException {
+    return update(invoice, lineItemId, params, (RequestOptions) null);
+  }
+  /**
+   * Updates an invoice’s line item. Some fields, such as {@code tax_amounts}, only live on the
+   * invoice line item, so they can only be updated through this endpoint. Other fields, such as
+   * {@code amount}, live on both the invoice item and the invoice line item, so updates on this
+   * endpoint will propagate to the invoice item as well. Updating an invoice’s line item is only
+   * possible before the invoice is finalized.
+   */
+  public InvoiceLineItem update(String invoice, String lineItemId, RequestOptions options)
+      throws StripeException {
+    return update(invoice, lineItemId, (InvoiceLineItemUpdateParams) null, options);
+  }
+  /**
+   * Updates an invoice’s line item. Some fields, such as {@code tax_amounts}, only live on the
+   * invoice line item, so they can only be updated through this endpoint. Other fields, such as
+   * {@code amount}, live on both the invoice item and the invoice line item, so updates on this
+   * endpoint will propagate to the invoice item as well. Updating an invoice’s line item is only
+   * possible before the invoice is finalized.
+   */
+  public InvoiceLineItem update(String invoice, String lineItemId) throws StripeException {
+    return update(invoice, lineItemId, (InvoiceLineItemUpdateParams) null, (RequestOptions) null);
+  }
+  /**
+   * Updates an invoice’s line item. Some fields, such as {@code tax_amounts}, only live on the
+   * invoice line item, so they can only be updated through this endpoint. Other fields, such as
+   * {@code amount}, live on both the invoice item and the invoice line item, so updates on this
+   * endpoint will propagate to the invoice item as well. Updating an invoice’s line item is only
+   * possible before the invoice is finalized.
+   */
+  public InvoiceLineItem update(
+      String invoice, String lineItemId, InvoiceLineItemUpdateParams params, RequestOptions options)
+      throws StripeException {
+    String path =
+        String.format(
+            "/v1/invoices/%s/lines/%s",
+            ApiResource.urlEncodeId(invoice), ApiResource.urlEncodeId(lineItemId));
+    return getResponseGetter()
+        .request(
+            BaseAddress.API,
+            ApiResource.RequestMethod.POST,
+            path,
+            ApiRequestParams.paramsToMap(params),
+            InvoiceLineItem.class,
             options,
             ApiMode.V1);
   }
