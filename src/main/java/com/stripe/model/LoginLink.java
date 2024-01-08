339c89397adf798489694dc4efc0dc07d98abc2a
Richard Marmorstein <richardm@stripe.com>
2024-01-08 17:09:38 -0600

    Add missing overloads

--- src/main/java/com/stripe/model/LoginLink.java
+++ src/main/java/com/stripe/model/LoginLink.java
@@ -44,6 +44,41 @@ public class LoginLink extends ApiResource {
    * href="https://stripe.com/docs/connect/express-accounts">Express accounts</a> connected to your
    * platform</strong>.
    */
+  public static LoginLink createOnAccount(String account) throws StripeException {
+    return createOnAccount(account, (Map<String, Object>) null, (RequestOptions) null);
+  }
+
+  /**
+   * Creates a single-use login link for an Express account to access their Stripe dashboard.
+   *
+   * <p><strong>You may only create login links for <a
+   * href="https://stripe.com/docs/connect/express-accounts">Express accounts</a> connected to your
+   * platform</strong>.
+   */
+  public static LoginLink createOnAccount(String account, RequestOptions options)
+      throws StripeException {
+    return createOnAccount(account, (Map<String, Object>) null, options);
+  }
+
+  /**
+   * Creates a single-use login link for an Express account to access their Stripe dashboard.
+   *
+   * <p><strong>You may only create login links for <a
+   * href="https://stripe.com/docs/connect/express-accounts">Express accounts</a> connected to your
+   * platform</strong>.
+   */
+  public static LoginLink createOnAccount(String account, Map<String, Object> params)
+      throws StripeException {
+    return createOnAccount(account, params, (RequestOptions) null);
+  }
+
+  /**
+   * Creates a single-use login link for an Express account to access their Stripe dashboard.
+   *
+   * <p><strong>You may only create login links for <a
+   * href="https://stripe.com/docs/connect/express-accounts">Express accounts</a> connected to your
+   * platform</strong>.
+   */
   public static LoginLink createOnAccount(
       String account, Map<String, Object> params, RequestOptions options) throws StripeException {
     String path = String.format("/v1/accounts/%s/login_links", ApiResource.urlEncodeId(account));
@@ -56,6 +91,18 @@ public class LoginLink extends ApiResource {
             LoginLink.class,
             options,
             ApiMode.V1);
+  }
+
+  /**
+   * Creates a single-use login link for an Express account to access their Stripe dashboard.
+   *
+   * <p><strong>You may only create login links for <a
+   * href="https://stripe.com/docs/connect/express-accounts">Express accounts</a> connected to your
+   * platform</strong>.
+   */
+  public static LoginLink createOnAccount(String account, LoginLinkCreateOnAccountParams params)
+      throws StripeException {
+    return createOnAccount(account, params, (RequestOptions) null);
   }
 
   /**
