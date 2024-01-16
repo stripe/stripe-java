4f8883c32f302d1fe9822550b5a32629da012c9b
Richard Marmorstein <richardm@stripe.com>
2024-01-16 12:56:48 -0600

    Immutable BaseApiRequest

--- src/main/java/com/stripe/net/BaseApiRequest.java
+++ src/main/java/com/stripe/net/BaseApiRequest.java
@@ -2,37 +2,18 @@ package com.stripe.net;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 class BaseApiRequest {
-  private BaseAddress baseAddress;
-  private ApiResource.RequestMethod method;
-  private String path;
-  private RequestOptions options;
-  private ApiMode apiMode;
-
-  @Setter private List<String> usage;
-
-  /** Internal method. Adds a record of a tracked behavior identified by "used" to the request. */
-  public void addUsage(String used) {
-    if (this.usage == null) {
-      this.usage = new ArrayList<>();
-    }
-    this.usage.add(used);
-  }
-
-  public BaseApiRequest(
-      BaseAddress baseAddress,
-      ApiResource.RequestMethod method,
-      String path,
-      RequestOptions options,
-      ApiMode apiMode) {
-    this.baseAddress = baseAddress;
-    this.method = method;
-    this.path = path;
-    this.options = options;
-    this.apiMode = apiMode;
-  }
+  private final BaseAddress baseAddress;
+  private final ApiResource.RequestMethod method;
+  private final String path;
+  private final RequestOptions options;
+  private final ApiMode apiMode;
+  private final List<String> usage;
 }
