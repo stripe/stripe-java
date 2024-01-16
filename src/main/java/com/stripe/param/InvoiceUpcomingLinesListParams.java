e56a45845a69071142632f5975a0234d4965880b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-16 09:29:40 +0000

    Update generated code for v757

--- src/main/java/com/stripe/param/InvoiceUpcomingLinesListParams.java
+++ src/main/java/com/stripe/param/InvoiceUpcomingLinesListParams.java
@@ -819,9 +819,18 @@ public class InvoiceUpcomingLinesListParams extends ApiRequestParams {
     @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
     Map<String, Object> extraParams;
 
-    private AutomaticTax(Boolean enabled, Map<String, Object> extraParams) {
+    /**
+     * The account that's liable for tax. If set, the business address and tax registrations
+     * required to perform the tax calculation are loaded from this account. The tax transaction is
+     * returned in the report of the connected account.
+     */
+    @SerializedName("liability")
+    Liability liability;
+
+    private AutomaticTax(Boolean enabled, Map<String, Object> extraParams, Liability liability) {
       this.enabled = enabled;
       this.extraParams = extraParams;
+      this.liability = liability;
     }
 
     public static Builder builder() {
@@ -833,9 +842,12 @@ public class InvoiceUpcomingLinesListParams extends ApiRequestParams {
 
       private Map<String, Object> extraParams;
 
+      private Liability liability;
+
       /** Finalize and obtain parameter instance from this builder. */
       public InvoiceUpcomingLinesListParams.AutomaticTax build() {
-        return new InvoiceUpcomingLinesListParams.AutomaticTax(this.enabled, this.extraParams);
+        return new InvoiceUpcomingLinesListParams.AutomaticTax(
+            this.enabled, this.extraParams, this.liability);
       }
 
       /**
@@ -874,6 +886,115 @@ public class InvoiceUpcomingLinesListParams extends ApiRequestParams {
         }
         this.extraParams.putAll(map);
         return this;
+      }
+
+      /**
+       * The account that's liable for tax. If set, the business address and tax registrations
+       * required to perform the tax calculation are loaded from this account. The tax transaction
+       * is returned in the report of the connected account.
+       */
+      public Builder setLiability(InvoiceUpcomingLinesListParams.AutomaticTax.Liability liability) {
+        this.liability = liability;
+        return this;
+      }
+    }
+
+    @Getter
+    public static class Liability {
+      /** The connected account being referenced when {@code type} is {@code account}. */
+      @SerializedName("account")
+      String account;
+
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      /** <strong>Required.</strong> Type of the account referenced in the request. */
+      @SerializedName("type")
+      Type type;
+
+      private Liability(String account, Map<String, Object> extraParams, Type type) {
+        this.account = account;
+        this.extraParams = extraParams;
+        this.type = type;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private String account;
+
+        private Map<String, Object> extraParams;
+
+        private Type type;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public InvoiceUpcomingLinesListParams.AutomaticTax.Liability build() {
+          return new InvoiceUpcomingLinesListParams.AutomaticTax.Liability(
+              this.account, this.extraParams, this.type);
+        }
+
+        /** The connected account being referenced when {@code type} is {@code account}. */
+        public Builder setAccount(String account) {
+          this.account = account;
+          return this;
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link InvoiceUpcomingLinesListParams.AutomaticTax.Liability#extraParams} for
+         * the field documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link InvoiceUpcomingLinesListParams.AutomaticTax.Liability#extraParams} for
+         * the field documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+
+        /** <strong>Required.</strong> Type of the account referenced in the request. */
+        public Builder setType(InvoiceUpcomingLinesListParams.AutomaticTax.Liability.Type type) {
+          this.type = type;
+          return this;
+        }
+      }
+
+      public enum Type implements ApiRequestParams.EnumParam {
+        @SerializedName("account")
+        ACCOUNT("account"),
+
+        @SerializedName("self")
+        SELF("self");
+
+        @Getter(onMethod_ = {@Override})
+        private final String value;
+
+        Type(String value) {
+          this.value = value;
+        }
       }
     }
   }
