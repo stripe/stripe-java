be8e00a27ad04e91a07cacc8823d4f8c494a64b0
Richard Marmorstein <richardm@stripe.com>
2024-01-18 11:14:56 -0600

    Format

--- src/test/java/com/stripe/net/RawApiRequestTest.java
+++ src/test/java/com/stripe/net/RawApiRequestTest.java
@@ -4,13 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.stripe.BaseStripeTest;
 import com.stripe.net.ApiResource.RequestMethod;
-
 import org.junit.jupiter.api.Test;
 
 public class RawApiRequestTest extends BaseStripeTest {
   @Test
   public void testAddUsage() {
-    RawApiRequest req1 = new RawApiRequest(BaseAddress.API, RequestMethod.POST, "/v1/customers", "description=foo", null, ApiMode.V1);
+    RawApiRequest req1 =
+        new RawApiRequest(
+            BaseAddress.API,
+            RequestMethod.POST,
+            "/v1/customers",
+            "description=foo",
+            null,
+            ApiMode.V1);
     RawApiRequest req2 = req1.addUsage("foo");
     RawApiRequest req3 = req2.addUsage("bar");
     assertEquals(null, req1.getUsage());
