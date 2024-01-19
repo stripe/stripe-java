b3a703728855a5d2dbf1f537484dd8142bd9233e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-19 20:53:38 +0000

    Update generated code for v771

--- src/main/java/com/stripe/param/QuoteUpdateParams.java
+++ src/main/java/com/stripe/param/QuoteUpdateParams.java
@@ -1253,8 +1253,8 @@ public class QuoteUpdateParams extends ApiRequestParams {
 
         /**
          * The number of intervals between subscription billings. For example, {@code
-         * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of one year
-         * interval allowed (1 year, 12 months, or 52 weeks).
+         * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of three years
+         * interval allowed (3 years, 36 months, or 156 weeks).
          */
         @SerializedName("interval_count")
         Long intervalCount;
@@ -1322,8 +1322,8 @@ public class QuoteUpdateParams extends ApiRequestParams {
 
           /**
            * The number of intervals between subscription billings. For example, {@code
-           * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of one year
-           * interval allowed (1 year, 12 months, or 52 weeks).
+           * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of three
+           * years interval allowed (3 years, 36 months, or 156 weeks).
            */
           public Builder setIntervalCount(Long intervalCount) {
             this.intervalCount = intervalCount;
--- src/main/java/com/stripe/param/SubscriptionScheduleCreateParams.java
+++ src/main/java/com/stripe/param/SubscriptionScheduleCreateParams.java
@@ -2862,8 +2862,8 @@ public class SubscriptionScheduleCreateParams extends ApiRequestParams {
 
           /**
            * The number of intervals between subscription billings. For example, {@code
-           * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of one year
-           * interval allowed (1 year, 12 months, or 52 weeks).
+           * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of three
+           * years interval allowed (3 years, 36 months, or 156 weeks).
            */
           @SerializedName("interval_count")
           Long intervalCount;
@@ -2934,8 +2934,8 @@ public class SubscriptionScheduleCreateParams extends ApiRequestParams {
 
             /**
              * The number of intervals between subscription billings. For example, {@code
-             * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of one
-             * year interval allowed (1 year, 12 months, or 52 weeks).
+             * interval=month} and {@code interval_count=3} bills every 3 months. Maximum of three
+             * years interval allowed (3 years, 36 months, or 156 weeks).
              */
             public Builder setIntervalCount(Long intervalCount) {
               this.intervalCount = intervalCount;
