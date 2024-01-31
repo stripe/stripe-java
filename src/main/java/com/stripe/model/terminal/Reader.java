51c2b3cb1904c600e611bc4dc788c48e5a2aa4ca
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-01-31 06:42:00 +0000

    Update generated code for v803

--- src/main/java/com/stripe/model/terminal/Reader.java
+++ src/main/java/com/stripe/model/terminal/Reader.java
@@ -114,7 +114,11 @@ public class Reader extends ApiResource implements HasId, MetadataStore<Reader> 
   @SerializedName("serial_number")
   String serialNumber;
 
-  /** The networking status of the reader. */
+  /**
+   * The networking status of the reader.
+   *
+   * <p>One of {@code offline}, or {@code online}.
+   */
   @SerializedName("status")
   String status;
 
