c700b1136e1c05c159537550f19bb7137b2e59ed
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-20 23:00:31 +0000

    Update generated code for v890

--- src/main/java/com/stripe/model/Charge.java
+++ src/main/java/com/stripe/model/Charge.java
@@ -3057,6 +3057,10 @@ public class Charge extends ApiResource implements MetadataStore<Charge>, Balanc
       @SerializedName("last4")
       String last4;
 
+      /** Reference number to locate ACH payments with customer's bank. */
+      @SerializedName("payment_reference")
+      String paymentReference;
+
       /** Routing number of the bank account. */
       @SerializedName("routing_number")
       String routingNumber;
