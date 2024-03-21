cd37085cbc969111a91757e85bbaf411805560f0
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-03-21 17:08:03 +0000

    Update generated code for v894

--- src/main/java/com/stripe/model/StripeError.java
+++ src/main/java/com/stripe/model/StripeError.java
@@ -38,20 +38,21 @@ public class StripeError extends StripeObject {
    * customer_max_subscriptions}, {@code customer_tax_location_invalid}, {@code
    * debit_not_authorized}, {@code email_invalid}, {@code expired_card}, {@code
    * financial_connections_account_inactive}, {@code
-   * financial_connections_no_successful_transaction_refresh}, {@code idempotency_key_in_use},
-   * {@code incorrect_address}, {@code incorrect_cvc}, {@code incorrect_number}, {@code
-   * incorrect_zip}, {@code instant_payouts_config_disabled}, {@code
-   * instant_payouts_currency_disabled}, {@code instant_payouts_limit_exceeded}, {@code
-   * instant_payouts_unsupported}, {@code insufficient_funds}, {@code intent_invalid_state}, {@code
-   * intent_verification_method_missing}, {@code invalid_card_type}, {@code invalid_characters},
-   * {@code invalid_charge_amount}, {@code invalid_cvc}, {@code invalid_expiry_month}, {@code
-   * invalid_expiry_year}, {@code invalid_number}, {@code invalid_source_usage}, {@code
-   * invalid_tax_location}, {@code invoice_no_customer_line_items}, {@code
-   * invoice_no_payment_method_types}, {@code invoice_no_subscription_line_items}, {@code
-   * invoice_not_editable}, {@code invoice_on_behalf_of_not_editable}, {@code
-   * invoice_payment_intent_requires_action}, {@code invoice_upcoming_none}, {@code
-   * livemode_mismatch}, {@code lock_timeout}, {@code missing}, {@code no_account}, {@code
-   * not_allowed_on_standard_account}, {@code out_of_inventory}, {@code
+   * financial_connections_no_successful_transaction_refresh}, {@code forwarding_api_inactive},
+   * {@code forwarding_api_invalid_parameter}, {@code forwarding_api_upstream_connection_error},
+   * {@code forwarding_api_upstream_connection_timeout}, {@code idempotency_key_in_use}, {@code
+   * incorrect_address}, {@code incorrect_cvc}, {@code incorrect_number}, {@code incorrect_zip},
+   * {@code instant_payouts_config_disabled}, {@code instant_payouts_currency_disabled}, {@code
+   * instant_payouts_limit_exceeded}, {@code instant_payouts_unsupported}, {@code
+   * insufficient_funds}, {@code intent_invalid_state}, {@code intent_verification_method_missing},
+   * {@code invalid_card_type}, {@code invalid_characters}, {@code invalid_charge_amount}, {@code
+   * invalid_cvc}, {@code invalid_expiry_month}, {@code invalid_expiry_year}, {@code
+   * invalid_number}, {@code invalid_source_usage}, {@code invalid_tax_location}, {@code
+   * invoice_no_customer_line_items}, {@code invoice_no_payment_method_types}, {@code
+   * invoice_no_subscription_line_items}, {@code invoice_not_editable}, {@code
+   * invoice_on_behalf_of_not_editable}, {@code invoice_payment_intent_requires_action}, {@code
+   * invoice_upcoming_none}, {@code livemode_mismatch}, {@code lock_timeout}, {@code missing},
+   * {@code no_account}, {@code not_allowed_on_standard_account}, {@code out_of_inventory}, {@code
    * ownership_declaration_not_allowed}, {@code parameter_invalid_empty}, {@code
    * parameter_invalid_integer}, {@code parameter_invalid_string_blank}, {@code
    * parameter_invalid_string_empty}, {@code parameter_missing}, {@code parameter_unknown}, {@code
