5f9b2a3905c2fd035f16053a2ce77082df3a5a35
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-03 23:48:43 +0000

    Update generated code for v930

--- src/main/java/com/stripe/model/Account.java
+++ src/main/java/com/stripe/model/Account.java
@@ -757,7 +757,7 @@ public class Account extends ApiResource implements MetadataStore<Account>, Paym
     public static class AnnualRevenue extends StripeObject {
       /**
        * A non-negative integer representing the amount in the <a
-       * href="https://stripe.com/docs/currencies#zero-decimal">smallest currency unit</a>.
+       * href="https://docs.stripe.com/currencies#zero-decimal">smallest currency unit</a>.
        */
       @SerializedName("amount")
       Long amount;
@@ -784,7 +784,7 @@ public class Account extends ApiResource implements MetadataStore<Account>, Paym
     public static class MonthlyEstimatedRevenue extends StripeObject {
       /**
        * A non-negative integer representing how much to charge in the <a
-       * href="https://stripe.com/docs/currencies#zero-decimal">smallest currency unit</a>.
+       * href="https://docs.stripe.com/currencies#zero-decimal">smallest currency unit</a>.
        */
       @SerializedName("amount")
       Long amount;
