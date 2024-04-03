5c0cfa4930a6da3acc305a0aae82f371a6088be5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-03 21:37:44 +0000

    Update generated code for v927

--- src/main/java/com/stripe/model/billingportal/Configuration.java
+++ src/main/java/com/stripe/model/billingportal/Configuration.java
@@ -312,6 +312,9 @@ public class Configuration extends ApiResource implements HasId, MetadataStore<C
     @SerializedName("subscription_cancel")
     SubscriptionCancel subscriptionCancel;
 
+    @SerializedName("subscription_pause")
+    SubscriptionPause subscriptionPause;
+
     @SerializedName("subscription_update")
     SubscriptionUpdate subscriptionUpdate;
 
@@ -388,6 +391,15 @@ public class Configuration extends ApiResource implements HasId, MetadataStore<C
         @SerializedName("options")
         List<String> options;
       }
+    }
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode(callSuper = false)
+    public static class SubscriptionPause extends StripeObject {
+      /** Whether the feature is enabled. */
+      @SerializedName("enabled")
+      Boolean enabled;
     }
 
     @Getter
