9d5714cc2cc894a0ca16e6ef1277f478f277fd54
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-04 19:16:16 +0000

    Update generated code for v932

--- src/main/java/com/stripe/model/terminal/Reader.java
+++ src/main/java/com/stripe/model/terminal/Reader.java
@@ -62,7 +62,8 @@ public class Reader extends ApiResource implements HasId, MetadataStore<Reader> 
 
   /**
    * Type of reader, one of {@code bbpos_wisepad3}, {@code stripe_m2}, {@code bbpos_chipper2x},
-   * {@code bbpos_wisepos_e}, {@code verifone_P400}, or {@code simulated_wisepos_e}.
+   * {@code bbpos_wisepos_e}, {@code verifone_P400}, {@code simulated_wisepos_e}, or {@code
+   * mobile_phone_reader}.
    */
   @SerializedName("device_type")
   String deviceType;
