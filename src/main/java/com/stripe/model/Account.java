905c381905d9f6f3467737bcde2ab1b9d1462411
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-11 17:38:16 +0000

    Update generated code for v946

--- src/main/java/com/stripe/model/Account.java
+++ src/main/java/com/stripe/model/Account.java
@@ -1589,9 +1589,11 @@ public class Account extends ApiResource implements MetadataStore<Account>, Paym
     List<String> pastDue;
 
     /**
-     * Fields that may become required depending on the results of verification or review. Will be
-     * an empty array unless an asynchronous verification is pending. If verification fails, these
-     * fields move to {@code eventually_due} or {@code currently_due}.
+     * Fields that might become required depending on the results of verification or review. It's an
+     * empty array unless an asynchronous verification is pending. If verification fails, these
+     * fields move to {@code eventually_due} or {@code currently_due}. Fields might appear in {@code
+     * eventually_due} or {@code currently_due} and in {@code pending_verification} if verification
+     * fails but another verification is still pending.
      */
     @SerializedName("pending_verification")
     List<String> pendingVerification;
@@ -1754,9 +1756,11 @@ public class Account extends ApiResource implements MetadataStore<Account>, Paym
     List<String> pastDue;
 
     /**
-     * Fields that may become required depending on the results of verification or review. Will be
-     * an empty array unless an asynchronous verification is pending. If verification fails, these
-     * fields move to {@code eventually_due}, {@code currently_due}, or {@code past_due}.
+     * Fields that might become required depending on the results of verification or review. It's an
+     * empty array unless an asynchronous verification is pending. If verification fails, these
+     * fields move to {@code eventually_due}, {@code currently_due}, or {@code past_due}. Fields
+     * might appear in {@code eventually_due}, {@code currently_due}, or {@code past_due} and in
+     * {@code pending_verification} if verification fails but another verification is still pending.
      */
     @SerializedName("pending_verification")
     List<String> pendingVerification;
