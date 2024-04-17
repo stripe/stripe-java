d74940022215963118c7c9563609fbbd2393a009
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-17 18:03:21 +0000

    Update generated code for v964

--- src/main/java/com/stripe/param/billing/MeterEventCreateParams.java
+++ src/main/java/com/stripe/param/billing/MeterEventCreateParams.java
@@ -39,8 +39,10 @@ public class MeterEventCreateParams extends ApiRequestParams {
   String identifier;
 
   /**
-   * <strong>Required.</strong> The payload of the event. This must contain a field with the event's
-   * numerical value and a field to map the event to a customer.
+   * <strong>Required.</strong> The payload of the event. This must contain the fields corresponding
+   * to a meter's {@code customer_mapping.event_payload_key} (default is {@code stripe_customer_id})
+   * and {@code value_settings.event_payload_key} (default is {@code value}). Read more about the <a
+   * href="https://docs.stripe.com/billing/subscriptions/usage-based/recording-usage#payload-key-overrides">payload</a>.
    */
   @SerializedName("payload")
   Map<String, String> payload;
