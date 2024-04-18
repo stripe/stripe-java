50cb3fc51e128142994816b7535b5bf2ce2266b7
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-18 17:19:57 +0000

    Update generated code for v971

--- src/main/java/com/stripe/model/checkout/Session.java
+++ src/main/java/com/stripe/model/checkout/Session.java
@@ -1436,6 +1436,9 @@ public class Session extends ApiResource implements HasId {
     @SerializedName("link")
     Link link;
 
+    @SerializedName("mobilepay")
+    Mobilepay mobilepay;
+
     @SerializedName("oxxo")
     Oxxo oxxo;
 
@@ -2119,6 +2122,31 @@ public class Session extends ApiResource implements HasId {
        * rules, such as <a href="https://stripe.com/docs/strong-customer-authentication">SCA</a>.
        *
        * <p>One of {@code none}, or {@code off_session}.
+       */
+      @SerializedName("setup_future_usage")
+      String setupFutureUsage;
+    }
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode(callSuper = false)
+    public static class Mobilepay extends StripeObject {
+      /**
+       * Indicates that you intend to make future payments with this PaymentIntent's payment method.
+       *
+       * <p>Providing this parameter will <a
+       * href="https://stripe.com/docs/payments/save-during-payment">attach the payment method</a>
+       * to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any
+       * required actions from the user are complete. If no Customer was provided, the payment
+       * method can still be <a
+       * href="https://stripe.com/docs/api/payment_methods/attach">attached</a> to a Customer after
+       * the transaction completes.
+       *
+       * <p>When processing card payments, Stripe also uses {@code setup_future_usage} to
+       * dynamically optimize your payment flow and comply with regional legislation and network
+       * rules, such as <a href="https://stripe.com/docs/strong-customer-authentication">SCA</a>.
+       *
+       * <p>Equal to {@code none}.
        */
       @SerializedName("setup_future_usage")
       String setupFutureUsage;
