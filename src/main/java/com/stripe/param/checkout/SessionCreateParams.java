d6017f096fd8efdf7951e6020138bab26d6b1012
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-04-18 17:19:18 +0000

    Update generated code for v971

--- src/main/java/com/stripe/param/checkout/SessionCreateParams.java
+++ src/main/java/com/stripe/param/checkout/SessionCreateParams.java
@@ -5591,6 +5591,10 @@ public class SessionCreateParams extends ApiRequestParams {
     @SerializedName("link")
     Link link;
 
+    /** contains details about the Mobilepay payment method options. */
+    @SerializedName("mobilepay")
+    Mobilepay mobilepay;
+
     /** contains details about the OXXO payment method options. */
     @SerializedName("oxxo")
     Oxxo oxxo;
@@ -5657,6 +5661,7 @@ public class SessionCreateParams extends ApiRequestParams {
         Klarna klarna,
         Konbini konbini,
         Link link,
+        Mobilepay mobilepay,
         Oxxo oxxo,
         P24 p24,
         Paynow paynow,
@@ -5689,6 +5694,7 @@ public class SessionCreateParams extends ApiRequestParams {
       this.klarna = klarna;
       this.konbini = konbini;
       this.link = link;
+      this.mobilepay = mobilepay;
       this.oxxo = oxxo;
       this.p24 = p24;
       this.paynow = paynow;
@@ -5749,6 +5755,8 @@ public class SessionCreateParams extends ApiRequestParams {
 
       private Link link;
 
+      private Mobilepay mobilepay;
+
       private Oxxo oxxo;
 
       private P24 p24;
@@ -5795,6 +5803,7 @@ public class SessionCreateParams extends ApiRequestParams {
             this.klarna,
             this.konbini,
             this.link,
+            this.mobilepay,
             this.oxxo,
             this.p24,
             this.paynow,
@@ -5955,6 +5964,12 @@ public class SessionCreateParams extends ApiRequestParams {
       /** contains details about the Link payment method options. */
       public Builder setLink(SessionCreateParams.PaymentMethodOptions.Link link) {
         this.link = link;
+        return this;
+      }
+
+      /** contains details about the Mobilepay payment method options. */
+      public Builder setMobilepay(SessionCreateParams.PaymentMethodOptions.Mobilepay mobilepay) {
+        this.mobilepay = mobilepay;
         return this;
       }
 
@@ -9261,6 +9276,119 @@ public class SessionCreateParams extends ApiRequestParams {
 
         @SerializedName("off_session")
         OFF_SESSION("off_session");
+
+        @Getter(onMethod_ = {@Override})
+        private final String value;
+
+        SetupFutureUsage(String value) {
+          this.value = value;
+        }
+      }
+    }
+
+    @Getter
+    public static class Mobilepay {
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      /**
+       * Indicates that you intend to make future payments with this PaymentIntent's payment method.
+       *
+       * <p>Providing this parameter will <a
+       * href="https://stripe.com/docs/payments/save-during-payment">attach the payment method</a>
+       * to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any
+       * required actions from the user are complete. If no Customer was provided, the payment
+       * method can still be <a
+       * href="https://stripe.com/docs/api/payment_methods/attach">attached</a> to a Customer after
+       * the transaction completes.
+       *
+       * <p>When processing card payments, Stripe also uses {@code setup_future_usage} to
+       * dynamically optimize your payment flow and comply with regional legislation and network
+       * rules, such as <a href="https://stripe.com/docs/strong-customer-authentication">SCA</a>.
+       */
+      @SerializedName("setup_future_usage")
+      SetupFutureUsage setupFutureUsage;
+
+      private Mobilepay(Map<String, Object> extraParams, SetupFutureUsage setupFutureUsage) {
+        this.extraParams = extraParams;
+        this.setupFutureUsage = setupFutureUsage;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private Map<String, Object> extraParams;
+
+        private SetupFutureUsage setupFutureUsage;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public SessionCreateParams.PaymentMethodOptions.Mobilepay build() {
+          return new SessionCreateParams.PaymentMethodOptions.Mobilepay(
+              this.extraParams, this.setupFutureUsage);
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SessionCreateParams.PaymentMethodOptions.Mobilepay#extraParams} for the
+         * field documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SessionCreateParams.PaymentMethodOptions.Mobilepay#extraParams} for the
+         * field documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+
+        /**
+         * Indicates that you intend to make future payments with this PaymentIntent's payment
+         * method.
+         *
+         * <p>Providing this parameter will <a
+         * href="https://stripe.com/docs/payments/save-during-payment">attach the payment method</a>
+         * to the PaymentIntent's Customer, if present, after the PaymentIntent is confirmed and any
+         * required actions from the user are complete. If no Customer was provided, the payment
+         * method can still be <a
+         * href="https://stripe.com/docs/api/payment_methods/attach">attached</a> to a Customer
+         * after the transaction completes.
+         *
+         * <p>When processing card payments, Stripe also uses {@code setup_future_usage} to
+         * dynamically optimize your payment flow and comply with regional legislation and network
+         * rules, such as <a href="https://stripe.com/docs/strong-customer-authentication">SCA</a>.
+         */
+        public Builder setSetupFutureUsage(
+            SessionCreateParams.PaymentMethodOptions.Mobilepay.SetupFutureUsage setupFutureUsage) {
+          this.setupFutureUsage = setupFutureUsage;
+          return this;
+        }
+      }
+
+      public enum SetupFutureUsage implements ApiRequestParams.EnumParam {
+        @SerializedName("none")
+        NONE("none");
 
         @Getter(onMethod_ = {@Override})
         private final String value;
@@ -14257,6 +14385,9 @@ public class SessionCreateParams extends ApiRequestParams {
 
     @SerializedName("link")
     LINK("link"),
+
+    @SerializedName("mobilepay")
+    MOBILEPAY("mobilepay"),
 
     @SerializedName("oxxo")
     OXXO("oxxo"),
