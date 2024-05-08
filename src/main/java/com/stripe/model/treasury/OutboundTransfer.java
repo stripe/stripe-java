1e4211165aea7ba4fc1ea484a556f291b83e2822
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-08 22:07:07 +0000

    Update generated code for v1016

--- src/main/java/com/stripe/model/treasury/OutboundTransfer.java
+++ src/main/java/com/stripe/model/treasury/OutboundTransfer.java
@@ -7,7 +7,6 @@ import com.stripe.model.Address;
 import com.stripe.model.ExpandableField;
 import com.stripe.model.HasId;
 import com.stripe.model.Mandate;
-import com.stripe.model.MetadataStore;
 import com.stripe.model.StripeObject;
 import com.stripe.net.ApiMode;
 import com.stripe.net.ApiRequest;
@@ -43,8 +42,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
-public class OutboundTransfer extends ApiResource
-    implements HasId, MetadataStore<OutboundTransfer> {
+public class OutboundTransfer extends ApiResource implements HasId {
   /** Amount (in cents) transferred. */
   @SerializedName("amount")
   Long amount;
@@ -108,7 +106,6 @@ public class OutboundTransfer extends ApiResource
    * to an object. This can be useful for storing additional information about the object in a
    * structured format.
    */
-  @Getter(onMethod_ = {@Override})
   @SerializedName("metadata")
   Map<String, String> metadata;
 
