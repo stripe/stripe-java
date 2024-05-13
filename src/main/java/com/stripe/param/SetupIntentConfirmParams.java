f1f582b04561afb422b2711fb62cea629bfa911c
prathmesh-stripe <165320323+prathmesh-stripe@users.noreply.github.com>
2024-05-13 12:27:03 -0400

    Added deprecated annotation to builder methods (#1804)

--- src/main/java/com/stripe/param/SetupIntentConfirmParams.java
+++ src/main/java/com/stripe/param/SetupIntentConfirmParams.java
@@ -6756,6 +6756,7 @@ public class SetupIntentConfirmParams extends ApiRequestParams {
         }
 
         /** [Deprecated] This is a legacy parameter that no longer has any function. */
+        @Deprecated
         public Builder setPersistentToken(String persistentToken) {
           this.persistentToken = persistentToken;
           return this;
