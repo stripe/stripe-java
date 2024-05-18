897954829d15a80f4b477cee22f06f086d182773
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-18 08:26:23 +0000

    Update generated code for v1042

--- src/main/java/com/stripe/param/PaymentIntentCreateParams.java
+++ src/main/java/com/stripe/param/PaymentIntentCreateParams.java
@@ -5817,7 +5817,7 @@ public class PaymentIntentCreateParams extends ApiRequestParams {
     Swish swish;
 
     /**
-     * If this is a Twint PaymentMethod, this hash contains details about the Twint payment method.
+     * If this is a TWINT PaymentMethod, this hash contains details about the TWINT payment method.
      */
     @SerializedName("twint")
     Twint twint;
@@ -6498,7 +6498,7 @@ public class PaymentIntentCreateParams extends ApiRequestParams {
       }
 
       /**
-       * If this is a Twint PaymentMethod, this hash contains details about the Twint payment
+       * If this is a TWINT PaymentMethod, this hash contains details about the TWINT payment
        * method.
        */
       public Builder setTwint(PaymentIntentCreateParams.PaymentMethodData.Twint twint) {
