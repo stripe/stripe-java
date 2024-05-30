5aea789194de36088a96d939cdf4c80870ab0e14
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-05-30 02:40:58 +0000

    Update generated code for v1056

--- src/main/java/com/stripe/model/BankAccount.java
+++ src/main/java/com/stripe/model/BankAccount.java
@@ -598,8 +598,9 @@ public class BankAccount extends ApiResource
        * verification_failed_representative_authority}, {@code
        * verification_failed_residential_address}, {@code verification_failed_tax_id_match}, {@code
        * verification_failed_tax_id_not_issued}, {@code verification_missing_directors}, {@code
-       * verification_missing_executives}, {@code verification_missing_owners}, or {@code
-       * verification_requires_additional_memorandum_of_associations}.
+       * verification_missing_executives}, {@code verification_missing_owners}, {@code
+       * verification_requires_additional_memorandum_of_associations}, or {@code
+       * verification_requires_additional_proof_of_registration}.
        */
       @SerializedName("code")
       String code;
@@ -714,8 +715,9 @@ public class BankAccount extends ApiResource
        * verification_failed_representative_authority}, {@code
        * verification_failed_residential_address}, {@code verification_failed_tax_id_match}, {@code
        * verification_failed_tax_id_not_issued}, {@code verification_missing_directors}, {@code
-       * verification_missing_executives}, {@code verification_missing_owners}, or {@code
-       * verification_requires_additional_memorandum_of_associations}.
+       * verification_missing_executives}, {@code verification_missing_owners}, {@code
+       * verification_requires_additional_memorandum_of_associations}, or {@code
+       * verification_requires_additional_proof_of_registration}.
        */
       @SerializedName("code")
       String code;
