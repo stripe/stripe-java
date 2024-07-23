060f511a884cd4f956128df6d3383cba6bd8d2b2
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-07-23 15:44:27 +0000

    Update generated code for v1145

--- src/main/java/com/stripe/param/PaymentMethodConfigurationUpdateParams.java
+++ src/main/java/com/stripe/param/PaymentMethodConfigurationUpdateParams.java
@@ -366,6 +366,13 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
   Swish swish;
 
   /**
+   * Twint is a payment method popular in Switzerland. It allows customers to pay using their mobile
+   * phone. Check this <a href="https://docs.stripe.com/payments/twint">page</a> for more details.
+   */
+  @SerializedName("twint")
+  Twint twint;
+
+  /**
    * Stripe users in the United States can accept ACH direct debit payments from customers with a US
    * bank account using the Automated Clearing House (ACH) payments system operated by Nacha. Check
    * this <a href="https://stripe.com/docs/payments/ach-debit">page</a> for more details.
@@ -433,6 +440,7 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
       SepaDebit sepaDebit,
       Sofort sofort,
       Swish swish,
+      Twint twint,
       UsBankAccount usBankAccount,
       WechatPay wechatPay,
       Zip zip) {
@@ -477,6 +485,7 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
     this.sepaDebit = sepaDebit;
     this.sofort = sofort;
     this.swish = swish;
+    this.twint = twint;
     this.usBankAccount = usBankAccount;
     this.wechatPay = wechatPay;
     this.zip = zip;
@@ -569,6 +578,8 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
 
     private Swish swish;
 
+    private Twint twint;
+
     private UsBankAccount usBankAccount;
 
     private WechatPay wechatPay;
@@ -619,6 +630,7 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
           this.sepaDebit,
           this.sofort,
           this.swish,
+          this.twint,
           this.usBankAccount,
           this.wechatPay,
           this.zip);
@@ -1103,6 +1115,16 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
      */
     public Builder setSwish(PaymentMethodConfigurationUpdateParams.Swish swish) {
       this.swish = swish;
+      return this;
+    }
+
+    /**
+     * Twint is a payment method popular in Switzerland. It allows customers to pay using their
+     * mobile phone. Check this <a href="https://docs.stripe.com/payments/twint">page</a> for more
+     * details.
+     */
+    public Builder setTwint(PaymentMethodConfigurationUpdateParams.Twint twint) {
+      this.twint = twint;
       return this;
     }
 
@@ -7167,6 +7189,169 @@ public class PaymentMethodConfigurationUpdateParams extends ApiRequestParams {
         /** The account's preference for whether or not to display this payment method. */
         public Builder setPreference(
             PaymentMethodConfigurationUpdateParams.Swish.DisplayPreference.Preference preference) {
+          this.preference = preference;
+          return this;
+        }
+      }
+
+      public enum Preference implements ApiRequestParams.EnumParam {
+        @SerializedName("none")
+        NONE("none"),
+
+        @SerializedName("off")
+        OFF("off"),
+
+        @SerializedName("on")
+        ON("on");
+
+        @Getter(onMethod_ = {@Override})
+        private final String value;
+
+        Preference(String value) {
+          this.value = value;
+        }
+      }
+    }
+  }
+
+  @Getter
+  public static class Twint {
+    /** Whether or not the payment method should be displayed. */
+    @SerializedName("display_preference")
+    DisplayPreference displayPreference;
+
+    /**
+     * Map of extra parameters for custom features not available in this client library. The content
+     * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+     * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+     * param object. Effectively, this map is flattened to its parent instance.
+     */
+    @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+    Map<String, Object> extraParams;
+
+    private Twint(DisplayPreference displayPreference, Map<String, Object> extraParams) {
+      this.displayPreference = displayPreference;
+      this.extraParams = extraParams;
+    }
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    public static class Builder {
+      private DisplayPreference displayPreference;
+
+      private Map<String, Object> extraParams;
+
+      /** Finalize and obtain parameter instance from this builder. */
+      public PaymentMethodConfigurationUpdateParams.Twint build() {
+        return new PaymentMethodConfigurationUpdateParams.Twint(
+            this.displayPreference, this.extraParams);
+      }
+
+      /** Whether or not the payment method should be displayed. */
+      public Builder setDisplayPreference(
+          PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference displayPreference) {
+        this.displayPreference = displayPreference;
+        return this;
+      }
+
+      /**
+       * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+       * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+       * PaymentMethodConfigurationUpdateParams.Twint#extraParams} for the field documentation.
+       */
+      public Builder putExtraParam(String key, Object value) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.put(key, value);
+        return this;
+      }
+
+      /**
+       * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+       * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+       * See {@link PaymentMethodConfigurationUpdateParams.Twint#extraParams} for the field
+       * documentation.
+       */
+      public Builder putAllExtraParam(Map<String, Object> map) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.putAll(map);
+        return this;
+      }
+    }
+
+    @Getter
+    public static class DisplayPreference {
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      /** The account's preference for whether or not to display this payment method. */
+      @SerializedName("preference")
+      Preference preference;
+
+      private DisplayPreference(Map<String, Object> extraParams, Preference preference) {
+        this.extraParams = extraParams;
+        this.preference = preference;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private Map<String, Object> extraParams;
+
+        private Preference preference;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference build() {
+          return new PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference(
+              this.extraParams, this.preference);
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link
+         * PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference#extraParams} for the field
+         * documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link
+         * PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference#extraParams} for the field
+         * documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+
+        /** The account's preference for whether or not to display this payment method. */
+        public Builder setPreference(
+            PaymentMethodConfigurationUpdateParams.Twint.DisplayPreference.Preference preference) {
           this.preference = preference;
           return this;
         }
