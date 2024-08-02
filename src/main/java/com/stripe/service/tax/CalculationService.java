789c9d31bd98514e51eeb6161441d5f7d31bbda4
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-02 12:48:17 +0000

    Update generated code for v1171

--- src/main/java/com/stripe/service/tax/CalculationService.java
+++ src/main/java/com/stripe/service/tax/CalculationService.java
@@ -12,12 +12,41 @@ import com.stripe.net.BaseAddress;
 import com.stripe.net.RequestOptions;
 import com.stripe.net.StripeResponseGetter;
 import com.stripe.param.tax.CalculationCreateParams;
+import com.stripe.param.tax.CalculationRetrieveParams;
 
 public final class CalculationService extends ApiService {
   public CalculationService(StripeResponseGetter responseGetter) {
     super(responseGetter);
   }
 
+  /** Retrieves a Tax {@code Calculation} object, if the calculation hasn’t expired. */
+  public Calculation retrieve(String calculation, CalculationRetrieveParams params)
+      throws StripeException {
+    return retrieve(calculation, params, (RequestOptions) null);
+  }
+  /** Retrieves a Tax {@code Calculation} object, if the calculation hasn’t expired. */
+  public Calculation retrieve(String calculation, RequestOptions options) throws StripeException {
+    return retrieve(calculation, (CalculationRetrieveParams) null, options);
+  }
+  /** Retrieves a Tax {@code Calculation} object, if the calculation hasn’t expired. */
+  public Calculation retrieve(String calculation) throws StripeException {
+    return retrieve(calculation, (CalculationRetrieveParams) null, (RequestOptions) null);
+  }
+  /** Retrieves a Tax {@code Calculation} object, if the calculation hasn’t expired. */
+  public Calculation retrieve(
+      String calculation, CalculationRetrieveParams params, RequestOptions options)
+      throws StripeException {
+    String path = String.format("/v1/tax/calculations/%s", ApiResource.urlEncodeId(calculation));
+    ApiRequest request =
+        new ApiRequest(
+            BaseAddress.API,
+            ApiResource.RequestMethod.GET,
+            path,
+            ApiRequestParams.paramsToMap(params),
+            options,
+            ApiMode.V1);
+    return this.request(request, Calculation.class);
+  }
   /** Calculates tax based on the input and returns a Tax {@code Calculation} object. */
   public Calculation create(CalculationCreateParams params) throws StripeException {
     return create(params, (RequestOptions) null);
