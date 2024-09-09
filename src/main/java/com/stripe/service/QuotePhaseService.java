b55fcb06aa383da7f984e47e1473aa5928b9c583
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-09-09 19:11:48 +0000

    Update generated code for v1253

--- src/main/java/com/stripe/service/QuotePhaseService.java
+++ src/main/java/com/stripe/service/QuotePhaseService.java
@@ -1,10 +1,8 @@
 // File generated from our OpenAPI spec
 package com.stripe.service;
 
-import com.google.gson.reflect.TypeToken;
 import com.stripe.exception.StripeException;
 import com.stripe.model.QuotePhase;
-import com.stripe.model.StripeCollection;
 import com.stripe.net.ApiRequest;
 import com.stripe.net.ApiRequestParams;
 import com.stripe.net.ApiResource;
@@ -12,7 +10,6 @@ import com.stripe.net.ApiService;
 import com.stripe.net.BaseAddress;
 import com.stripe.net.RequestOptions;
 import com.stripe.net.StripeResponseGetter;
-import com.stripe.param.QuotePhaseListParams;
 import com.stripe.param.QuotePhaseRetrieveParams;
 
 public final class QuotePhaseService extends ApiService {
@@ -20,23 +17,6 @@ public final class QuotePhaseService extends ApiService {
     super(responseGetter);
   }
 
-  /** Returns a list of quote phases. */
-  public StripeCollection<QuotePhase> list(QuotePhaseListParams params) throws StripeException {
-    return list(params, (RequestOptions) null);
-  }
-  /** Returns a list of quote phases. */
-  public StripeCollection<QuotePhase> list(QuotePhaseListParams params, RequestOptions options)
-      throws StripeException {
-    String path = "/v1/quote_phases";
-    ApiRequest request =
-        new ApiRequest(
-            BaseAddress.API,
-            ApiResource.RequestMethod.GET,
-            path,
-            ApiRequestParams.paramsToMap(params),
-            options);
-    return this.request(request, new TypeToken<StripeCollection<QuotePhase>>() {}.getType());
-  }
   /** Retrieves the quote phase with the given ID. */
   public QuotePhase retrieve(String quotePhase, QuotePhaseRetrieveParams params)
       throws StripeException {
