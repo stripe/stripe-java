782fc2b5a60b7d527cbde5ddf6819f5904c39b7d
Ramya Rao <ramya@stripe.com>
2024-09-24 16:39:33 -0700

    Use the right events in tests

--- src/test/java/com/stripe/model/v2/EventTests.java
+++ src/test/java/com/stripe/model/v2/EventTests.java
@@ -3,8 +3,9 @@ package com.stripe.model.v2;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.stripe.BaseStripeTest;
-import com.stripe.events.FinancialAccountBalanceOpenedEvent;
+import com.stripe.events.V1BillingMeterErrorReportTriggeredEvent;
 import com.stripe.exception.StripeException;
+import com.stripe.model.billing.Meter;
 import com.stripe.net.ApiResource;
 import java.io.IOException;
 import java.time.Instant;
@@ -22,12 +23,12 @@ public class EventTests extends BaseStripeTest {
         "{\n"
             + "  \"id\": \"evt_234\",\n"
             + "  \"object\": \"event\",\n"
-            + "  \"type\": \"financial_account.balance.opened\",\n"
+            + "  \"type\": \"v1.billing.meter.error_report_triggered\",\n"
             + "  \"created\": \"2022-02-15T00:27:45.330Z\",\n"
             + "  \"related_object\": {\n"
-            + "    \"id\": \"fa_123\",\n"
-            + "    \"type\": \"financial_account\",\n"
-            + "    \"url\": \"/v2/financial_accounts/fa_123\",\n"
+            + "    \"id\": \"meter_123\",\n"
+            + "    \"type\": \"meter\",\n"
+            + "    \"url\": \"/v1/meters/meter_123\",\n"
             + "    \"stripe_context\": \"acct_123\"\n"
             + "  }\n"
             + "}";
@@ -35,16 +36,16 @@ public class EventTests extends BaseStripeTest {
         "{\n"
             + "  \"id\": \"evt_234\",\n"
             + "  \"object\": \"event\",\n"
-            + "  \"type\": \"financial_account.balance.opened\",\n"
+            + "  \"type\": \"v1.billing.meter.error_report_triggered\",\n"
             + "  \"created\": \"2022-02-15T00:27:45.330Z\",\n"
             + "  \"related_object\": {\n"
-            + "    \"id\": \"fa_123\",\n"
-            + "    \"type\": \"financial_account\",\n"
-            + "    \"url\": \"/v2/financial_accounts/fa_123\",\n"
+            + "    \"id\": \"meter_123\",\n"
+            + "    \"type\": \"meter\",\n"
+            + "    \"url\": \"/v1/meters/meter_123\",\n"
             + "    \"stripe_context\": \"acct_123\"\n"
             + "  },\n"
             + "  \"data\": {\n"
-            + "    \"type\": \"foo\"\n"
+            + "    \"developerMessageSummary\": \"foo\"\n"
             + "  }\n"
             + "}";
     v2UnknownEventPayload =
@@ -54,9 +55,9 @@ public class EventTests extends BaseStripeTest {
             + "  \"type\": \"financial_account.features_updated\",\n"
             + "  \"created\": \"2022-02-15T00:27:45.330Z\",\n"
             + "  \"related_object\": {\n"
-            + "    \"id\": \"fa_123\",\n"
+            + "    \"id\": \"meter_123\",\n"
             + "    \"type\": \"financial_account\",\n"
-            + "    \"url\": \"/v2/financial_accounts/fa_123\",\n"
+            + "    \"url\": \"/v2/financial_accounts/meter_123\",\n"
             + "    \"stripe_context\": \"acct_123\"\n"
             + "  },\n"
             + "  \"data\": {\n"
@@ -78,47 +79,47 @@ public class EventTests extends BaseStripeTest {
 
   @Test
   public void parsesV2Event() {
-    FinancialAccountBalanceOpenedEvent event =
-        (FinancialAccountBalanceOpenedEvent) Event.parse(v2PayloadNoData);
+    V1BillingMeterErrorReportTriggeredEvent event =
+        (V1BillingMeterErrorReportTriggeredEvent) Event.parse(v2PayloadNoData);
     assertEquals("evt_234", event.getId());
-    assertEquals("financial_account.balance.opened", event.getType());
+    assertEquals("v1.billing.meter.error_report_triggered", event.getType());
     assertEquals(Instant.parse("2022-02-15T00:27:45.330Z"), event.getCreated());
 
-    assertEquals("fa_123", event.getRelatedObject().getId());
-    assertEquals("financial_account", event.getRelatedObject().getType());
-    assertEquals("/v2/financial_accounts/fa_123", event.getRelatedObject().getUrl());
+    assertEquals("meter_123", event.getRelatedObject().getId());
+    assertEquals("meter", event.getRelatedObject().getType());
+    assertEquals("/v1/meters/meter_123", event.getRelatedObject().getUrl());
   }
 
   @Test
   public void parsesV2EventAndDeserializesEventData() throws StripeException {
-    FinancialAccountBalanceOpenedEvent event =
-        (FinancialAccountBalanceOpenedEvent) Event.parse(v2PayloadWithData);
+    V1BillingMeterErrorReportTriggeredEvent event =
+        (V1BillingMeterErrorReportTriggeredEvent) Event.parse(v2PayloadWithData);
     event.setResponseGetter(networkSpy);
     stubRequest(
         ApiResource.RequestMethod.GET,
-        String.format("/v2/events/%s", event.getId()),
+        String.format("/v2/core/events/%s", event.getId()),
         null,
         Event.class,
         v2PayloadWithData);
 
-    FinancialAccountBalanceOpenedEvent.EventData data = event.getData();
+        V1BillingMeterErrorReportTriggeredEvent.EventData data = event.getData();
 
-    assertEquals("foo", data.getType());
+    assertEquals("foo", data.getDeveloperMessageSummary());
   }
 
   @Test
   public void retrieveObjectFetchesAndDeserializesObject() throws StripeException, IOException {
-    FinancialAccountBalanceOpenedEvent event =
-        (FinancialAccountBalanceOpenedEvent) Event.parse(v2PayloadNoData);
+    V1BillingMeterErrorReportTriggeredEvent event =
+        (V1BillingMeterErrorReportTriggeredEvent) Event.parse(v2PayloadNoData);
     event.setResponseGetter(networkSpy);
     stubRequest(
         ApiResource.RequestMethod.GET,
-        "/v2/financial_accounts/fa_123",
+        "/v1/meters/meter_123",
         null,
-        FinancialAccount.class,
+        Meter.class,
         getResourceAsString("/api_fixtures/financial_account.json"));
 
-    FinancialAccount data = event.fetchRelatedObject();
+        Meter data = event.fetchRelatedObject();
 
     assertEquals(data.getId(), event.getRelatedObject().id);
   }
@@ -133,7 +134,7 @@ public class EventTests extends BaseStripeTest {
   //   event.setResponseGetter(networkSpy);
   //   stubRequest(
   //       ApiResource.RequestMethod.GET,
-  //       "/v2/financial_accounts/fa_123",
+  //       "/v2/financial_accounts/meter_123",
   //       null,
   //       StripeRawJsonObject.class,
   //       getResourceAsString("/api_fixtures/card.json"));
