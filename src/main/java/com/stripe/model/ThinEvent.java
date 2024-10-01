d7f76c9f5d216f26f0c80ac53b742ad2859d4558
prathmesh-stripe <165320323+prathmesh-stripe@users.noreply.github.com>
2024-10-01 11:54:51 -0400

    Added better description for ThinEvent class (#1885)

--- src/main/java/com/stripe/model/ThinEvent.java
+++ src/main/java/com/stripe/model/ThinEvent.java
@@ -4,22 +4,31 @@ import com.google.gson.annotations.SerializedName;
 import java.time.Instant;
 import lombok.Getter;
 
+/**
+ * ThinEvent represents the json that's delivered from an Event Destination. It's a basic class with
+ * no additional methods or properties. Use it to check basic information about a delivered event.
+ * If you want more details, use `stripeClient.v2().core().events().retrieve(thin_event.id)` to
+ * fetch the full event object.
+ */
 @Getter
 public class ThinEvent {
+  /** Unique identifier for the event. */
   @SerializedName("id")
   public String id;
 
+  /** The type of the event. */
   @SerializedName("type")
   public String type;
 
+  /** Time at which the object was created. */
   @SerializedName("created")
   public Instant created;
 
-  // this is optional, and may be null
+  /** [Optional] Authentication context needed to fetch the event or related object. */
   @SerializedName("context")
   public String context;
 
-  // this is optional, and may be null
+  /** [Optional] Object containing the reference to API resource relevant to the event. */
   @SerializedName("related_object")
   public ThinEventRelatedObject relatedObject;
 }
