fda553da4a0fe4f12dc504481e1b1ebfb96da589
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-12-06 16:32:29 +0000

    Update generated code for v1387

--- src/main/java/com/stripe/model/issuing/Authorization.java
+++ src/main/java/com/stripe/model/issuing/Authorization.java
@@ -1113,8 +1113,8 @@ public class Authorization extends ApiResource
      * <p>One of {@code account_disabled}, {@code card_active}, {@code card_canceled}, {@code
      * card_expired}, {@code card_inactive}, {@code cardholder_blocked}, {@code
      * cardholder_inactive}, {@code cardholder_verification_required}, {@code
-     * insecure_authorization_method}, {@code insufficient_funds}, {@code not_allowed}, {@code
-     * pin_blocked}, {@code spending_controls}, {@code suspected_fraud}, {@code
+     * insecure_authorization_method}, {@code insufficient_funds}, {@code network_fallback}, {@code
+     * not_allowed}, {@code pin_blocked}, {@code spending_controls}, {@code suspected_fraud}, {@code
      * verification_failed}, {@code webhook_approved}, {@code webhook_declined}, {@code
      * webhook_error}, or {@code webhook_timeout}.
      */
