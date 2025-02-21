483c92fff9de281814693347c0015a1f5a38f2a5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-21 15:20:17 +0000

    Update generated code for v1501

--- src/main/java/com/stripe/param/AccountPersonUpdateParams.java
+++ src/main/java/com/stripe/param/AccountPersonUpdateParams.java
@@ -148,7 +148,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
    * any jurisdiction.
    */
   @SerializedName("political_exposure")
-  PoliticalExposure politicalExposure;
+  Object politicalExposure;
 
   /** The person's registered address. */
   @SerializedName("registered_address")
@@ -191,7 +191,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
       Object nationality,
       Object personToken,
       Object phone,
-      PoliticalExposure politicalExposure,
+      Object politicalExposure,
       RegisteredAddress registeredAddress,
       Relationship relationship,
       Object ssnLast4,
@@ -280,7 +280,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
 
     private Object phone;
 
-    private PoliticalExposure politicalExposure;
+    private Object politicalExposure;
 
     private RegisteredAddress registeredAddress;
 
@@ -726,8 +726,17 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
      * related persons, declares that they hold or have held an important public job or function, in
      * any jurisdiction.
      */
-    public Builder setPoliticalExposure(
-        AccountPersonUpdateParams.PoliticalExposure politicalExposure) {
+    public Builder setPoliticalExposure(String politicalExposure) {
+      this.politicalExposure = politicalExposure;
+      return this;
+    }
+
+    /**
+     * Indicates if the person or any of their representatives, family members, or other closely
+     * related persons, declares that they hold or have held an important public job or function, in
+     * any jurisdiction.
+     */
+    public Builder setPoliticalExposure(EmptyParam politicalExposure) {
       this.politicalExposure = politicalExposure;
       return this;
     }
@@ -2815,21 +2824,6 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
           return this;
         }
       }
-    }
-  }
-
-  public enum PoliticalExposure implements ApiRequestParams.EnumParam {
-    @SerializedName("existing")
-    EXISTING("existing"),
-
-    @SerializedName("none")
-    NONE("none");
-
-    @Getter(onMethod_ = {@Override})
-    private final String value;
-
-    PoliticalExposure(String value) {
-      this.value = value;
     }
   }
 }
