6f3e5de4c2445a3373d363a2227f024ff7f46274
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-25 17:32:13 +0000

    Update generated code for v1508

--- src/main/java/com/stripe/param/WebhookEndpointCreateParams.java
+++ src/main/java/com/stripe/param/WebhookEndpointCreateParams.java
@@ -604,7 +604,13 @@ public class WebhookEndpointCreateParams extends ApiRequestParams {
     VERSION_2025_01_27_ACACIA("2025-01-27.acacia"),
 
     @SerializedName("2025-02-24.acacia")
-    VERSION_2025_02_24_ACACIA("2025-02-24.acacia");
+    VERSION_2025_02_24_ACACIA("2025-02-24.acacia"),
+
+    @SerializedName("2025-03-01.dashboard")
+    VERSION_2025_03_01_DASHBOARD("2025-03-01.dashboard"),
+
+    @SerializedName("2025-03-31.basil")
+    VERSION_2025_03_31_BASIL("2025-03-31.basil");
 
     @Getter(onMethod_ = {@Override})
     private final String value;
