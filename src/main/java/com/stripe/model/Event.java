c84a32b3bf64ec765e6ffc90c8c9d00e41a74c88
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-03-28 23:22:19 +0000

    Update generated code for v1641

--- src/main/java/com/stripe/model/Event.java
+++ src/main/java/com/stripe/model/Event.java
@@ -240,7 +240,7 @@ public class Event extends ApiResource implements HasId {
    * treasury.received_debit.created}, {@code billing.credit_balance_transaction.created}, {@code
    * billing.credit_grant.created}, {@code billing.credit_grant.updated}, {@code
    * billing.meter.created}, {@code billing.meter.deactivated}, {@code billing.meter.reactivated},
-   * or {@code billing.meter.updated}.
+   * {@code billing.meter.updated}, or {@code balance_settings.updated}.
    */
   @SerializedName("type")
   String type;
