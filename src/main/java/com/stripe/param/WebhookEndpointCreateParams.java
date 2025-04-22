a0f69b781387c69ef44d276dbe27d2234de9bd3e
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-04-22 15:43:24 +0000

    Update generated code for v1716

--- src/main/java/com/stripe/param/WebhookEndpointCreateParams.java
+++ src/main/java/com/stripe/param/WebhookEndpointCreateParams.java
@@ -665,6 +665,9 @@ public class WebhookEndpointCreateParams extends ApiRequestParams {
     @SerializedName("balance.available")
     BALANCE__AVAILABLE("balance.available"),
 
+    @SerializedName("balance_settings.updated")
+    BALANCE_SETTINGS__UPDATED("balance_settings.updated"),
+
     @SerializedName("billing.alert.triggered")
     BILLING__ALERT__TRIGGERED("billing.alert.triggered"),
 
@@ -1500,10 +1503,7 @@ public class WebhookEndpointCreateParams extends ApiRequestParams {
     BILLING__METER__REACTIVATED("billing.meter.reactivated"),
 
     @SerializedName("billing.meter.updated")
-    BILLING__METER__UPDATED("billing.meter.updated"),
-
-    @SerializedName("balance_settings.updated")
-    BALANCE_SETTINGS__UPDATED("balance_settings.updated");
+    BILLING__METER__UPDATED("billing.meter.updated");
 
     @Getter(onMethod_ = {@Override})
     private final String value;
