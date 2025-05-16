dbdf13d7c55f66b5565410ed3f86d20a3e296e02
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-05-16 09:46:18 +0000

    Update generated code for v1764

--- src/main/java/com/stripe/model/tax/Association.java
+++ src/main/java/com/stripe/model/tax/Association.java
@@ -4,12 +4,14 @@ package com.stripe.model.tax;
 import com.google.gson.annotations.SerializedName;
 import com.stripe.exception.StripeException;
 import com.stripe.model.HasId;
+import com.stripe.model.StripeObject;
 import com.stripe.net.ApiRequest;
 import com.stripe.net.ApiRequestParams;
 import com.stripe.net.ApiResource;
 import com.stripe.net.BaseAddress;
 import com.stripe.net.RequestOptions;
 import com.stripe.param.tax.AssociationFindParams;
+import java.util.List;
 import java.util.Map;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -50,13 +52,9 @@ public class Association extends ApiResource implements HasId {
   @SerializedName("payment_intent")
   String paymentIntent;
 
-  /**
-   * Status of the Tax Association.
-   *
-   * <p>One of {@code committed}, or {@code errored}.
-   */
-  @SerializedName("status")
-  String status;
+  /** Information about the tax transactions linked to this payment intent. */
+  @SerializedName("tax_transaction_attempts")
+  List<Association.TaxTransactionAttempt> taxTransactionAttempts;
 
   /** Finds a tax association object by PaymentIntent id. */
   public static Association find(Map<String, Object> params) throws StripeException {
@@ -90,5 +88,60 @@ public class Association extends ApiResource implements HasId {
             ApiRequestParams.paramsToMap(params),
             options);
     return getGlobalResponseGetter().request(request, Association.class);
+  }
+
+  /**
+   * For more details about TaxTransactionAttempt, please refer to the <a
+   * href="https://docs.stripe.com/api">API Reference.</a>
+   */
+  @Getter
+  @Setter
+  @EqualsAndHashCode(callSuper = false)
+  public static class TaxTransactionAttempt extends StripeObject {
+    @SerializedName("committed")
+    Committed committed;
+
+    @SerializedName("errored")
+    Errored errored;
+
+    /** The source of the tax transaction attempt. This is either a refund or a payment intent. */
+    @SerializedName("source")
+    String source;
+
+    /** The status of the transaction attempt. This can be {@code errored} or {@code committed}. */
+    @SerializedName("status")
+    String status;
+
+    /**
+     * For more details about Committed, please refer to the <a
+     * href="https://docs.stripe.com/api">API Reference.</a>
+     */
+    @Getter
+    @Setter
+    @EqualsAndHashCode(callSuper = false)
+    public static class Committed extends StripeObject {
+      /** The <a href="https://stripe.com/docs/api/tax/transaction/object">Tax Transaction.</a> */
+      @SerializedName("transaction")
+      String transaction;
+    }
+
+    /**
+     * For more details about Errored, please refer to the <a href="https://docs.stripe.com/api">API
+     * Reference.</a>
+     */
+    @Getter
+    @Setter
+    @EqualsAndHashCode(callSuper = false)
+    public static class Errored extends StripeObject {
+      /**
+       * Details on why we couldn't commit the tax transaction.
+       *
+       * <p>One of {@code another_payment_associated_with_calculation}, {@code calculation_expired},
+       * {@code currency_mismatch}, {@code original_transaction_voided}, or {@code
+       * unique_reference_violation}.
+       */
+      @SerializedName("reason")
+      String reason;
+    }
   }
 }
