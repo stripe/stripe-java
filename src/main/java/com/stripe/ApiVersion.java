97e7e5b2821ee2f4d8252e2f743faabf53a5c6cb
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-06-17 17:56:32 +0000

    Update generated code for v1800 and 2025-05-28.basil

--- src/main/java/com/stripe/ApiVersion.java
+++ src/main/java/com/stripe/ApiVersion.java
@@ -4,5 +4,4 @@ package com.stripe;
 final class ApiVersion {
   public static final String CURRENT = "2025-05-28.basil";
   public static final String CURRENT_MAJOR = "basil";
-  public static final String CURRENT_MONTHLY = "2025-05-28";
 }
