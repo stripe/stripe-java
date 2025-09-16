88c862df08cc71f5e96ce598beb9c423754b2098
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-09-16 22:48:16 +0000

    Update generated code for v1977 and 2025-09-30.preview

--- src/main/java/com/stripe/param/PaymentIntentConfirmParams.java
+++ src/main/java/com/stripe/param/PaymentIntentConfirmParams.java
@@ -51,6 +51,10 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
   @SerializedName("error_on_requires_action")
   Boolean errorOnRequiresAction;
 
+  /** The list of payment method types to exclude from use with this payment. */
+  @SerializedName("excluded_payment_method_types")
+  Object excludedPaymentMethodTypes;
+
   /** Specifies which fields in the response should be expanded. */
   @SerializedName("expand")
   List<String> expand;
@@ -195,6 +199,7 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
       CaptureMethod captureMethod,
       String confirmationToken,
       Boolean errorOnRequiresAction,
+      Object excludedPaymentMethodTypes,
       List<String> expand,
       Map<String, Object> extraParams,
       String fxQuote,
@@ -218,6 +223,7 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
     this.captureMethod = captureMethod;
     this.confirmationToken = confirmationToken;
     this.errorOnRequiresAction = errorOnRequiresAction;
+    this.excludedPaymentMethodTypes = excludedPaymentMethodTypes;
     this.expand = expand;
     this.extraParams = extraParams;
     this.fxQuote = fxQuote;
@@ -252,6 +258,8 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
     private String confirmationToken;
 
     private Boolean errorOnRequiresAction;
+
+    private Object excludedPaymentMethodTypes;
 
     private List<String> expand;
 
@@ -297,6 +305,7 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
           this.captureMethod,
           this.confirmationToken,
           this.errorOnRequiresAction,
+          this.excludedPaymentMethodTypes,
           this.expand,
           this.extraParams,
           this.fxQuote,
@@ -380,6 +389,55 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
      */
     public Builder setErrorOnRequiresAction(Boolean errorOnRequiresAction) {
       this.errorOnRequiresAction = errorOnRequiresAction;
+      return this;
+    }
+
+    /**
+     * Add an element to `excludedPaymentMethodTypes` list. A list is initialized for the first
+     * `add/addAll` call, and subsequent calls adds additional elements to the original list. See
+     * {@link PaymentIntentConfirmParams#excludedPaymentMethodTypes} for the field documentation.
+     */
+    @SuppressWarnings("unchecked")
+    public Builder addExcludedPaymentMethodType(
+        PaymentIntentConfirmParams.ExcludedPaymentMethodType element) {
+      if (this.excludedPaymentMethodTypes == null
+          || this.excludedPaymentMethodTypes instanceof EmptyParam) {
+        this.excludedPaymentMethodTypes =
+            new ArrayList<PaymentIntentConfirmParams.ExcludedPaymentMethodType>();
+      }
+      ((List<PaymentIntentConfirmParams.ExcludedPaymentMethodType>) this.excludedPaymentMethodTypes)
+          .add(element);
+      return this;
+    }
+
+    /**
+     * Add all elements to `excludedPaymentMethodTypes` list. A list is initialized for the first
+     * `add/addAll` call, and subsequent calls adds additional elements to the original list. See
+     * {@link PaymentIntentConfirmParams#excludedPaymentMethodTypes} for the field documentation.
+     */
+    @SuppressWarnings("unchecked")
+    public Builder addAllExcludedPaymentMethodType(
+        List<PaymentIntentConfirmParams.ExcludedPaymentMethodType> elements) {
+      if (this.excludedPaymentMethodTypes == null
+          || this.excludedPaymentMethodTypes instanceof EmptyParam) {
+        this.excludedPaymentMethodTypes =
+            new ArrayList<PaymentIntentConfirmParams.ExcludedPaymentMethodType>();
+      }
+      ((List<PaymentIntentConfirmParams.ExcludedPaymentMethodType>) this.excludedPaymentMethodTypes)
+          .addAll(elements);
+      return this;
+    }
+
+    /** The list of payment method types to exclude from use with this payment. */
+    public Builder setExcludedPaymentMethodTypes(EmptyParam excludedPaymentMethodTypes) {
+      this.excludedPaymentMethodTypes = excludedPaymentMethodTypes;
+      return this;
+    }
+
+    /** The list of payment method types to exclude from use with this payment. */
+    public Builder setExcludedPaymentMethodTypes(
+        List<PaymentIntentConfirmParams.ExcludedPaymentMethodType> excludedPaymentMethodTypes) {
+      this.excludedPaymentMethodTypes = excludedPaymentMethodTypes;
       return this;
     }
 
@@ -30838,6 +30896,183 @@ public class PaymentIntentConfirmParams extends ApiRequestParams {
     private final String value;
 
     CaptureMethod(String value) {
+      this.value = value;
+    }
+  }
+
+  public enum ExcludedPaymentMethodType implements ApiRequestParams.EnumParam {
+    @SerializedName("acss_debit")
+    ACSS_DEBIT("acss_debit"),
+
+    @SerializedName("affirm")
+    AFFIRM("affirm"),
+
+    @SerializedName("afterpay_clearpay")
+    AFTERPAY_CLEARPAY("afterpay_clearpay"),
+
+    @SerializedName("alipay")
+    ALIPAY("alipay"),
+
+    @SerializedName("alma")
+    ALMA("alma"),
+
+    @SerializedName("amazon_pay")
+    AMAZON_PAY("amazon_pay"),
+
+    @SerializedName("au_becs_debit")
+    AU_BECS_DEBIT("au_becs_debit"),
+
+    @SerializedName("bacs_debit")
+    BACS_DEBIT("bacs_debit"),
+
+    @SerializedName("bancontact")
+    BANCONTACT("bancontact"),
+
+    @SerializedName("billie")
+    BILLIE("billie"),
+
+    @SerializedName("blik")
+    BLIK("blik"),
+
+    @SerializedName("boleto")
+    BOLETO("boleto"),
+
+    @SerializedName("card")
+    CARD("card"),
+
+    @SerializedName("cashapp")
+    CASHAPP("cashapp"),
+
+    @SerializedName("crypto")
+    CRYPTO("crypto"),
+
+    @SerializedName("customer_balance")
+    CUSTOMER_BALANCE("customer_balance"),
+
+    @SerializedName("eps")
+    EPS("eps"),
+
+    @SerializedName("fpx")
+    FPX("fpx"),
+
+    @SerializedName("giropay")
+    GIROPAY("giropay"),
+
+    @SerializedName("gopay")
+    GOPAY("gopay"),
+
+    @SerializedName("grabpay")
+    GRABPAY("grabpay"),
+
+    @SerializedName("id_bank_transfer")
+    ID_BANK_TRANSFER("id_bank_transfer"),
+
+    @SerializedName("ideal")
+    IDEAL("ideal"),
+
+    @SerializedName("kakao_pay")
+    KAKAO_PAY("kakao_pay"),
+
+    @SerializedName("klarna")
+    KLARNA("klarna"),
+
+    @SerializedName("konbini")
+    KONBINI("konbini"),
+
+    @SerializedName("kr_card")
+    KR_CARD("kr_card"),
+
+    @SerializedName("mb_way")
+    MB_WAY("mb_way"),
+
+    @SerializedName("mobilepay")
+    MOBILEPAY("mobilepay"),
+
+    @SerializedName("multibanco")
+    MULTIBANCO("multibanco"),
+
+    @SerializedName("naver_pay")
+    NAVER_PAY("naver_pay"),
+
+    @SerializedName("nz_bank_account")
+    NZ_BANK_ACCOUNT("nz_bank_account"),
+
+    @SerializedName("oxxo")
+    OXXO("oxxo"),
+
+    @SerializedName("p24")
+    P24("p24"),
+
+    @SerializedName("pay_by_bank")
+    PAY_BY_BANK("pay_by_bank"),
+
+    @SerializedName("payco")
+    PAYCO("payco"),
+
+    @SerializedName("paynow")
+    PAYNOW("paynow"),
+
+    @SerializedName("paypal")
+    PAYPAL("paypal"),
+
+    @SerializedName("paypay")
+    PAYPAY("paypay"),
+
+    @SerializedName("payto")
+    PAYTO("payto"),
+
+    @SerializedName("pix")
+    PIX("pix"),
+
+    @SerializedName("promptpay")
+    PROMPTPAY("promptpay"),
+
+    @SerializedName("qris")
+    QRIS("qris"),
+
+    @SerializedName("rechnung")
+    RECHNUNG("rechnung"),
+
+    @SerializedName("revolut_pay")
+    REVOLUT_PAY("revolut_pay"),
+
+    @SerializedName("samsung_pay")
+    SAMSUNG_PAY("samsung_pay"),
+
+    @SerializedName("satispay")
+    SATISPAY("satispay"),
+
+    @SerializedName("sepa_debit")
+    SEPA_DEBIT("sepa_debit"),
+
+    @SerializedName("shopeepay")
+    SHOPEEPAY("shopeepay"),
+
+    @SerializedName("sofort")
+    SOFORT("sofort"),
+
+    @SerializedName("stripe_balance")
+    STRIPE_BALANCE("stripe_balance"),
+
+    @SerializedName("swish")
+    SWISH("swish"),
+
+    @SerializedName("twint")
+    TWINT("twint"),
+
+    @SerializedName("us_bank_account")
+    US_BANK_ACCOUNT("us_bank_account"),
+
+    @SerializedName("wechat_pay")
+    WECHAT_PAY("wechat_pay"),
+
+    @SerializedName("zip")
+    ZIP("zip");
+
+    @Getter(onMethod_ = {@Override})
+    private final String value;
+
+    ExcludedPaymentMethodType(String value) {
       this.value = value;
     }
   }
