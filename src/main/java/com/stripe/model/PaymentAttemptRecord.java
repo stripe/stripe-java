e3817d8c779ab448a8077a3cad0562b5b856b378
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-10-07 21:37:58 +0000

    Update generated code for v2046 and

--- src/main/java/com/stripe/model/PaymentAttemptRecord.java
+++ src/main/java/com/stripe/model/PaymentAttemptRecord.java
@@ -1666,7 +1666,7 @@ public class PaymentAttemptRecord extends ApiResource implements HasId {
       /**
        * The blockchain network that the transaction was sent on.
        *
-       * <p>One of {@code base}, {@code ethereum}, or {@code polygon}.
+       * <p>One of {@code base}, {@code ethereum}, {@code polygon}, or {@code solana}.
        */
       @SerializedName("network")
       String network;
