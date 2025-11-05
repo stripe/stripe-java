ee5249b7c5cd0e2a843e3a3c56f71a12a3425160
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-11-05 22:43:12 +0000

    Update generated code for v2111 and

--- src/main/java/com/stripe/ApiVersion.java
+++ src/main/java/com/stripe/ApiVersion.java
@@ -2,6 +2,6 @@
 package com.stripe;
 
 final class ApiVersion {
-  public static final String CURRENT = "2025-11-17.clover";
+  public static final String CURRENT = "2025-10-29.clover";
   public static final String CURRENT_MAJOR = "clover";
 }
