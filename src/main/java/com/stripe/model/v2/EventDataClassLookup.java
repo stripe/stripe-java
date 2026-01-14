5b1856dfdc3353452eb8ba12ace07a9e3f40168c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-01-14 20:36:12 +0000

    Update generated code for v2151 and

--- src/main/java/com/stripe/model/v2/EventDataClassLookup.java
+++ src/main/java/com/stripe/model/v2/EventDataClassLookup.java
@@ -41,6 +41,53 @@ public final class EventDataClassLookup {
     eventClassLookup.put(
         "v1.billing.meter.no_meter_found", com.stripe.events.V1BillingMeterNoMeterFoundEvent.class);
     eventClassLookup.put(
+        "v2.core.account.closed", com.stripe.events.V2CoreAccountClosedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account.created", com.stripe.events.V2CoreAccountCreatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account.updated", com.stripe.events.V2CoreAccountUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.customer].capability_status_updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationCustomerCapabilityStatusUpdatedEvent
+            .class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.customer].updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationCustomerUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.merchant].capability_status_updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationMerchantCapabilityStatusUpdatedEvent
+            .class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.merchant].updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationMerchantUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.recipient].capability_status_updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationRecipientCapabilityStatusUpdatedEvent
+            .class);
+    eventClassLookup.put(
+        "v2.core.account[configuration.recipient].updated",
+        com.stripe.events.V2CoreAccountIncludingConfigurationRecipientUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[defaults].updated",
+        com.stripe.events.V2CoreAccountIncludingDefaultsUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[future_requirements].updated",
+        com.stripe.events.V2CoreAccountIncludingFutureRequirementsUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[identity].updated",
+        com.stripe.events.V2CoreAccountIncludingIdentityUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account[requirements].updated",
+        com.stripe.events.V2CoreAccountIncludingRequirementsUpdatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account_link.returned", com.stripe.events.V2CoreAccountLinkReturnedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account_person.created", com.stripe.events.V2CoreAccountPersonCreatedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account_person.deleted", com.stripe.events.V2CoreAccountPersonDeletedEvent.class);
+    eventClassLookup.put(
+        "v2.core.account_person.updated", com.stripe.events.V2CoreAccountPersonUpdatedEvent.class);
+    eventClassLookup.put(
         "v2.core.event_destination.ping", com.stripe.events.V2CoreEventDestinationPingEvent.class);
   }
 }
