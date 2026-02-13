ec5c1324e1d9729e942736ad40a13916f61ca09c
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-02-13 19:11:27 +0000

    Update generated code for v2172 and

--- src/main/java/com/stripe/param/SubscriptionPauseParams.java
+++ src/main/java/com/stripe/param/SubscriptionPauseParams.java
@@ -163,23 +163,26 @@ public class SubscriptionPauseParams extends ApiRequestParams {
     Map<String, Object> extraParams;
 
     /**
-     * Controls whether to debit for accrued metered usage in the current billing period. The
-     * default is {@code true}.
+     * Controls when to bill for metered usage in the current period. Defaults to {@code { type:
+     * "now" }}.
      */
-    @SerializedName("outstanding_usage")
-    Boolean outstandingUsage;
+    @SerializedName("outstanding_usage_through")
+    OutstandingUsageThrough outstandingUsageThrough;
 
     /**
-     * Controls whether to credit for licensed items in the current billing period. The default is
-     * {@code true}.
+     * Controls when to credit for unused time on licensed items. Defaults to {@code { type: "now"
+     * }}.
      */
-    @SerializedName("unused_time")
-    Boolean unusedTime;
+    @SerializedName("unused_time_from")
+    UnusedTimeFrom unusedTimeFrom;
 
-    private BillFor(Map<String, Object> extraParams, Boolean outstandingUsage, Boolean unusedTime) {
+    private BillFor(
+        Map<String, Object> extraParams,
+        OutstandingUsageThrough outstandingUsageThrough,
+        UnusedTimeFrom unusedTimeFrom) {
       this.extraParams = extraParams;
-      this.outstandingUsage = outstandingUsage;
-      this.unusedTime = unusedTime;
+      this.outstandingUsageThrough = outstandingUsageThrough;
+      this.unusedTimeFrom = unusedTimeFrom;
     }
 
     public static Builder builder() {
@@ -189,14 +192,14 @@ public class SubscriptionPauseParams extends ApiRequestParams {
     public static class Builder {
       private Map<String, Object> extraParams;
 
-      private Boolean outstandingUsage;
+      private OutstandingUsageThrough outstandingUsageThrough;
 
-      private Boolean unusedTime;
+      private UnusedTimeFrom unusedTimeFrom;
 
       /** Finalize and obtain parameter instance from this builder. */
       public SubscriptionPauseParams.BillFor build() {
         return new SubscriptionPauseParams.BillFor(
-            this.extraParams, this.outstandingUsage, this.unusedTime);
+            this.extraParams, this.outstandingUsageThrough, this.unusedTimeFrom);
       }
 
       /**
@@ -226,21 +229,199 @@ public class SubscriptionPauseParams extends ApiRequestParams {
       }
 
       /**
-       * Controls whether to debit for accrued metered usage in the current billing period. The
-       * default is {@code true}.
+       * Controls when to bill for metered usage in the current period. Defaults to {@code { type:
+       * "now" }}.
        */
-      public Builder setOutstandingUsage(Boolean outstandingUsage) {
-        this.outstandingUsage = outstandingUsage;
+      public Builder setOutstandingUsageThrough(
+          SubscriptionPauseParams.BillFor.OutstandingUsageThrough outstandingUsageThrough) {
+        this.outstandingUsageThrough = outstandingUsageThrough;
         return this;
       }
 
       /**
-       * Controls whether to credit for licensed items in the current billing period. The default is
-       * {@code true}.
+       * Controls when to credit for unused time on licensed items. Defaults to {@code { type: "now"
+       * }}.
        */
-      public Builder setUnusedTime(Boolean unusedTime) {
-        this.unusedTime = unusedTime;
+      public Builder setUnusedTimeFrom(
+          SubscriptionPauseParams.BillFor.UnusedTimeFrom unusedTimeFrom) {
+        this.unusedTimeFrom = unusedTimeFrom;
         return this;
+      }
+    }
+
+    @Getter
+    @EqualsAndHashCode(callSuper = false)
+    public static class OutstandingUsageThrough {
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      /** <strong>Required.</strong> When to bill metered usage in the current period. */
+      @SerializedName("type")
+      Type type;
+
+      private OutstandingUsageThrough(Map<String, Object> extraParams, Type type) {
+        this.extraParams = extraParams;
+        this.type = type;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private Map<String, Object> extraParams;
+
+        private Type type;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public SubscriptionPauseParams.BillFor.OutstandingUsageThrough build() {
+          return new SubscriptionPauseParams.BillFor.OutstandingUsageThrough(
+              this.extraParams, this.type);
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SubscriptionPauseParams.BillFor.OutstandingUsageThrough#extraParams} for
+         * the field documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SubscriptionPauseParams.BillFor.OutstandingUsageThrough#extraParams} for
+         * the field documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+
+        /** <strong>Required.</strong> When to bill metered usage in the current period. */
+        public Builder setType(SubscriptionPauseParams.BillFor.OutstandingUsageThrough.Type type) {
+          this.type = type;
+          return this;
+        }
+      }
+
+      public enum Type implements ApiRequestParams.EnumParam {
+        @SerializedName("none")
+        NONE("none"),
+
+        @SerializedName("now")
+        NOW("now");
+
+        @Getter(onMethod_ = {@Override})
+        private final String value;
+
+        Type(String value) {
+          this.value = value;
+        }
+      }
+    }
+
+    @Getter
+    @EqualsAndHashCode(callSuper = false)
+    public static class UnusedTimeFrom {
+      /**
+       * Map of extra parameters for custom features not available in this client library. The
+       * content in this map is not serialized under this field's {@code @SerializedName} value.
+       * Instead, each key/value pair is serialized as if the key is a root-level field (serialized)
+       * name in this param object. Effectively, this map is flattened to its parent instance.
+       */
+      @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+      Map<String, Object> extraParams;
+
+      /** <strong>Required.</strong> When to credit for unused time. */
+      @SerializedName("type")
+      Type type;
+
+      private UnusedTimeFrom(Map<String, Object> extraParams, Type type) {
+        this.extraParams = extraParams;
+        this.type = type;
+      }
+
+      public static Builder builder() {
+        return new Builder();
+      }
+
+      public static class Builder {
+        private Map<String, Object> extraParams;
+
+        private Type type;
+
+        /** Finalize and obtain parameter instance from this builder. */
+        public SubscriptionPauseParams.BillFor.UnusedTimeFrom build() {
+          return new SubscriptionPauseParams.BillFor.UnusedTimeFrom(this.extraParams, this.type);
+        }
+
+        /**
+         * Add a key/value pair to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SubscriptionPauseParams.BillFor.UnusedTimeFrom#extraParams} for the field
+         * documentation.
+         */
+        public Builder putExtraParam(String key, Object value) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.put(key, value);
+          return this;
+        }
+
+        /**
+         * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+         * `put/putAll` call, and subsequent calls add additional key/value pairs to the original
+         * map. See {@link SubscriptionPauseParams.BillFor.UnusedTimeFrom#extraParams} for the field
+         * documentation.
+         */
+        public Builder putAllExtraParam(Map<String, Object> map) {
+          if (this.extraParams == null) {
+            this.extraParams = new HashMap<>();
+          }
+          this.extraParams.putAll(map);
+          return this;
+        }
+
+        /** <strong>Required.</strong> When to credit for unused time. */
+        public Builder setType(SubscriptionPauseParams.BillFor.UnusedTimeFrom.Type type) {
+          this.type = type;
+          return this;
+        }
+      }
+
+      public enum Type implements ApiRequestParams.EnumParam {
+        @SerializedName("item_current_period_start")
+        ITEM_CURRENT_PERIOD_START("item_current_period_start"),
+
+        @SerializedName("none")
+        NONE("none"),
+
+        @SerializedName("now")
+        NOW("now");
+
+        @Getter(onMethod_ = {@Override})
+        private final String value;
+
+        Type(String value) {
+          this.value = value;
+        }
       }
     }
   }
