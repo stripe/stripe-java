40860a9cdefd70513abbf18e2c6bc5ddb9bbd946
Michael Broshi <mbroshi@stripe.com>
2026-03-16 18:34:16 -0400

    Fix fomatting

--- src/main/java/com/stripe/net/Authenticator.java
+++ src/main/java/com/stripe/net/Authenticator.java
@@ -3,14 +3,14 @@ package com.stripe.net;
 import com.stripe.exception.StripeException;
 
 /**
- * Interface for applying authentication to a request. This is used by {@link StripeResponseGetter} to
- * apply authentication to requests before they are sent. Implementations of this interface should
- * not modify the request in-place, but should instead return a new request with the appropriate
- * authentication headers applied.
- * The default implementation of this interface is {@link BearerTokenAuthenticator}, which
- * applies the API key as a Bearer token in the Authorization header. Users can also implement this
- * interface to provide custom authentication logic, often by wrapping the BearerTokenAuthenticator
- * and applying additional headers or logic as needed.
+ * Interface for applying authentication to a request. This is used by {@link StripeResponseGetter}
+ * to apply authentication to requests before they are sent. Implementations of this interface
+ * should not modify the request in-place, but should instead return a new request with the
+ * appropriate authentication headers applied. The default implementation of this interface is
+ * {@link BearerTokenAuthenticator}, which applies the API key as a Bearer token in the
+ * Authorization header. Users can also implement this interface to provide custom authentication
+ * logic, often by wrapping the BearerTokenAuthenticator and applying additional headers or logic as
+ * needed.
  */
 public interface Authenticator {
   /**
