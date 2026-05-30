4b4b34f47b51ce05f13daccc56ce7cd84e087cca
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2026-05-30 00:32:01 +0000

    Update generated code for v2280 and 4adb7cc8d107e5bfa5d86f090a0a9af8fc6ebbb2

--- src/main/java/com/stripe/param/AccountPersonUpdateParams.java
+++ src/main/java/com/stripe/param/AccountPersonUpdateParams.java
@@ -33,6 +33,10 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
   @SerializedName("address_kanji")
   AddressKanji addressKanji;
 
+  /** The person's place of birth. */
+  @SerializedName("birth_address")
+  BirthAddress birthAddress;
+
   /** The person's date of birth. */
   @SerializedName("dob")
   Object dob;
@@ -185,6 +189,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
       Address address,
       AddressKana addressKana,
       AddressKanji addressKanji,
+      BirthAddress birthAddress,
       Object dob,
       Documents documents,
       Object email,
@@ -217,6 +222,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
     this.address = address;
     this.addressKana = addressKana;
     this.addressKanji = addressKanji;
+    this.birthAddress = birthAddress;
     this.dob = dob;
     this.documents = documents;
     this.email = email;
@@ -259,6 +265,8 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
     private AddressKana addressKana;
 
     private AddressKanji addressKanji;
+
+    private BirthAddress birthAddress;
 
     private Object dob;
 
@@ -323,6 +331,7 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
           this.address,
           this.addressKana,
           this.addressKanji,
+          this.birthAddress,
           this.dob,
           this.documents,
           this.email,
@@ -377,6 +386,12 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
     /** The Kanji variation of the person's address (Japan only). */
     public Builder setAddressKanji(AccountPersonUpdateParams.AddressKanji addressKanji) {
       this.addressKanji = addressKanji;
+      return this;
+    }
+
+    /** The person's place of birth. */
+    public Builder setBirthAddress(AccountPersonUpdateParams.BirthAddress birthAddress) {
+      this.birthAddress = birthAddress;
       return this;
     }
 
@@ -1636,6 +1651,208 @@ public class AccountPersonUpdateParams extends ApiRequestParams {
       /** Town or cho-me. */
       public Builder setTown(EmptyParam town) {
         this.town = town;
+        return this;
+      }
+    }
+  }
+
+  @Getter
+  @EqualsAndHashCode(callSuper = false)
+  public static class BirthAddress {
+    /** City, district, suburb, town, or village. */
+    @SerializedName("city")
+    Object city;
+
+    /**
+     * Two-letter country code (<a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO
+     * 3166-1 alpha-2</a>).
+     */
+    @SerializedName("country")
+    Object country;
+
+    /**
+     * Map of extra parameters for custom features not available in this client library. The content
+     * in this map is not serialized under this field's {@code @SerializedName} value. Instead, each
+     * key/value pair is serialized as if the key is a root-level field (serialized) name in this
+     * param object. Effectively, this map is flattened to its parent instance.
+     */
+    @SerializedName(ApiRequestParams.EXTRA_PARAMS_KEY)
+    Map<String, Object> extraParams;
+
+    /** Address line 1, such as the street, PO Box, or company name. */
+    @SerializedName("line1")
+    Object line1;
+
+    /** Address line 2, such as the apartment, suite, unit, or building. */
+    @SerializedName("line2")
+    Object line2;
+
+    /** ZIP or postal code. */
+    @SerializedName("postal_code")
+    Object postalCode;
+
+    /**
+     * State, county, province, or region (<a href="https://en.wikipedia.org/wiki/ISO_3166-2">ISO
+     * 3166-2</a>).
+     */
+    @SerializedName("state")
+    Object state;
+
+    private BirthAddress(
+        Object city,
+        Object country,
+        Map<String, Object> extraParams,
+        Object line1,
+        Object line2,
+        Object postalCode,
+        Object state) {
+      this.city = city;
+      this.country = country;
+      this.extraParams = extraParams;
+      this.line1 = line1;
+      this.line2 = line2;
+      this.postalCode = postalCode;
+      this.state = state;
+    }
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    public static class Builder {
+      private Object city;
+
+      private Object country;
+
+      private Map<String, Object> extraParams;
+
+      private Object line1;
+
+      private Object line2;
+
+      private Object postalCode;
+
+      private Object state;
+
+      /** Finalize and obtain parameter instance from this builder. */
+      public AccountPersonUpdateParams.BirthAddress build() {
+        return new AccountPersonUpdateParams.BirthAddress(
+            this.city,
+            this.country,
+            this.extraParams,
+            this.line1,
+            this.line2,
+            this.postalCode,
+            this.state);
+      }
+
+      /** City, district, suburb, town, or village. */
+      public Builder setCity(String city) {
+        this.city = city;
+        return this;
+      }
+
+      /** City, district, suburb, town, or village. */
+      public Builder setCity(EmptyParam city) {
+        this.city = city;
+        return this;
+      }
+
+      /**
+       * Two-letter country code (<a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO
+       * 3166-1 alpha-2</a>).
+       */
+      public Builder setCountry(String country) {
+        this.country = country;
+        return this;
+      }
+
+      /**
+       * Two-letter country code (<a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">ISO
+       * 3166-1 alpha-2</a>).
+       */
+      public Builder setCountry(EmptyParam country) {
+        this.country = country;
+        return this;
+      }
+
+      /**
+       * Add a key/value pair to `extraParams` map. A map is initialized for the first `put/putAll`
+       * call, and subsequent calls add additional key/value pairs to the original map. See {@link
+       * AccountPersonUpdateParams.BirthAddress#extraParams} for the field documentation.
+       */
+      public Builder putExtraParam(String key, Object value) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.put(key, value);
+        return this;
+      }
+
+      /**
+       * Add all map key/value pairs to `extraParams` map. A map is initialized for the first
+       * `put/putAll` call, and subsequent calls add additional key/value pairs to the original map.
+       * See {@link AccountPersonUpdateParams.BirthAddress#extraParams} for the field documentation.
+       */
+      public Builder putAllExtraParam(Map<String, Object> map) {
+        if (this.extraParams == null) {
+          this.extraParams = new HashMap<>();
+        }
+        this.extraParams.putAll(map);
+        return this;
+      }
+
+      /** Address line 1, such as the street, PO Box, or company name. */
+      public Builder setLine1(String line1) {
+        this.line1 = line1;
+        return this;
+      }
+
+      /** Address line 1, such as the street, PO Box, or company name. */
+      public Builder setLine1(EmptyParam line1) {
+        this.line1 = line1;
+        return this;
+      }
+
+      /** Address line 2, such as the apartment, suite, unit, or building. */
+      public Builder setLine2(String line2) {
+        this.line2 = line2;
+        return this;
+      }
+
+      /** Address line 2, such as the apartment, suite, unit, or building. */
+      public Builder setLine2(EmptyParam line2) {
+        this.line2 = line2;
+        return this;
+      }
+
+      /** ZIP or postal code. */
+      public Builder setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+        return this;
+      }
+
+      /** ZIP or postal code. */
+      public Builder setPostalCode(EmptyParam postalCode) {
+        this.postalCode = postalCode;
+        return this;
+      }
+
+      /**
+       * State, county, province, or region (<a href="https://en.wikipedia.org/wiki/ISO_3166-2">ISO
+       * 3166-2</a>).
+       */
+      public Builder setState(String state) {
+        this.state = state;
+        return this;
+      }
+
+      /**
+       * State, county, province, or region (<a href="https://en.wikipedia.org/wiki/ISO_3166-2">ISO
+       * 3166-2</a>).
+       */
+      public Builder setState(EmptyParam state) {
+        this.state = state;
         return this;
       }
     }
